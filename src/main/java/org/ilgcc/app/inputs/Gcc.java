68d85dff68ff585f0629f22b28c18b204effafa7
Guilherme Burato <guiburato@gmail.com>
2024-02-01 09:31:19 -0500

    add property on inputs

--- src/main/java/org/ilgcc/app/inputs/Gcc.java
+++ src/main/java/org/ilgcc/app/inputs/Gcc.java
@@ -1,7 +1,9 @@
 package org.ilgcc.app.inputs;
 
 import formflow.library.data.FlowInputs;
+import jakarta.validation.constraints.NotBlank;
 
 public class Gcc extends FlowInputs {
-
+    @NotBlank
+    private String schoolName;
 }
