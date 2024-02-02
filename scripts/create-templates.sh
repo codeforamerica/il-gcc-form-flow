d5759f5e28486f17c0886f67a959f9e368cfea4f
Sree P <sprasad@codeforamerica.org>
2024-02-02 08:23:46 -0800

    fix spacing

--- scripts/create-templates.sh
+++ scripts/create-templates.sh
@@ -8,7 +8,7 @@ screens=( $(yq '[select(.name == "'${flowName}'") | .flow | map(key)] | flatten'
 screenNames=( $( printf '%s\n' ${screens[@]} | egrep -v '^(---|-|null|\[*\])' ) )
 for i in "${!screenNames[@]}"; do
   if grep -q "${screenNames[$i]}.html" <<< "${templates[*]}"; then
-   echo "found ${screenNames[$i]} in templates"
+    echo "found ${screenNames[$i]} in templates"
   else
     echo "${screenNames[$i]} not in templates; creating one"
     cat scripts/template.html >> "src/main/resources/templates/${flowName}/${screenNames[$i]}.html"
