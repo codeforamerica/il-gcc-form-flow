8dc8cd8a8eb0585dbe72c34fa8173b086f190203
chibuisienyia <cenyia@codeforamerica.org>
2024-02-06 10:44:24 -0600

    Updated GCC flow to include activites-ed. Updated JourneyTest to new onboarding screen #186993117

--- src/test/java/org/ilgcc/app/journeys/GccFlowJourneyTest.java
+++ src/test/java/org/ilgcc/app/journeys/GccFlowJourneyTest.java
@@ -1,5 +1,6 @@
 package org.ilgcc.app.journeys;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.ilgcc.app.utils.AbstractBasePageTest;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,11 @@ public class GccFlowJourneyTest extends AbstractBasePageTest {
     // onboarding-getting-started
     assertThat(testPage.getTitle()).isEqualTo("Getting started");
     testPage.clickContinue();
-
+    //onboarding-choose-provider
+    assertThat(testPage.getTitle()).isEqualTo("Choose Provider");
+    assertThat(testPage.getElementText("dayCareChoice-none-label")).isEqualTo("None of the above");
+    testPage.clickElementById("dayCareChoice-none-label");
+    testPage.clickContinue();
     //activities-add-ed-program
     assertThat(testPage.getTitle()).isEqualTo("Now tell us about your school or training program.");
     testPage.clickContinue();
