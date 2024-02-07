fe416a32fbe026a02688c5f83949b6e1716782cc
Sree P <sprasad@codeforamerica.org>
2024-02-07 12:49:46 -0800

    Adding language preference page and mixpanel tracking
    on 'es'.

--- src/main/java/org/ilgcc/app/inputs/Gcc.java
+++ src/main/java/org/ilgcc/app/inputs/Gcc.java
@@ -4,6 +4,9 @@ import formflow.library.data.FlowInputs;
 import jakarta.validation.constraints.NotBlank;
 
 public class Gcc extends FlowInputs {
+
+    private String lang;
+
     @NotBlank
     private String schoolName;
 
@@ -12,4 +15,7 @@ public class Gcc extends FlowInputs {
 
     @NotBlank
     private String dayCareChoice;
+
+    private String languageRead;
+    private String languageSpeak;
 }
