599bff0af18e66cf774e35214feb570ec9bcdebd
Sree P <sprasad@codeforamerica.org>
2024-02-13 09:43:19 -0800

    WIP children-info-basic subflow screen

--- src/main/java/org/ilgcc/app/inputs/Gcc.java
+++ src/main/java/org/ilgcc/app/inputs/Gcc.java
@@ -4,6 +4,8 @@ import formflow.library.data.FlowInputs;
 import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
+import java.util.List;
+
 public class Gcc extends FlowInputs {
 
     private String lang;
@@ -25,5 +27,16 @@ public class Gcc extends FlowInputs {
 
     private String languageRead;
     private String languageSpeak;
+
+    @NotBlank(message = "{errors.provide-first-name}")
+    private String childFirstName;
+    @NotBlank(message = "{errors.provide-last-name}")
+    private String childLastName;
+
+    private String childDateOfBirthDay;
+    private String childDateOfBirthMonth;
+    private String childDateOfBirthYear;
+    @NotBlank(message = "{errors.required}")
+    private String needFinancialAssistanceForChild;
     private List<String> weeklySchedule;
 }
