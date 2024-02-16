bcf932ef89ee1ed3b7d1459107c11c916ef95b4e
Sree <72890349+sree-cfa@users.noreply.github.com>
2024-02-16 14:27:13 -0800

    Fixing flow logic (#55)

---
+++ src/main/java/org/ilgcc/app/submission/conditions/NeedsFinancialAssistanceForChild.java
@@ -0,0 +1,14 @@
+package org.ilgcc.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+import org.ilgcc.app.utils.SubmissionUtilities;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NeedsFinancialAssistanceForChild implements Condition {
+
+  public Boolean run(Submission submission) {
+    return submission.getInputData().getOrDefault("needFinancialAssistanceForChild", "No").equals("Yes");
+  }
+}
