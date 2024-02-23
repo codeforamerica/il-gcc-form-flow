5fd8c369108dc966251e49531cd9f11a1571737c
chibuisienyia <cenyia@codeforamerica.org>
2024-02-23 17:41:47 -0600

    WIP - Updating children-ccap-start-date
    #187090179

--- src/main/java/org/ilgcc/app/submission/actions/ValidateChildrenCCAPStartDateWhenChildIsInChildCareWithProvider.java
+++ src/main/java/org/ilgcc/app/submission/actions/ValidateChildrenCCAPStartDateWhenChildIsInChildCareWithProvider.java
@@ -2,9 +2,6 @@ package org.ilgcc.app.submission.actions;
 
 import formflow.library.data.FormSubmission;
 import formflow.library.data.Submission;
-import java.time.DateTimeException;
-import java.time.OffsetDateTime;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -20,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class ValidateChildrenCCAPStartDate extends VerifyDate {
+public class ValidateChildrenCCAPStartDateWhenChildIsInChildCareWithProvider extends VerifyDate {
   @Autowired
   MessageSource messageSource;
 
@@ -45,12 +42,10 @@ public class ValidateChildrenCCAPStartDate extends VerifyDate {
       DateTime present = DateTime.now();
       DateTime earliest_supported_date = dtf.parseDateTime(EARLIEST_DATE_SUPPORTED);
 
-      if(this.isDateNotWithinSupportedRange(dateCCAPStart, earliest_supported_date, present)){
-        errorMessages.put(INPUT_NAME, List.of((messageSource.getMessage("errors.date-outside-of-supported-range", List.of(ccapStartingDate).toArray(), locale))));
+        if(this.isDateNotWithinSupportedRange(dateCCAPStart, earliest_supported_date, present)){
+          errorMessages.put(INPUT_NAME, List.of((messageSource.getMessage("errors.date-outside-of-supported-range", List.of(ccapStartingDate).toArray(), locale))));
+        }
       }
-    }
-
-
     return errorMessages;
   }
 }
