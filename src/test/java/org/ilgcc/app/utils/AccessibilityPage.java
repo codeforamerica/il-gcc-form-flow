7fa4e49cf1ebda2edc6175d7cb5552650617de48
Sree P <sprasad@codeforamerica.org>
2024-02-26 14:25:47 -0800

    adding axe tests

---
+++ src/test/java/org/ilgcc/app/utils/AccessibilityPage.java
@@ -0,0 +1,62 @@
+package org.ilgcc.app.utils;
+import com.deque.html.axecore.results.Results;
+import com.deque.html.axecore.results.Rule;
+import com.deque.html.axecore.selenium.AxeBuilder;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Slf4j
+public class AccessibilityPage extends Page {
+  public List<Rule> resultsList = new ArrayList<>();
+
+  public AccessibilityPage(RemoteWebDriver driver) {
+    super(driver);
+  }
+
+
+  @Override
+  public void clickLink(String linkText) {
+    super.clickLink(linkText);
+    testAccessibility();
+  }
+
+  @Override
+  public void clickButton(String buttonText) {
+    super.clickButton(buttonText);
+    testAccessibility();
+  }
+
+  @Override
+  public void clickButtonLink(String buttonLinkText) {
+    super.clickButtonLink(buttonLinkText);
+    testAccessibility();
+  }
+
+  @Override
+  public void clickContinue() {
+    super.clickContinue();
+    testAccessibility();
+  }
+
+  public void testAccessibility() {
+    AxeBuilder builder = new AxeBuilder();
+    builder.setOptions("""
+                {   "resultTypes": ["violations"],
+                    "runOnly": { 
+                        "type": "tag", 
+                        "values": ["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "section508"]
+                    } 
+                }
+                """);
+    Results results = builder.analyze(driver);
+    List<Rule> violations = results.getViolations();
+    violations.forEach(rule -> rule.setUrl(getTitle()));
+    resultsList.addAll(violations);
+    log.info("Testing a11y on page %s, found %s violations".formatted(getTitle(), violations.size()));
+  }
+}
