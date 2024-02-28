7a6f91e6b0b6b5bc79d175884c6dc52f5952e838
Sree P <sprasad@codeforamerica.org>
2024-02-28 13:33:22 -0800

    fix flows config

--- src/main/java/org/ilgcc/app/submission/actions/VerifyDate.java
+++ src/main/java/org/ilgcc/app/submission/actions/VerifyDate.java
@@ -6,22 +6,31 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 abstract class VerifyDate implements Action {
+  public static final DateTimeFormatter DTF = DateTimeFormat.forPattern("MM/dd/yyyy");
+  public static final DateTime MIN_BIRTHDATE = DTF.parseDateTime("01/01/1901");
 
   protected boolean isDateInvalid(String date) {
     try {
-      DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy");
-
-      dtf.parseDateTime(date);
+      DTF.parseDateTime(date);
     } catch (Exception e) {
       return true;
     }
     return false;
   }
 
- protected boolean isDateNotWithinSupportedRange (DateTime date, DateTime earliestSupportedDate, DateTime latestSupportedDate){
-   if(latestSupportedDate == null){
-     return (date.withTimeAtStartOfDay().isBefore(earliestSupportedDate.withTimeAtStartOfDay()) );
-   }
+  protected boolean isValidBirthdate(String date) {
+    try {
+      DateTime birthday = DTF.parseDateTime(date);
+      return MIN_BIRTHDATE.isBefore(birthday.getMillis()) && birthday.isBeforeNow();
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  protected boolean isDateNotWithinSupportedRange(DateTime date, DateTime earliestSupportedDate, DateTime latestSupportedDate) {
+    if (latestSupportedDate == null) {
+      return (date.withTimeAtStartOfDay().isBefore(earliestSupportedDate.withTimeAtStartOfDay()));
+    }
     return (date.withTimeAtStartOfDay().isBefore(earliestSupportedDate.withTimeAtStartOfDay()) || date.withTimeAtStartOfDay().isAfter(latestSupportedDate.withTimeAtStartOfDay()));
- }
+  }
 }
