4324bc954d960ec0a1721706eeee37d7e248bb52
Chibuisi Enyia <cenyia@codeforamerica.org>
2024-02-29 12:38:36 -0600

    Update error messages on update-childcare-screen (#85)
    * Update error messages on update-childcare-screen
    
    #187090179
    Co-authored-by: Cypress Borg <cborg@codeforamerica.org>

--- src/main/java/org/ilgcc/app/submission/actions/ValidateChildrenCCAPStartDate.java
+++ src/main/java/org/ilgcc/app/submission/actions/ValidateChildrenCCAPStartDate.java
@@ -4,6 +4,7 @@ import static java.util.Collections.emptyList;
 
 import formflow.library.data.FormSubmission;
 import formflow.library.data.Submission;
+import io.netty.channel.AbstractEventLoop;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -32,26 +33,35 @@ public class ValidateChildrenCCAPStartDate extends VerifyDate {
     Locale locale = LocaleContextHolder.getLocale();
     Map<String, List<String>> errorMessages = new HashMap<>();
     Map<String, Object> inputData = formSubmission.getFormData();
+    if (isDateEnteredBlank(inputData)){
+      return errorMessages;
+    }
     String ccapStartingDate = String.format("%s/%s/%s",
         (String) inputData.get("ccapStartMonth"),
         (String) inputData.get("ccapStartDay"),
         (String) inputData.get("ccapStartYear"));
     String current_uuid = (String) inputData.get("current_uuid");
     DateTime present = DateTime.now();
+
+    boolean inChildCare = isChildInChildcare(submission, current_uuid);
     if (this.isDateInvalid(ccapStartingDate)) {
-      errorMessages.put(INPUT_NAME, List.of(messageSource.getMessage("errors.invalid-date-entered", null, locale)));
-    } else if (isChildInChildcare(submission, current_uuid)) {
+      if(inChildCare){
+        errorMessages.put(INPUT_NAME, List.of(messageSource.getMessage("errors.invalid-past-date-entered", null, locale)));
+      }else {
+        errorMessages.put(INPUT_NAME, List.of(messageSource.getMessage("errors.invalid-future-date-entered", null, locale)));
+      }
+    } else if (inChildCare) {
       DateTime dateCCAPStart = dtf.parseDateTime(ccapStartingDate);
       DateTime earliest_supported_date = dtf.parseDateTime(EARLIEST_DATE_SUPPORTED);
 
       if (this.isDateNotWithinSupportedRange(dateCCAPStart, earliest_supported_date, present)) {
         errorMessages.put(INPUT_NAME, List.of(
-            (messageSource.getMessage("errors.date-outside-of-supported-range", List.of(ccapStartingDate).toArray(), locale))));
+            (messageSource.getMessage("errors.past-childcare-date-out-of-range", List.of(ccapStartingDate).toArray(), locale))));
       }
     }else{
       DateTime dateCCAPStart = dtf.parseDateTime(ccapStartingDate);
       if(this.isDateNotWithinSupportedRange(dateCCAPStart, present, null)){
-        errorMessages.put(INPUT_NAME, List.of((messageSource.getMessage("errors.date-outside-of-supported-range", List.of(ccapStartingDate).toArray(), locale))));
+        errorMessages.put(INPUT_NAME, List.of((messageSource.getMessage("errors.future-childcare-date-outside-of-range", List.of(ccapStartingDate).toArray(), locale))));
       }
     }
     return errorMessages;
@@ -66,5 +76,11 @@ public class ValidateChildrenCCAPStartDate extends VerifyDate {
     }
     return false;
   }
+  public Boolean isDateEnteredBlank(Map<String, Object> inputData){
+    boolean blankCCAPMonth = inputData.get("ccapStartMonth").toString().isBlank();
+    boolean blankCCAPDay  = inputData.get("ccapStartMonth").toString().isBlank();
+    boolean blankCCAPYear  = inputData.get("ccapStartMonth").toString().isBlank();
 
+    return blankCCAPMonth && blankCCAPDay && blankCCAPYear;
+  }
 }
