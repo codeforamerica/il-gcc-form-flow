491c77a9dc82e2db2bcd92f1550b1244cff76b53
Sree P <sprasad@codeforamerica.org>
2024-02-29 09:45:52 -0800

    Cleanup

--- src/main/java/org/ilgcc/app/submission/actions/SetWeeklyScheduleGroup.java
+++ src/main/java/org/ilgcc/app/submission/actions/SetWeeklyScheduleGroup.java
@@ -7,10 +7,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -21,17 +18,20 @@ public class SetWeeklyScheduleGroup implements Action {
 
   public static final List<String> WEEKDAYS = List.of("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
 
+  public static final String DISPLAY_LABEL = "displayWeeklySchedule";
+
   public SetWeeklyScheduleGroup(MessageSource messageSource) {
     this.messageSource = messageSource;
   }
 
   @Override
   public void run(Submission submission) {
-    if (!submission.getInputData().containsKey("weeklySchedule[]")) {
+    Map<String, Object> inputData = submission.getInputData();
+    if (!inputData.containsKey("weeklySchedule[]")) {
       return;
     }
 
-    var weeklySchedule = new ArrayList<>((List<String>) submission.getInputData().get("weeklySchedule[]"));
+    var weeklySchedule = new ArrayList<>((List<String>) inputData.get("weeklySchedule[]"));
     var sortedDays = WEEKDAYS.stream().filter(weeklySchedule::contains).toList();
     String firstDay = null, lastDay = null;
     Locale locale = LocaleContextHolder.getLocale();
@@ -39,26 +39,20 @@ public class SetWeeklyScheduleGroup implements Action {
     Iterator<String> iter = WEEKDAYS.iterator();
     while (iter.hasNext() && !weeklySchedule.isEmpty()) {
       String day = iter.next();
-      if (weeklySchedule.contains(day)) {
+      if (weeklySchedule.remove(day)) {
         if (firstDay == null) {
           firstDay = messageSource.getMessage("general.week." + day, null, locale);
         } else {
           lastDay = messageSource.getMessage("general.week." + day, null, locale);
         }
-        weeklySchedule.remove(day);
       } else if (firstDay != null) {
         var displayDays = formatWeekdaysSeparated(sortedDays, locale);
-        submission.getInputData().put("displayWeeklySchedule", displayDays);
+        inputData.put(DISPLAY_LABEL, displayDays);
         return;
       }
     }
 
-    if (lastDay != null) {
-      submission.getInputData().put("displayWeeklySchedule", "%s-%s".formatted(firstDay, lastDay));
-      return;
-    }
-
-    submission.getInputData().put("displayWeeklySchedule", firstDay);
+    inputData.put(DISPLAY_LABEL, lastDay == null ? firstDay : "%s-%s".formatted(firstDay, lastDay));
   }
 
   private String formatWeekdaysSeparated(List<String> sortedDays, Locale locale) {
