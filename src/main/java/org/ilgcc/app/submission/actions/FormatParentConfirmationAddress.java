c4e5e21c574e79bda0db661e472f96ceea617d60
chibuisienyia <cenyia@codeforamerica.org>
2024-03-11 14:18:01 -0500

    Updated `FormatParentConfirmationAddress` to uses the mailing address and to redirect users to the home or mailing address based on response to `experiencingHomelessness` question.
    #187090179
    
    Co-authored-by: Sree Prasad <sprasad@codeforamerica.org>

--- src/main/java/org/ilgcc/app/submission/actions/FormatParentConfirmationAddress.java
+++ src/main/java/org/ilgcc/app/submission/actions/FormatParentConfirmationAddress.java
@@ -2,6 +2,7 @@ package org.ilgcc.app.submission.actions;
 
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
+import org.ilgcc.app.utils.SubmissionUtilities;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -15,21 +16,25 @@ public class FormatParentConfirmationAddress implements Action {
   public void run(Submission submission) {
     Map<String, Object> inputData = submission.getInputData();
     // TODO check if mailing addr is different than home addr
-    var parentHomeStreetAddress1 = (String) inputData.get("parentHomeStreetAddress1");
-    var parentHomeStreetAddress2 = (String) inputData.get("parentHomeStreetAddress2");
-    var parentHomeCity = (String) inputData.get("parentHomeCity");
-    var parentHomeState = (String) inputData.get("parentHomeState");
-    var parentHomeZipCode = (String) inputData.get("parentHomeZipCode");
+    var parentMailingStreetAddress1 = (String) inputData.get("parentMailingStreetAddress1");
+    var parentMailingStreetAddress2 = (String) inputData.get("parentMailingStreetAddress2");
+    var parentMailingCity = (String) inputData.get("parentMailingCity");
+    var parentMailingState = (String) inputData.get("parentMailingState");
+    var parentMailingZipCode = (String) inputData.get("parentMailingZipCode");
 
     List<String> addressLines = new ArrayList<>();
-    addressLines.add(parentHomeStreetAddress1);
-    addressLines.add(parentHomeStreetAddress2);
-    addressLines.add("%s, %s".formatted(parentHomeCity, parentHomeState));
-    addressLines.add(parentHomeZipCode);
+    addressLines.add(parentMailingStreetAddress1);
+    addressLines.add(parentMailingStreetAddress2);
+    addressLines.add("%s, %s".formatted(parentMailingCity, parentMailingState));
+    addressLines.add(parentMailingZipCode);
 
     inputData.put("addressLines", addressLines);
 
     // TODO redirect based on - selected homeless (mailing), mailing address different than home (mailing), mailing is the same as home (home)
-    inputData.put("redirectPage", "parent-home-address");
+    if(SubmissionUtilities.parentIsExperiencingHomelessness(inputData)){
+      inputData.put("redirectPage", "parent-mailing-address");
+    }else{
+      inputData.put("redirectPage", "parent-home-address");
+    }
   }
 }
