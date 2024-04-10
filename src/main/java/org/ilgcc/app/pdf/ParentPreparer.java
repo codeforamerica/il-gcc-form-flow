4703947479d6f265b2b649e09c94ee46fa860977
Chibuisi Enyia <cenyia@codeforamerica.org>
2024-04-10 11:44:28 -0500

    Update mapping (#203)
    Co-authored-by: Sree Prasad <sprasad@codeforamerica.org>
    Co-authored-by: Shayna Cummings <scummings@codeforamerica.org>

---
+++ src/main/java/org/ilgcc/app/pdf/ParentPreparer.java
@@ -0,0 +1,43 @@
+package org.ilgcc.app.pdf;
+
+import formflow.library.data.Submission;
+import formflow.library.pdf.CheckboxField;
+import formflow.library.pdf.PdfMap;
+import formflow.library.pdf.SingleField;
+import formflow.library.pdf.SubmissionField;
+import formflow.library.pdf.SubmissionFieldPreparer;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParentPreparer implements SubmissionFieldPreparer {
+
+  @Override
+  public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, PdfMap pdfMap) {
+    var results= new HashMap<String, SubmissionField>();
+    //active duty military
+    String parentIsActiveDutyMilitary = (String) submission.getInputData().getOrDefault("parentIsServing", "");
+    if (!parentIsActiveDutyMilitary.isBlank()){
+      results.put("parentIsServing", new SingleField("parentIsServing", selectedYes(parentIsActiveDutyMilitary),null));
+    }
+
+    //applicant is reserve or national guard
+    String parentIsReserveOrNationalGuard = (String) submission.getInputData().getOrDefault("parentInMilitaryReserveOrNationalGuard", "");
+    if(!parentIsReserveOrNationalGuard.isBlank()){
+      results.put("parentInMilitaryReserveOrNationalGuard", new SingleField("parentInMilitaryReserveOrNationalGuard", selectedYes(parentIsReserveOrNationalGuard),null));
+
+    }
+    return results;
+  }
+
+
+  private static String selectedYes(String selected){
+    if (selected.equals("Yes")){
+      return "true";
+    }else
+      return "false";
+  }
+
+
+}
