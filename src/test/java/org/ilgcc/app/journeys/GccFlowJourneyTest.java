7134fc0ae860d0a26a80460423d07e6f1a60a8ab
Ana Medrano Fernandez <amedrano@codeforamerica.org@Anas-MacBook-Pro-2.local>
2024-05-09 16:27:17 -0700

    Fix broken journey test

--- src/test/java/org/ilgcc/app/journeys/GccFlowJourneyTest.java
+++ src/test/java/org/ilgcc/app/journeys/GccFlowJourneyTest.java
@@ -414,6 +414,12 @@ public class GccFlowJourneyTest extends AbstractBasePageTest {
         testPage.enter("partnerSignedName", "partner parent");
         testPage.clickContinue();
 
+        // submit-next-steps
+        testPage.clickContinue();
+
+        assertThat(testPage.getTitle()).isEqualTo("Recommended documents");
+        testPage.clickButton("Submit documents now");
+
         // doc-upload-recommended docs
         assertThat(testPage.getTitle()).isEqualTo("Recommended documents");
         testPage.clickButton("Submit documents now");
