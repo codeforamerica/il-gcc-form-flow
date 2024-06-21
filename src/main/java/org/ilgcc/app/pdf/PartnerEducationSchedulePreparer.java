87517abde2a085b32925431c7072014e28d62129
Ana Medrano <amedrano@codeforamerica.org>
2024-06-21 14:14:18 -0700

    Implement PartnerEducationSchedule Preparer and Map

---
+++ src/main/java/org/ilgcc/app/pdf/PartnerEducationSchedulePreparer.java
@@ -0,0 +1,35 @@
+package org.ilgcc.app.pdf;
+
+import formflow.library.data.Submission;
+import formflow.library.pdf.PdfMap;
+import formflow.library.pdf.SubmissionField;
+import formflow.library.pdf.SubmissionFieldPreparer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.ilgcc.app.utils.ActivitySchedules.HourlySchedule;
+import org.ilgcc.app.utils.ActivitySchedules.LocalTimeRange;
+import org.ilgcc.app.utils.DayOfWeekOption;
+import org.ilgcc.app.utils.SchedulePreparerUtility;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PartnerEducationSchedulePreparer implements SubmissionFieldPreparer {
+    @Override
+    public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, PdfMap pdfMap) {
+        var results = new HashMap<String, SubmissionField>();
+
+        Optional<HourlySchedule> educationSchedule = SchedulePreparerUtility.getHourlySchedule(submission, "partnerClass", "partnerClassWeeklySchedule[]");
+        if (educationSchedule.isEmpty()) {
+            return results;
+        }
+
+        Map<DayOfWeekOption, LocalTimeRange> dailyScheduleMap = educationSchedule.get().toDailyScheduleMap();
+        for (var scheduleEntry : dailyScheduleMap.entrySet()) {
+            DayOfWeekOption day = scheduleEntry.getKey();
+            LocalTimeRange schedule = scheduleEntry.getValue();
+            results.putAll(SchedulePreparerUtility.createSubmissionFieldsFromSchedule(schedule, day, "partnerEducationSchedule"));
+        }
+        return results;
+    }
+}
---
+++ src/test/java/org/ilgcc/app/pdf/PartnerEducationSchedulePreparerTest.java
@@ -0,0 +1,103 @@
+package org.ilgcc.app.pdf;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import formflow.library.data.Submission;
+import formflow.library.pdf.SingleField;
+import formflow.library.pdf.SubmissionField;
+import java.util.List;
+import java.util.Map;
+import org.ilgcc.app.utils.SubmissionTestBuilder;
+import org.junit.jupiter.api.Test;
+
+public class PartnerEducationSchedulePreparerTest {
+
+    PartnerEducationSchedulePreparer preparer = new PartnerEducationSchedulePreparer();
+
+    private Submission submission;
+
+    @Test
+    public void withTheSameScheduleEveryDay() {
+        submission = new SubmissionTestBuilder()
+            .withRegularSchoolSchedule("partnerClass", "partnerClassWeeklySchedule[]", List.of("Monday", "Thursday","Sunday"),"10:00", "15:45")
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+
+        assertThat(result.get("partnerEducationScheduleMondayStart")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayStart", "10:00", null));
+        assertThat(result.get("partnerEducationScheduleMondayStartAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayStartAmPm", "AM", null));
+        assertThat(result.get("partnerEducationScheduleMondayEnd")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayEnd", "03:45", null));
+        assertThat(result.get("partnerEducationScheduleMondayEndAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayEndAmPm", "PM", null));
+
+        assertThat(result.get("partnerEducationScheduleTuesdayStart")).isEqualTo(null);
+        assertThat(result.get("partnerEducationScheduleTuesdayStartAmPm")).isEqualTo(null);
+        assertThat(result.get("partnerEducationScheduleTuesdayEnd")).isEqualTo(null);
+        assertThat(result.get("partnerEducationScheduleTuesdayEndAmPm")).isEqualTo(null);
+
+        assertThat(result.get("partnerEducationScheduleThursdayStart")).isEqualTo(
+            new SingleField("partnerEducationScheduleThursdayStart", "10:00", null));
+        assertThat(result.get("partnerEducationScheduleThursdayStartAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleThursdayStartAmPm", "AM", null));
+        assertThat(result.get("partnerEducationScheduleThursdayEnd")).isEqualTo(
+            new SingleField("partnerEducationScheduleThursdayEnd", "03:45", null));
+        assertThat(result.get("partnerEducationScheduleThursdayEndAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleThursdayEndAmPm", "PM", null));
+
+        assertThat(result.get("partnerEducationScheduleSundayStart")).isEqualTo(
+            new SingleField("partnerEducationScheduleSundayStart", "10:00", null));
+        assertThat(result.get("partnerEducationScheduleSundayStartAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleSundayStartAmPm", "AM", null));
+        assertThat(result.get("partnerEducationScheduleSundayEnd")).isEqualTo(
+            new SingleField("partnerEducationScheduleSundayEnd", "03:45", null));
+        assertThat(result.get("partnerEducationScheduleSundayEndAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleSundayEndAmPm", "PM", null));
+    }
+
+    @Test
+    public void withDifferentScheduleEveryDay() {
+        submission = new SubmissionTestBuilder()
+            .withSchoolScheduleByDay("partnerClass","Monday","10:00", "15:45")
+            .withSchoolScheduleByDay("partnerClass","Wednesday", "08:00", "12:45")
+            .withSchoolScheduleByDay("partnerClass","Friday","12:00", "19:00")
+            .with("weeklySchedule[]", List.of("Monday", "Wednesday", "Friday"))
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+
+        assertThat(result.get("partnerEducationScheduleMondayStart")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayStart", "10:00", null));
+        assertThat(result.get("partnerEducationScheduleMondayStartAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayStartAmPm", "AM", null));
+        assertThat(result.get("partnerEducationScheduleMondayEnd")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayEnd", "03:45", null));
+        assertThat(result.get("partnerEducationScheduleMondayEndAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleMondayEndAmPm", "PM", null));
+
+        assertThat(result.get("partnerEducationScheduleTuesdayStart")).isEqualTo(null);
+        assertThat(result.get("partnerEducationScheduleTuesdayStartAmPm")).isEqualTo(null);
+        assertThat(result.get("partnerEducationScheduleTuesdayEnd")).isEqualTo(null);
+        assertThat(result.get("partnerEducationScheduleTuesdayEndAmPm")).isEqualTo(null);
+
+        assertThat(result.get("partnerEducationScheduleWednesdayStart")).isEqualTo(
+            new SingleField("partnerEducationScheduleWednesdayStart", "08:00", null));
+        assertThat(result.get("partnerEducationScheduleWednesdayStartAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleWednesdayStartAmPm", "AM", null));
+        assertThat(result.get("partnerEducationScheduleWednesdayEnd")).isEqualTo(
+            new SingleField("partnerEducationScheduleWednesdayEnd", "12:45", null));
+        assertThat(result.get("partnerEducationScheduleWednesdayEndAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleWednesdayEndAmPm", "PM", null));
+
+        assertThat(result.get("partnerEducationScheduleFridayStart")).isEqualTo(
+            new SingleField("partnerEducationScheduleFridayStart", "12:00", null));
+        assertThat(result.get("partnerEducationScheduleFridayStartAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleFridayStartAmPm", "PM", null));
+        assertThat(result.get("partnerEducationScheduleFridayEnd")).isEqualTo(
+            new SingleField("partnerEducationScheduleFridayEnd", "07:00", null));
+        assertThat(result.get("partnerEducationScheduleFridayEndAmPm")).isEqualTo(
+            new SingleField("partnerEducationScheduleFridayEndAmPm", "PM", null));
+    }
+}
