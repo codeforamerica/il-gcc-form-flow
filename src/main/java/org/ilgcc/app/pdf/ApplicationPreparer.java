f01fb19e35f0fa7ce57f7824df0f5e5104a3d710
Ana Medrano <amedrano@codeforamerica.org>
2024-06-24 12:55:01 -0700

    Implement otherMonthlyIncome total

--- src/main/java/org/ilgcc/app/pdf/ApplicationPreparer.java
+++ src/main/java/org/ilgcc/app/pdf/ApplicationPreparer.java
@@ -1,5 +1,7 @@
 package org.ilgcc.app.pdf;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.list;
 import static org.ilgcc.app.utils.SubmissionUtilities.formatToStringFromLocalDate;
 
 import formflow.library.data.Submission;
@@ -10,6 +12,7 @@ import formflow.library.pdf.SubmissionFieldPreparer;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import net.bytebuddy.asm.Advice.Local;
@@ -28,8 +31,21 @@ public class ApplicationPreparer implements SubmissionFieldPreparer {
 
         if (!partnerSignature.equals("")) {
             Optional<LocalDate> partnerSignatureDate = Optional.of(LocalDate.from(submission.getSubmittedAt()));
-            results.put("partnerSignedAt", new SingleField("partnerSignedAt", formatToStringFromLocalDate(partnerSignatureDate), null));
+            results.put("partnerSignedAt",
+                new SingleField("partnerSignedAt", formatToStringFromLocalDate(partnerSignatureDate), null));
         }
+
+        Integer rentalIncome = Integer.parseInt(inputData.getOrDefault("unearnedIncomeRental", "0").toString());
+        Integer dividendIncome = Integer.parseInt(inputData.getOrDefault("unearnedIncomeDividends", "0").toString());
+        Integer unemploymentIncome = Integer.parseInt(inputData.getOrDefault("unearnedIncomeUnemployment", "0").toString());
+        Integer royaltiesIncome = Integer.parseInt(inputData.getOrDefault("unearnedIncomeRoyalties", "0").toString());
+        Integer pensionIncome = Integer.parseInt(inputData.getOrDefault("unearnedIncomePension", "0").toString());
+        Integer workersIncome = Integer.parseInt(inputData.getOrDefault("unearnedIncomeWorkers", "0").toString());
+
+        var totalExpenses = rentalIncome + dividendIncome + unemploymentIncome + royaltiesIncome + pensionIncome + workersIncome;
+
+        results.put("otherMonthlyIncomeApplicant",
+            new SingleField("otherMonthlyIncomeApplicant", Integer.toString(totalExpenses), null));
 
         return results;
     }
--- src/test/java/org/ilgcc/app/pdf/ApplicationPreparerTest.java
+++ src/test/java/org/ilgcc/app/pdf/ApplicationPreparerTest.java
@@ -12,15 +12,16 @@ import org.ilgcc.app.utils.SubmissionTestBuilder;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationPreparerTest {
+
   private final ApplicationPreparer preparer = new ApplicationPreparer();
 
   private Submission submission;
 
   @Test
-  public void addsPartnerSignatureDateIfPartnerSignatureExists(){
+  public void addsPartnerSignatureDateIfPartnerSignatureExists() {
     submission = new SubmissionTestBuilder()
         .with("partnerSignedName", "PartnerSignature")
-        .withSubmittedAtDate(OffsetDateTime.of(2022,10,11,0,0,0,0, ZoneOffset.ofTotalSeconds(0)))
+        .withSubmittedAtDate(OffsetDateTime.of(2022, 10, 11, 0, 0, 0, 0, ZoneOffset.ofTotalSeconds(0)))
         .build();
 
     Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
@@ -29,13 +30,38 @@ public class ApplicationPreparerTest {
   }
 
   @Test
-  public void doesNotAddPartnerSignatureDateIfPartnerSignatureBlank(){
+  public void doesNotAddPartnerSignatureDateIfPartnerSignatureBlank() {
     submission = new SubmissionTestBuilder()
-        .withSubmittedAtDate(OffsetDateTime.of(2022,10,11,0,0,0,0, ZoneOffset.ofTotalSeconds(0)))
+        .withSubmittedAtDate(OffsetDateTime.of(2022, 10, 11, 0, 0, 0, 0, ZoneOffset.ofTotalSeconds(0)))
         .build();
 
     Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
 
     assertThat(result.get("partnerSignedAt")).isEqualTo(null);
+  }
+
+  @Test
+  public void setsOtherMonthlyIncomeToZeroWhenNoUnearnedIncome(){
+    submission = new SubmissionTestBuilder().build();
+
+    Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+
+    assertThat(result.get("otherMonthlyIncomeApplicant")).isEqualTo(new SingleField("otherMonthlyIncomeApplicant", "0", null));
+  }
+
+  @Test
+  public void setsOtherMonthlyIncomeByAddingUnearnedIncome(){
+    submission = new SubmissionTestBuilder()
+        .with("unearnedIncomeRental", "123")
+        .with("unearnedIncomeDividends", "127")
+        .with("unearnedIncomeUnemployment", "124")
+        .with("unearnedIncomeRoyalties", "126")
+        .with("unearnedIncomePension", "122")
+        .with("unearnedIncomeWorkers", "128")
+        .build();
+
+    Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+
+    assertThat(result.get("otherMonthlyIncomeApplicant")).isEqualTo(new SingleField("otherMonthlyIncomeApplicant", "750", null));
   }
 }
