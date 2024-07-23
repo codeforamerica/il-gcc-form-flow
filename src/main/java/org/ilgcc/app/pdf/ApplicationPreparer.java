2092cf8854f20d92ebbf8c8e4b00e1e352ba411e
Cypress Borg <cborg@codeforamerica.org>
2024-07-23 13:54:51 -0500

    Add new PDF custom preparers
    - Add unit test for parentFullName
    - Add unit test for receivedTimestamp
    - Ignore these fields in verifyPDF

--- src/main/java/org/ilgcc/app/pdf/ApplicationPreparer.java
+++ src/main/java/org/ilgcc/app/pdf/ApplicationPreparer.java
@@ -8,6 +8,9 @@ import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
 import formflow.library.pdf.SubmissionFieldPreparer;
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -20,16 +23,29 @@ public class ApplicationPreparer implements SubmissionFieldPreparer {
     @Override
     public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, PdfMap pdfMap) {
         var results = new HashMap<String, SubmissionField>();
-
         var inputData = submission.getInputData();
+        var receivedTimestampFormat = DateTimeFormatter.ofPattern("MMMM d, yyyy, h:mm a zzz");
+        ZoneId chicagoTimeZone = ZoneId.of("America/Chicago");
+
+        Optional<OffsetDateTime> submittedAt = Optional.ofNullable(submission.getSubmittedAt());
+        submittedAt.ifPresent(offsetDateTime -> {
+            offsetDateTime.atZoneSameInstant(chicagoTimeZone);
+            String formattedSubmittedAtDate = offsetDateTime.atZoneSameInstant(chicagoTimeZone).format(receivedTimestampFormat);
+            results.put("receivedTimestamp", new SingleField("receivedTimestamp", formattedSubmittedAtDate, null));
+        });
 
         var partnerSignature = inputData.getOrDefault("partnerSignedName", "");
 
         if (!partnerSignature.equals("")) {
             Optional<LocalDate> partnerSignatureDate = Optional.of(LocalDate.from(submission.getSubmittedAt()));
             results.put("partnerSignedAt",
-                new SingleField("partnerSignedAt", formatToStringFromLocalDate(partnerSignatureDate), null));
+                    new SingleField("partnerSignedAt", formatToStringFromLocalDate(partnerSignatureDate), null));
         }
+
+        var parentFirstName = inputData.getOrDefault("parentFirstName", "");
+        var parentLastName = inputData.getOrDefault("parentLastName", "");
+        results.put("parentFullName",
+                new SingleField("parentFullName", String.format("%s, %s", parentLastName, parentFirstName), null));
 
         String rentalIncome = inputData.getOrDefault("unearnedIncomeRental", "").toString();
         String dividendIncome = inputData.getOrDefault("unearnedIncomeDividends", "").toString();
@@ -38,10 +54,12 @@ public class ApplicationPreparer implements SubmissionFieldPreparer {
         String pensionIncome = inputData.getOrDefault("unearnedIncomePension", "0").toString();
         String workersIncome = inputData.getOrDefault("unearnedIncomeWorkers", "0").toString();
 
-        var totalExpenses = PreparerUtilities.numberValueOf(rentalIncome) + PreparerUtilities.numberValueOf(dividendIncome) + PreparerUtilities.numberValueOf(unemploymentIncome) + PreparerUtilities.numberValueOf(royaltiesIncome) + PreparerUtilities.numberValueOf(pensionIncome) + PreparerUtilities.numberValueOf(workersIncome);
+        var totalExpenses = PreparerUtilities.numberValueOf(rentalIncome) + PreparerUtilities.numberValueOf(dividendIncome)
+                + PreparerUtilities.numberValueOf(unemploymentIncome) + PreparerUtilities.numberValueOf(royaltiesIncome)
+                + PreparerUtilities.numberValueOf(pensionIncome) + PreparerUtilities.numberValueOf(workersIncome);
 
         results.put("otherMonthlyIncomeApplicant",
-            new SingleField("otherMonthlyIncomeApplicant", String.format("%.0f", Math.floor(totalExpenses)), null));
+                new SingleField("otherMonthlyIncomeApplicant", String.format("%.0f", Math.floor(totalExpenses)), null));
 
         return results;
     }
