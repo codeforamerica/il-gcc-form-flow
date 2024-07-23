2274679c42f6a92dfc8f50d4efdf4d339b5efa06
Marc Perlman <mperlman@codeforamerica.org>
2024-07-23 10:34:07 -0500

    fixing up script

--- scripts/jobrunr_dashboard_launcher.sh
+++ scripts/jobrunr_dashboard_launcher.sh
@@ -1,4 +1,4 @@
-# script for starting jobrunr dashboard via Aptible, used in Procfile
 #!/bin/bash
+# script for starting jobrunr dashboard via Aptible, used in Procfile
 export JOBRUNR_DASHBOARD_ENABLED=true
 exec  java -jar /opt/il-gcc/app.jar
