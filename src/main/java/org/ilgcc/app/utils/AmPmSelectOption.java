003a136db5405dd406a53ce81a8c2f73ba30964b
Ana Medrano <amedrano@codeforamerica.org>
2024-08-16 11:58:34 -0700

    Implement time select front end

---
+++ src/main/java/org/ilgcc/app/utils/AmPmSelectOption.java
@@ -0,0 +1,12 @@
+package org.ilgcc.app.utils;
+
+public enum AmPmSelectOption {
+  AM("AM"),
+  PM("PM");
+
+  private final String value;
+
+  AmPmSelectOption(String value) {
+    this.value = value;
+  }
+}
