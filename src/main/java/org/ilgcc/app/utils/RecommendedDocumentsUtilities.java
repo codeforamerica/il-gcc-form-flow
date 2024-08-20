96745d65b83b728626921c550270ee80e0017f70
chibuisienyia <cenyia@codeforamerica.org>
2024-08-20 10:51:09 -0500

    CCAP-321 Skip doc-upload-recommend-docs page

---
+++ src/main/java/org/ilgcc/app/utils/RecommendedDocumentsUtilities.java
@@ -0,0 +1,102 @@
+package org.ilgcc.app.utils;
+
+import static java.util.Collections.emptyList;
+
+import formflow.library.data.Submission;
+import java.util.List;
+import java.util.Map;
+
+public class RecommendedDocumentsUtilities {
+
+  //create a function that checks homelessness
+  public static boolean showRequiredHomelessnessDocuments(Submission submission) {
+    return submission.getInputData().getOrDefault("parentHomeExperiencingHomelessness[]", List.of("no")).equals(List.of("yes"));
+  }
+
+  public static boolean showTANFDocumentation(Submission submission) {
+    Map<String, Object> inputData = submission.getInputData();
+    List<String> parentNeedForChildcareReasons = (List<String>) inputData.getOrDefault("activitiesParentChildcareReason[]",
+        List.of());
+    boolean tanfISReasonForParentChildcareNeed = parentNeedForChildcareReasons.stream().anyMatch(
+        reason -> reason.equals(ChildcareReasonOption.TANF_TRAINING.toString())
+    );
+    boolean parentHasAPartner = inputData.getOrDefault("parentHasPartner", "false").equals("true");
+    if (parentHasAPartner) {
+      List<String> partnerNeedForChildcareReasons = (List<String>) inputData.getOrDefault(
+          "activitiesParentPartnerChildcareReason[]", List.of());
+      boolean tanfISReasonForPartnerChildcareNeed = partnerNeedForChildcareReasons.stream().anyMatch(
+          reason -> reason.equals(ChildcareReasonOption.TANF_TRAINING.toString())
+      );
+      return (tanfISReasonForPartnerChildcareNeed || tanfISReasonForParentChildcareNeed);
+    }
+    return tanfISReasonForParentChildcareNeed;
+  }
+
+  public static boolean showWorkingDocumentation(Submission submission) {
+    Map<String, Object> inputData = submission.getInputData();
+    List<String> parentNeedForChildcareReasons = (List<String>) inputData.getOrDefault("activitiesParentChildcareReason[]",
+        List.of());
+    boolean workingISReasonForParentChildcareNeed = parentNeedForChildcareReasons.stream().anyMatch(
+        reason -> reason.equals(ChildcareReasonOption.WORKING.toString())
+    );
+    boolean parentHasAPartner = inputData.getOrDefault("parentHasPartner", "false").equals("true");
+    if (parentHasAPartner) {
+      List<String> partnerNeedForChildcareReasons = (List<String>) inputData.getOrDefault(
+          "activitiesParentPartnerChildcareReason[]", List.of());
+      boolean workingISReasonForPartnerChildcareNeed = partnerNeedForChildcareReasons.stream().anyMatch(
+          reason -> reason.equals(ChildcareReasonOption.WORKING.toString())
+      );
+      return (workingISReasonForPartnerChildcareNeed || workingISReasonForParentChildcareNeed);
+    }
+    return workingISReasonForParentChildcareNeed;
+  }
+
+  public static boolean showSchoolOrTrainingDocumentation(Submission submission) {
+    Map<String, Object> inputData = submission.getInputData();
+    List<String> parentNeedForChildcareReasons = (List<String>) inputData.getOrDefault("activitiesParentChildcareReason[]",
+        List.of());
+    boolean schoolISReasonForParentChildcareNeed = parentNeedForChildcareReasons.stream().anyMatch(
+        reason -> reason.equals(ChildcareReasonOption.SCHOOL.toString())
+    );
+    boolean parentHasAPartner = inputData.getOrDefault("parentHasPartner", "false").equals("true");
+    if (parentHasAPartner) {
+      List<String> partnerNeedForChildcareReasons = (List<String>) inputData.getOrDefault(
+          "activitiesParentPartnerChildcareReason[]", List.of());
+      boolean schoolISReasonForPartnerChildcareNeed = partnerNeedForChildcareReasons.stream().anyMatch(
+          reason -> reason.equals(ChildcareReasonOption.SCHOOL.toString())
+      );
+      return (schoolISReasonForPartnerChildcareNeed || schoolISReasonForParentChildcareNeed);
+    }
+    return schoolISReasonForParentChildcareNeed;
+  }
+
+  public static boolean showSelfEmploymentDocumentation(Submission submission) {
+    //get jobs for parent if parent has a job
+    List<Map> jobs = (List<Map>) submission.getInputData().getOrDefault("jobs", emptyList());
+    if (!jobs.isEmpty()) {
+      for (var job : jobs) {
+        if (job.getOrDefault("isSelfEmployed", "no").equals("yes")) {
+          return true;
+        }
+      }
+    }
+    List<Map> partnerJobs = (List<Map>) submission.getInputData().getOrDefault("partnerJobs", emptyList());
+    if (!partnerJobs.isEmpty()) {
+      for (var job : partnerJobs) {
+        if (job.getOrDefault("isSelfEmployed", "no").equals("yes")) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public static boolean shouldDisplayRecommendedDocumentScreens(Submission submission){
+    return (showSchoolOrTrainingDocumentation(submission) ||
+        showTANFDocumentation(submission) ||
+        showWorkingDocumentation(submission) ||
+        showSelfEmploymentDocumentation(submission) ||
+        showSelfEmploymentDocumentation(submission)) ||
+        showRequiredHomelessnessDocuments(submission);
+  }
+}
--- src/test/java/org/ilgcc/app/journeys/DocumentUploadConditionalLogicJourneyTest.java
+++ src/test/java/org/ilgcc/app/journeys/DocumentUploadConditionalLogicJourneyTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.ilgcc.app.utils.AbstractBasePageTest;
 import org.junit.jupiter.api.Test;
-
+import org.ilgcc.app.utils.ChildcareReasonOption;
 public class DocumentUploadConditionalLogicJourneyTest extends AbstractBasePageTest {
     @Test
     void skipDocUploadRecommendedDocsScreenIfNoDocumentsAreRequiredForParentandNoSpouse(){
@@ -15,9 +15,47 @@ public class DocumentUploadConditionalLogicJourneyTest extends AbstractBasePageT
         .with("parentMailingAddressSameAsHomeAddress[]", List.of("no"))
         .withMailingAddress("972 Mission St", "5", "San Francisco", "CA", "94103")
         .with("parentHomeExperiencingHomelessness[]", List.of())
-        .with("activitiesParentChildcareReason", List.of())
+        .with("activitiesParentChildcareReason[]", List.of())
         .build());
     assertThat(testPage.getTitle()).isEqualTo(getEnMessage("submit-ccap-terms.title"));
-//    testPage.clickElementById();
+    testPage.clickElementById("agreesToLegalTerms-true");
+    testPage.clickContinue();
+
+    // submit-sign-name
+    assertThat(testPage.getTitle()).isEqualTo(getEnMessage("submit-sign-name.title"));
+    testPage.enter("signedName", "parent first parent last");
+    testPage.clickButton(getEnMessage("submit-sign-name.submit-application"));
+
+    // submit-complete
+    assertThat(testPage.getTitle()).isEqualTo(getEnMessage("submit-complete.title"));
+    testPage.clickButton(getEnMessage("doc-upload-recommended-docs.submit"));
+
+    assertThat(testPage.getTitle()).isEqualTo(getEnMessage("doc-upload-add-files.title"));
+    }
+    @Test
+    void skipNotSkipDocUploadRecommendedDocsScreenIfOneDocumentIsRequiredForParent(){
+        testPage.navigateToFlowScreen("gcc/submit-ccap-terms");
+        saveSubmission(getSessionSubmissionTestBuilder().withDayCareProvider()
+            .withParentDetails()
+            .withChild("First", "Child", "Yes")
+            .with("parentMailingAddressSameAsHomeAddress[]", List.of("no"))
+            .withMailingAddress("972 Mission St", "5", "San Francisco", "CA", "94103")
+            .with("parentHomeExperiencingHomelessness[]", List.of())
+            .with("activitiesParentChildcareReason[]", List.of("TANF_TRAINING"))
+            .build());
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("submit-ccap-terms.title"));
+        testPage.clickElementById("agreesToLegalTerms-true");
+        testPage.clickContinue();
+
+        // submit-sign-name
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("submit-sign-name.title"));
+        testPage.enter("signedName", "parent first parent last");
+        testPage.clickButton(getEnMessage("submit-sign-name.submit-application"));
+
+        // submit-complete
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("submit-complete.title"));
+        testPage.clickButton(getEnMessage("doc-upload-recommended-docs.submit"));
+
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("doc-upload-add-files.title"));
     }
 }
