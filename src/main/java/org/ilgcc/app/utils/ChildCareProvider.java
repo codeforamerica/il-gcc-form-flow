a0d4d503dd246717621cb21aad9322d0e97e9889
Marc P <mperlman@codeforamerica.org>
2024-09-18 10:00:02 -0500

    [CCAP-365] Add additional providers to CCAP application (#608)

--- src/main/java/org/ilgcc/app/utils/ChildCareProvider.java
+++ src/main/java/org/ilgcc/app/utils/ChildCareProvider.java
@@ -8,9 +8,14 @@ import java.util.List;
 @Getter
 public enum ChildCareProvider {
 
+  BROWN_BEAR_DAYCARE("Brown Bear Daycare", "747750839233009", "21007 McGuire Rd", null, "Harvard", "IL", "60033", null),
   CHILDRENS_LEARNING_CENTER("Children's Learning Center", "895593365433000", "905 S 4th St", "PO Box 531", "DeKalb", "IL", "60115", "815-756-3506"),
   THE_GROWING_PLACE("The Growing Place", "155014365433009", "909 S 4th St", null, "DeKalb", "IL", "60115", null),
+  KISHWAUKEE_COLLEGE_EARLY_LEARNING_CENTER("Kishwaukee College Early Learning Center", "226234365433006", "21193 Malta Rd", null, "Malta", "IL", "60150", null),
+  NIU_CHILD_DEVELOPMENT_FAMILY_CENTER("NIU Child Development & Family Center", "786045365433005", "NIU Campus Child Care Bldg", null, "DeKalb", "IL", "60115", null),
+  ONCE_UPON_A_TIME_CHILD_CARE("Once Upon A Time Child Care", "295762914133052", "1778 W State St", "Ste C", "Sycamore", "IL", "60178", null),
   OPEN_SESAME("Open Sesame", "239246768533006", "1101 Middle Rd", null, "Dixon", "IL", "61021", null);
+
 
   private final String displayName;
   private final String idNumber;
