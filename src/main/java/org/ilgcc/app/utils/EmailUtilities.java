c7a21ea6d1fca8b5158f4d89857d15ddfaa884a5
Chibuisi Enyia <cenyia@codeforamerica.org>
2024-09-25 17:17:28 -0500

    CCAP-342 Email by button push (#616)

---
+++ src/main/java/org/ilgcc/app/utils/EmailUtilities.java
@@ -0,0 +1,20 @@
+package org.ilgcc.app.utils;
+
+import static java.util.stream.Collectors.joining;
+
+import formflow.library.data.Submission;
+import java.util.HashMap;
+import java.util.Map;
+import org.yaml.snakeyaml.util.UriEncoder;
+
+public class EmailUtilities {
+  public static String generateWellFormedEmail (String recipient, String subject, String emailBody){
+  Map<String, String> params = new HashMap<>();
+  params.put("subject", subject);
+  params.put("body", emailBody);
+
+  return params.keySet().stream()
+      .map(key -> key + "=" + UriEncoder.encode(params.get(key)))
+      .collect(joining("&", String.format("mailto:%s?", recipient), ""));
+  }
+}
