2c08db68831b27b23345caf08c63612503311096
chibuisienyia <cenyia@codeforamerica.org>
2024-09-27 05:46:00 -0500

    CCAP-343 Update test links

---
+++ src/main/java/org/ilgcc/app/utils/TextUtilities.java
@@ -0,0 +1,28 @@
+package org.ilgcc.app.utils;
+
+import static java.util.stream.Collectors.joining;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.yaml.snakeyaml.util.UriEncoder;
+
+public class TextUtilities {
+  public static String generateWellFormedTextMessage (String recipient, String subject, String textBody){
+  Map<String, String> params = new HashMap<>();
+  params.put("subject", subject);
+  params.put("body", textBody);
+
+
+  return params.keySet().stream()
+      .map(key -> key + "=" + UriEncoder.encode(params.get(key)))
+      .collect(joining("&", String.format("sms://%s;?", reformatRecipientNumber(recipient)), ""));
+  }
+  private static String reformatRecipientNumber(String recipient) {
+    String formattedNumber = recipient.replaceAll("\\D", "");
+    if (formattedNumber.isEmpty()) {
+      return formattedNumber;
+    } else {
+      return String.format("+1%s", formattedNumber);
+    }
+  }
+}
