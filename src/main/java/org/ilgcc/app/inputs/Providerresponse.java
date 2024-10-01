822721ed256300ad219297608228a0d14a0770bb
Ana Medrano <amedrano@codeforamerica.org>
2024-10-01 10:00:29 -0700

    Completes submit-completion screen

--- src/main/java/org/ilgcc/app/inputs/Providerresponse.java
+++ src/main/java/org/ilgcc/app/inputs/Providerresponse.java
@@ -2,7 +2,6 @@ package org.ilgcc.app.inputs;
 
 import formflow.library.data.FlowInputs;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 
 public class Providerresponse extends FlowInputs {
     private String clientApplicationId;
