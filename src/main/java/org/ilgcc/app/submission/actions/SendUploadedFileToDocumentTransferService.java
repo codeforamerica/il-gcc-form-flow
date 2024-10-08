9202aad76cece2eda69b1df5d2f3b0890d40818a
Ana Medrano <amedrano@codeforamerica.org>
2024-10-08 16:21:31 -0700

    Implement wait for provider response feature flag in current transmission case

--- src/main/java/org/ilgcc/app/submission/actions/SendUploadedFileToDocumentTransferService.java
+++ src/main/java/org/ilgcc/app/submission/actions/SendUploadedFileToDocumentTransferService.java
@@ -23,21 +23,25 @@ public class SendUploadedFileToDocumentTransferService implements Action {
     private final UserFileRepositoryService userFileRepositoryService;
     private final UploadedDocumentTransmissionJob uploadedDocumentTransmissionJob;
     private final S3PresignService s3PresignService;
-    
+
     private final String enableBackgroundJobs;
+
+    private final String waitForProviderResponseFlag;
 
     public SendUploadedFileToDocumentTransferService(UserFileRepositoryService userFileRepositoryService,
             UploadedDocumentTransmissionJob uploadedDocumentTransmissionJob, S3PresignService s3PresignService,
-            @Value("${il-gcc.dts.enable-background-jobs}") String enableBackgroundJobs) {
+            @Value("${il-gcc.dts.enable-background-jobs}") String enableBackgroundJobs,
+            @Value("${il-gcc.dts.wait-for-provider-response}") String waitForProviderResponseFlag) {
         this.userFileRepositoryService = userFileRepositoryService;
         this.uploadedDocumentTransmissionJob = uploadedDocumentTransmissionJob;
         this.s3PresignService = s3PresignService;
         this.enableBackgroundJobs = enableBackgroundJobs;
+        this.waitForProviderResponseFlag=waitForProviderResponseFlag;
     }
 
     @Override
     public void run(Submission submission) {
-        if (!enableBackgroundJobs.equals("false")) {
+        if (enableBackgroundJobs.equals("true") && waitForProviderResponseFlag.equals("false")) {
             log.info("Sending uploaded files to document transfer service for submission with ID: {}", submission.getId());
             List<UserFile> userFiles = userFileRepositoryService.findAllBySubmission(submission);
             if (!userFiles.isEmpty()) {
