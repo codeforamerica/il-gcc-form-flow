a69c16541f3f93d8570e95671a029aaaacee8b3b
Ana Medrano <amedrano@codeforamerica.org>
2024-10-17 13:29:16 -0700

    Implement county and zip code minus skip button

---
+++ src/main/java/org/ilgcc/app/utils/CountyOption.java
@@ -0,0 +1,33 @@
+package org.ilgcc.app.utils;
+
+public enum CountyOption implements InputOption {
+
+    CARROLL("Carroll"),
+    DEKALB("DeKalb"),
+    LEE("Lee"),
+    MCHENRY("McHenry"),
+    OGLE("Ogle"),
+    WHITESIDE("Whiteside");
+
+
+    private final String value;
+
+    CountyOption(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.value;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getHelpText() {
+        return null;
+    }
+}
