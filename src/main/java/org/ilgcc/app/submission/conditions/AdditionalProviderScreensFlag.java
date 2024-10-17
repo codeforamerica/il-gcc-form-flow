96c1c1678987e9e5406baf6c110f3abd930628b7
Ana Medrano <amedrano@codeforamerica.org>
2024-10-17 11:21:19 -0700

    Create skeleton of new screens and add feature flag

---
+++ src/main/java/org/ilgcc/app/submission/conditions/AdditionalProviderScreensFlag.java
@@ -0,0 +1,25 @@
+package org.ilgcc.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is a false-Condition to skip the pages that have not yet been implemented.
+ */
+@Component
+public class AdditionalProviderScreensFlag implements Condition {
+
+  private final String revealAdditionalProviderScreens;
+
+  public AdditionalProviderScreensFlag(
+          @Value("${il-gcc.dts.reveal-additional-provider-screens}") String waitForProviderResponseFlag) {
+    this.revealAdditionalProviderScreens = waitForProviderResponseFlag;
+  }
+
+  @Override
+  public Boolean run(Submission submission) {
+    return revealAdditionalProviderScreens.equalsIgnoreCase("true");
+  }
+}
