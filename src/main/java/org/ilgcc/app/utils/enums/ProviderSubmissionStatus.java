53cadf5786b97672fa7a16cac3088e81217c1df4
Ana Medrano <amedrano@codeforamerica.org>
2024-10-23 14:38:08 -0700

    Updated provider name in submit-start screen and updated screen to match design

--- src/main/java/org/ilgcc/app/utils/enums/ProviderSubmissionStatus.java
+++ src/main/java/org/ilgcc/app/utils/enums/ProviderSubmissionStatus.java
@@ -6,24 +6,22 @@ import lombok.Getter;
 public enum ProviderSubmissionStatus {
 
     ACTIVE("caring", "provider-response-submit-start.active.header", "provider-response-submit-start.active.notice",
-            "provider-response-submit-start.active.button", ""),
+            "provider-response-submit-start.active.button"),
     EXPIRED("orangeClock", "provider-response-submit-start.expired.header", "provider-response-submit-start.expired.notice",
-            "provider-response-submit-start.expired.button", "provider-response-submit-start.expired.subtext"),
+            "provider-response-submit-start.expired.button"),
     RESPONDED("docValidation", "provider-response-submit-start.responded.header",
-            "provider-response-submit-start.responded.notice", "provider-response-submit-start.responded.button", "");
+            "provider-response-submit-start.responded.notice", "provider-response-submit-start.responded.button");
 
     private final String icon;
     private final String headerLabel;
     private final String noticeLabel;
     private final String buttonLabel;
-    private final String subtextLabel;
 
-    ProviderSubmissionStatus(String icon, String headerLabel, String noticeLabel, String buttonLabel, String subtextLabel) {
+    ProviderSubmissionStatus(String icon, String headerLabel, String noticeLabel, String buttonLabel) {
         this.icon = icon;
         this.headerLabel = headerLabel;
         this.noticeLabel = noticeLabel;
         this.buttonLabel = buttonLabel;
-        this.subtextLabel = subtextLabel;
     }
 }
 
