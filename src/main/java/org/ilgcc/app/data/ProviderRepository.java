584b684e6a2a2952ff5c29aa41b32551273f24b2
Marc Perlman <mperlman@codeforamerica.org>
2024-10-24 15:17:32 -0500

    [CCAP-424]

--- src/main/java/org/ilgcc/app/data/ProviderRepository.java
+++ src/main/java/org/ilgcc/app/data/ProviderRepository.java
@@ -1,11 +1,8 @@
-package org.ilgcc.app.data;
+package formflow.library.data;
 
-import formflow.library.data.Submission;
 import java.math.BigInteger;
-import java.util.List;
-import java.util.UUID;
+import org.ilgcc.app.data.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
