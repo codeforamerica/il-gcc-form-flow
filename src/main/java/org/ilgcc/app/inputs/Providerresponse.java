90351836154e14cde0e645549f7ed4b59ba454c8
Ana Medrano <amedrano@codeforamerica.org>
2024-10-30 14:06:53 -0700

    provider phone and email have validation if they are added by the provider

--- src/main/java/org/ilgcc/app/inputs/Providerresponse.java
+++ src/main/java/org/ilgcc/app/inputs/Providerresponse.java
@@ -1,6 +1,9 @@
 package org.ilgcc.app.inputs;
 
 import formflow.library.data.FlowInputs;
+import formflow.library.data.annotations.Phone;
+import formflow.library.utils.RegexUtils;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class Providerresponse extends FlowInputs {
@@ -36,8 +39,10 @@ public class Providerresponse extends FlowInputs {
     
     @NotBlank
     private String providerResponseServiceZipCode;
-    
+
+    @Phone(message = "{errors.invalid-phone-number}")
     private String providerResponseContactPhoneNumber;
-    
+
+    @Email(regexp = RegexUtils.EMAIL_REGEX, message = "{errors.invalid-email}")
     private String providerResponseContactEmail;
 }
