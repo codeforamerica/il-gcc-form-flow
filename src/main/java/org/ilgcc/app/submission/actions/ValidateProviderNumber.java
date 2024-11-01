866059a04d01c670c4c90126372db83974ff656f
Marc P <mperlman@codeforamerica.org>
2024-11-01 12:59:26 -0500

    Marc db call (#762)

--- src/main/java/org/ilgcc/app/submission/actions/ValidateProviderNumber.java
+++ src/main/java/org/ilgcc/app/submission/actions/ValidateProviderNumber.java
@@ -8,14 +8,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
 import org.ilgcc.app.data.ProviderRepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.core.env.Environment;
 
-@Slf4j
 @Component
 public class ValidateProviderNumber implements Action {
 
@@ -25,6 +25,9 @@ public class ValidateProviderNumber implements Action {
     @Autowired
     ProviderRepositoryService providerRepositoryService;
 
+    @Value("${il-gcc.validate-provider-id}")
+    boolean validateProviderId;
+
     private final String PROVIDER_NUMBER = "providerResponseProviderNumber";
 
     @Override
@@ -32,7 +35,7 @@ public class ValidateProviderNumber implements Action {
         Map<String, List<String>> errorMessages = new HashMap<>();
         String inputValue = (String) formSubmission.getFormData().get(PROVIDER_NUMBER);
 
-        if (!providerRepositoryService.isProviderIdValid(inputValue)) {
+        if (validateProviderId && !providerRepositoryService.isProviderIdValid(inputValue)) {
             Locale locale = LocaleContextHolder.getLocale();
             errorMessages.put(PROVIDER_NUMBER,
                     List.of(messageSource.getMessage("provider-response-ccap-registration.error.invalid-number", null, locale)));
