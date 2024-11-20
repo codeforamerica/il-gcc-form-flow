9f18f2eaa99382a7c9ad2577b8d3cb1df3baaaf1
Marc Perlman <mperlman@codeforamerica.org>
2024-11-20 14:51:25 -0600

    [CCAP-452]

---
+++ src/main/java/org/ilgcc/app/submission/conditions/IsPaidByCCAP.java
@@ -0,0 +1,15 @@
+package org.ilgcc.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IsPaidByCCAP implements Condition {
+
+    @Override
+    public Boolean run(Submission submission) {
+        return submission != null && submission.getInputData() != null && "true".equals(
+                submission.getInputData().get("providerPaidCcap"));
+    }
+}
