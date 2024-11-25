9fa5bb53b8049c6c8bcc88236f9ad8017a4808f4
Ana Medrano <amedrano@codeforamerica.org>
2024-11-25 15:14:35 -0800

    Clean up code

--- src/main/java/org/ilgcc/app/inputs/Providerresponse.java
+++ src/main/java/org/ilgcc/app/inputs/Providerresponse.java
@@ -5,8 +5,6 @@ import formflow.library.data.annotations.Phone;
 import formflow.library.utils.RegexUtils;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import java.util.List;
 
 public class Providerresponse extends FlowInputs {
 
