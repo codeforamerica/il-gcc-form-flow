c80a1f9edb4e7b7e74c55eb2145eb423c3bc8a41
Marc Perlman <mperlman@codeforamerica.org>
2024-12-02 15:33:52 -0600

    [CCAP-439]

--- src/main/java/org/ilgcc/app/data/ProviderRepository.java
+++ src/main/java/org/ilgcc/app/data/ProviderRepository.java
@@ -1,10 +1,14 @@
 package org.ilgcc.app.data;
 
+import formflow.library.data.Submission;
 import java.math.BigInteger;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, BigInteger> {
 
+    boolean existsByStatusInAndProviderId(List<String> statuses, BigInteger providerId);
 }
--- src/main/java/org/ilgcc/app/data/ProviderRepositoryService.java
+++ src/main/java/org/ilgcc/app/data/ProviderRepositoryService.java
@@ -1,6 +1,9 @@
 package org.ilgcc.app.data;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +14,10 @@ public class ProviderRepositoryService {
 
     ProviderRepository providerRepository;
 
+    private static final String STATUS_ACTIVE = "A";
+    private static final String STATUS_PENDING = "P";
+    private static final List<String> VALID_STATUSES = List.of(STATUS_ACTIVE, STATUS_PENDING);
+
     public ProviderRepositoryService(ProviderRepository providerRepository) {
         this.providerRepository = providerRepository;
     }
@@ -20,7 +27,7 @@ public class ProviderRepositoryService {
         if (!Objects.isNull(providerId) && !providerId.isEmpty()) {
             try {
                 BigInteger providerIdNumber = new BigInteger(providerId);
-                if (!providerRepository.existsById(providerIdNumber)) {
+                if (!providerRepository.existsByStatusInAndProviderId(VALID_STATUSES, providerIdNumber)) {
                     return false;
                 }
             } catch (NumberFormatException e) {
