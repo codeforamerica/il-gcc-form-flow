608f0c68c32f14e07519ace99bbc6d14a0223869
chibuisienyia <cenyia@codeforamerica.org>
2024-12-19 14:42:16 -0600

    CCAP-484 Fixed failed test

--- src/main/java/org/ilgcc/app/submission/actions/CheckClientSubmissionForProvider.java
+++ src/main/java/org/ilgcc/app/submission/actions/CheckClientSubmissionForProvider.java
@@ -60,7 +60,10 @@ public class CheckClientSubmissionForProvider implements Action {
                 Submission clientSubmissionInfo = clientSubmission.get();
                 submission.getInputData().put("shortCodeClientSubmissionId", clientSubmissionInfo.getId());
                 submissionRepositoryService.save(submission);
-                clientSubmissionInfo.getInputData().put("providerResponseSubmissionId", submission.getId());
+                if(!clientSubmissionInfo.getInputData().containsKey("providerResponseSubmissionId")) {
+                    clientSubmissionInfo.getInputData().put("providerResponseSubmissionId", submission.getId());
+                }
+
                 submissionRepositoryService.save(clientSubmissionInfo);
                 // Used to display the correct provider name, if available, for the first provider screen
                 httpSession.setAttribute(SESSION_KEY_SELECTED_PROVIDER_NAME, getProviderName(clientSubmissionInfo));
