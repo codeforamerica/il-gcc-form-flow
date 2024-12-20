f91fa3133bb3207fc3bd86db41aa1e9aa57c63a9
Ana Medrano <amedrano@codeforamerica.org>
2024-12-19 20:32:30 -0800

    Refactor provider type preparer

--- src/test/java/org/ilgcc/app/pdf/ProviderTypePreparerTest.java
+++ src/test/java/org/ilgcc/app/pdf/ProviderTypePreparerTest.java
@@ -9,6 +9,7 @@ import formflow.library.pdf.SubmissionField;
 import java.util.Map;
 import org.ilgcc.app.IlGCCApplication;
 import org.ilgcc.app.utils.SubmissionTestBuilder;
+import org.ilgcc.app.utils.enums.ProviderType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,148 +33,159 @@ public class ProviderTypePreparerTest {
     private Submission providerSubmission;
 
     @Test
-    public void shouldSelectLicensedDayCareCenter(){
+    public void doesNotSetProviderTypeIfNull() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","true")
-            .with("providerLicensedCareLocation", "childCareCenter")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
+        assertThat(result.get("providerType")).isEqualTo(null);
+    }
+    @Test
+    public void shouldSelectLicensedDayCareCenter() {
+        providerSubmission = new SubmissionTestBuilder()
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSED_DAY_CARE_CENTER.name())
+                .build();
+        submissionRepositoryService.save(providerSubmission);
+
+        familySubmission = new SubmissionTestBuilder()
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
         assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "LICENSED_DAY_CARE_CENTER_760", null));
     }
+
     @Test
-    public void shouldSelectLicensedDayCareHome(){
+    public void shouldSelectLicensedDayCareHome() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","true")
-            .with("providerLicensedCareLocation", "childCareHome")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSED_DAY_CARE_HOME.name())
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
         assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "LICENSED_DAY_CARE_HOME_762", null));
     }
+
     @Test
-    public void shouldSelectLicensedGroupDayCare(){
+    public void shouldSelectLicensedGroupDayCare() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","true")
-            .with("providerLicensedCareLocation", "groupChildCareHome")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSED_GROUP_CHILD_CARE_HOME.name())
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
-        assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "LICENSED_GROUP_DAY_CARE_HOME_763", null));
-    }
-    @Test
-    public void shouldSelectDayCareCenterExemptFromLicensing(){
-        providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","false")
-            .with("providerLicenseExemptType", "License-exempt")
-            .build();
-        submissionRepositoryService.save(providerSubmission);
-
-        familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
-        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
-        assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "DAY_CARE_CENTER_EXEMPT_FROM_LICENSING_761", null));
+        assertThat(result.get("providerType")).isEqualTo(
+                new SingleField("providerType", "LICENSED_GROUP_DAY_CARE_HOME_763", null));
     }
 
     @Test
-    public void shouldSelectCareByRelativeInChildCareProvidersHome(){
+    public void shouldSelectDayCareCenterExemptFromLicensing() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","false")
-            .with("providerLicenseExemptType", "Self")
-            .with("providerLicenseExemptCareLocation", "Providers home")
-            .with("providerLicenseExemptRelationship", "Relative")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSE_EXEMPT_CHILD_CARE_CENTER.name())
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
-        assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "CARE_BY_RELATIVE_IN_CHILD_CARE_PROVIDERS_HOME_765", null));
+        assertThat(result.get("providerType")).isEqualTo(
+                new SingleField("providerType", "DAY_CARE_CENTER_EXEMPT_FROM_LICENSING_761", null));
     }
+
     @Test
-    public void shouldSelectCareByNonRelativeInChildCareProvidersHome(){
+    public void shouldSelectCareByRelativeInChildCareProvidersHome() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","false")
-            .with("providerLicenseExemptType", "Self")
-            .with("providerLicenseExemptCareLocation", "Providers home")
-            .with("providerLicenseExemptRelationship", "Not related")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSE_EXEMPT_RELATIVE_IN_PROVIDER_HOME.name())
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
-        assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "CARE_BY_NON_RELATIVE_IN_CHILD_CARE_PROVIDERS_HOME_764", null));
+        assertThat(result.get("providerType")).isEqualTo(
+                new SingleField("providerType", "CARE_BY_RELATIVE_IN_CHILD_CARE_PROVIDERS_HOME_765", null));
     }
+
     @Test
-    public void shouldSelectCareByRelativeInChildsHome(){
+    public void shouldSelectCareByNonRelativeInChildCareProvidersHome() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","false")
-            .with("providerLicenseExemptType", "Self")
-            .with("providerLicenseExemptCareLocation", "Childs home")
-            .with("providerLicenseExemptRelationship", "Relative")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSE_EXEMPT_NONRELATIVE_IN_PROVIDER_HOME.name())
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
-        assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "CARE_BY_RELATIVE_IN_CHILDS_HOME_767", null));
+        assertThat(result.get("providerType")).isEqualTo(
+                new SingleField("providerType", "CARE_BY_NON_RELATIVE_IN_CHILD_CARE_PROVIDERS_HOME_764", null));
     }
+
     @Test
-    public void shouldSelectCareByNonRelativeInChildsHome(){
+    public void shouldSelectCareByRelativeInChildsHome() {
         providerSubmission = new SubmissionTestBuilder()
-            .withFlow("providerresponse")
-            .withProviderSubmissionData()
-            .with("providerCurrentlyLicensed","false")
-            .with("providerLicenseExemptType", "Self")
-            .with("providerLicenseExemptCareLocation", "Childs home")
-            .with("providerLicenseExemptRelationship", "Not related")
-            .build();
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSE_EXEMPT_RELATIVE_IN_CHILDS_HOME.name())
+                .build();
         submissionRepositoryService.save(providerSubmission);
 
         familySubmission = new SubmissionTestBuilder()
-            .withFlow("gcc")
-            .with("providerResponseSubmissionId", providerSubmission.getId())
-            .build();
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
-        assertThat(result.get("providerType")).isEqualTo(new SingleField("providerType", "CARE_BY_NON_RELATIVE_IN_CHILDS_HOME_766", null));
+        assertThat(result.get("providerType")).isEqualTo(
+                new SingleField("providerType", "CARE_BY_RELATIVE_IN_CHILDS_HOME_767", null));
+    }
+
+    @Test
+    public void shouldSelectCareByNonRelativeInChildsHome() {
+        providerSubmission = new SubmissionTestBuilder()
+                .withFlow("providerresponse")
+                .withProviderSubmissionData()
+                .with("providerType", ProviderType.LICENSE_EXEMPT_NONRELATIVE_IN_CHILDS_HOME.name())
+                .build();
+        submissionRepositoryService.save(providerSubmission);
+
+        familySubmission = new SubmissionTestBuilder()
+                .withFlow("gcc")
+                .with("providerResponseSubmissionId", providerSubmission.getId())
+                .build();
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(familySubmission, null);
+        assertThat(result.get("providerType")).isEqualTo(
+                new SingleField("providerType", "CARE_BY_NON_RELATIVE_IN_CHILDS_HOME_766", null));
     }
 }
