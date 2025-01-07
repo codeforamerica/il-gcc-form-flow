2473bdc09e0ac1a23625421761239d34f1c5ef67
Marc Perlman <mperlman@codeforamerica.org>
2025-01-07 11:01:13 -0600

    [CCAP-493]

--- src/main/java/org/ilgcc/app/config/SendGridWebhookSecurityConfig.java
+++ src/main/java/org/ilgcc/app/config/SendGridWebhookSecurityConfig.java
@@ -1,9 +1,9 @@
-package org.ilgcc.app.email;
+package org.ilgcc.app.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,9 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SendGridWebhookSecurityConfig {
 
-    @Bean("securityFilterChain")
-    @Order(1)
-    SecurityFilterChain customSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Bean
+    @ConditionalOnBean(name = "securityFilterChain")
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/sendgrid-webhook"))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/sendgrid-webhook").permitAll().anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable);
