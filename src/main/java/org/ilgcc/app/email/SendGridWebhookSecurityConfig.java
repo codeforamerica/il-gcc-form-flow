9d2116a2e81a52fdce2e22c1a0d0e4128892a4c3
Marc Perlman <mperlman@codeforamerica.org>
2025-01-07 10:31:59 -0600

    [CCAP-493]

--- src/main/java/org/ilgcc/app/email/SendGridWebhookSecurityConfig.java
+++ src/main/java/org/ilgcc/app/email/SendGridWebhookSecurityConfig.java
@@ -1,9 +1,9 @@
 package org.ilgcc.app.email;
 
-import formflow.library.config.SecurityConfigurationBase;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SendGridWebhookSecurityConfig {
 
     @Bean("securityFilterChain")
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/sendgrid-webhook")).formLogin(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
