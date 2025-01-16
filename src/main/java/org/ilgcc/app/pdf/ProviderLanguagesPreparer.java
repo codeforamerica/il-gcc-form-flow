95fc09a692a07fd481af92f1a3051df3bff2396c
chibuisienyia <cenyia@codeforamerica.org>
2025-01-16 13:30:34 -0600

    Added ProviderLanguagesPreparer and updated IL-CCAP-Form.pdf

---
+++ src/main/java/org/ilgcc/app/pdf/ProviderLanguagesPreparer.java
@@ -0,0 +1,73 @@
+package org.ilgcc.app.pdf;
+
+import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepositoryService;
+import formflow.library.pdf.PdfMap;
+import formflow.library.pdf.SingleField;
+import formflow.library.pdf.SubmissionField;
+import formflow.library.pdf.SubmissionFieldPreparer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.ilgcc.app.utils.ProviderSubmissionUtilities;
+import org.ilgcc.app.utils.enums.ProviderLanguagesOffered;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProviderLanguagesPreparer implements SubmissionFieldPreparer {
+
+    @Autowired
+    SubmissionRepositoryService submissionRepositoryService;
+    @Autowired
+    private ProviderSubmissionUtilities providerSubmissionUtilities;
+
+    @Override
+    public Map<String, SubmissionField> prepareSubmissionFields(Submission familySubmission, PdfMap pdfMap) {
+        var results = new HashMap<String, SubmissionField>();
+        Optional<Submission> providerSubmission = providerSubmissionUtilities.getProviderSubmissionFromId(
+                submissionRepositoryService, familySubmission);
+        if (providerSubmission.isEmpty()) {
+            return results;
+        }
+
+        var providerInputData = providerSubmission.get().getInputData();
+        List<String> otherLanguages = new ArrayList<>();
+        if (providerInputData.containsKey("providerLanguagesOffered[]")) {
+            List<String> providerLanguagesOffered = (List<String>) providerInputData.get("providerLanguagesOffered[]");
+            if(providerLanguagesOffered.isEmpty() || providerLanguagesOffered.contains("NONE")){
+                return results;
+            }
+
+            for (ProviderLanguagesOffered provider : ProviderLanguagesOffered.values()) {
+                if(providerLanguagesOffered.contains(provider.getValue())){
+                     if(!provider.getProviderLanguagePdfFieldValue().equalsIgnoreCase("Other")){
+                            String pdfMapInputName = String.format("providerLanguage%s", provider.getValue());
+                            results.put(pdfMapInputName, new SingleField(pdfMapInputName, "true", null));
+                     }
+                     if(provider.getProviderLanguagePdfFieldValue().equalsIgnoreCase("Other")){
+                         results.put("providerLanguageOther", new SingleField("providerLanguageOther", "true", null));
+                         otherLanguages.add(provider.getProviderLanguageOtherDetailPdfFieldValue());
+                     }
+                }
+            }
+
+            if(providerLanguagesOffered.contains("other")){
+                if (otherLanguages.isEmpty()) {
+                    results.put("providerLanguageOther", new SingleField("providerLanguageOther", "true", null));
+                    results.put("providerLanguageOtherDetail", new SingleField("providerLanguageOtherDetail", (String) providerInputData.getOrDefault("providerLanguagesOffered_other", ""), null));
+                }else{
+                    otherLanguages.add(providerInputData.getOrDefault("providerLanguagesOffered_other", "").toString());
+                }
+            }
+
+            if(!otherLanguages.isEmpty()){
+                results.put("providerLanguageOtherDetail", new SingleField("providerLanguageOtherDetail", otherLanguages.toString(), null));
+            }
+            return results;
+        }
+        return results;
+    }
+}
--- src/main/java/org/ilgcc/app/utils/enums/ProviderLanguagesOffered.java
+++ src/main/java/org/ilgcc/app/utils/enums/ProviderLanguagesOffered.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 
 public enum ProviderLanguagesOffered {
-  ENGLISH("registration-service-languages.provider-languages.english", "english", "ENGLISH", ""),
-  SPANISH("registration-service-languages.provider-languages.spanish" ,"spanish", "SPANISH", ""),
-  POLISH("registration-service-languages.provider-languages.polish","polish", "POLISH", ""),
-  CHINESE("registration-service-languages.provider-languages.chinese" ,"chinese", "CHINESE", ""),
-  TAGALOG("registration-service-languages.provider-languages.tagalog", "tagalog","OTHER",
+  ENGLISH("registration-service-languages.provider-languages.english", "English", "ENGLISH", ""),
+  SPANISH("registration-service-languages.provider-languages.spanish" ,"Spanish", "SPANISH", ""),
+  POLISH("registration-service-languages.provider-languages.polish","Polish", "POLISH", ""),
+  CHINESE("registration-service-languages.provider-languages.chinese" ,"Chinese", "CHINESE", ""),
+  TAGALOG("registration-service-languages.provider-languages.tagalog", "Tagalog","OTHER",
       "Tagalog (including Filipino)"),
-  HINDI("registration-service-languages.provider-languages.hindi", "hindi","OTHER", "Hindi"),
-  ARABIC("registration-service-languages.provider-languages.arabic", "arabic","OTHER", "Arabic");
+  HINDI("registration-service-languages.provider-languages.hindi", "Hindi","OTHER", "Hindi"),
+  ARABIC("registration-service-languages.provider-languages.arabic", "Arabic","OTHER", "Arabic");
   private final String label;
   private final String value;
   private final String providerLanguagePdfFieldValue;
@@ -24,6 +24,5 @@ public enum ProviderLanguagesOffered {
     this.providerLanguagePdfFieldValue = providerLanguagePdfFieldValue;
     this.providerLanguageOtherDetailPdfFieldValue = providerLanguageOtherDetailPdfFieldValue;
   }
-
 }
 
