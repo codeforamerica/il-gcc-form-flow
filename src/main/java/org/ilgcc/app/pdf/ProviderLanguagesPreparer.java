02dcf245f47a8e75e73052134b45720c4e23f9b0
chibuisienyia <cenyia@codeforamerica.org>
2025-01-17 16:45:04 -0600

    Removes else and fixes whitespace issues

--- src/main/java/org/ilgcc/app/pdf/ProviderLanguagesPreparer.java
+++ src/main/java/org/ilgcc/app/pdf/ProviderLanguagesPreparer.java
@@ -15,47 +15,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProviderLanguagesPreparer extends ProviderSubmissionFieldPreparer {
 
-    @Override
-    public Map<String, SubmissionField> prepareSubmissionFields(Submission familySubmission, PdfMap pdfMap) {
-        var results = new HashMap<String, SubmissionField>();
-        Optional<Submission> providerSubmission = getProviderSubmission(familySubmission);
-        if (providerSubmission.isEmpty()) {
-            return results;
-        }
-
-        var providerInputData = providerSubmission.get().getInputData();
-        List<String> otherLanguages = new ArrayList<>();
-        if (providerInputData.containsKey("providerLanguagesOffered[]")) {
-            List<String> providerLanguagesOffered = (List<String>) providerInputData.get("providerLanguagesOffered[]");
-            if(providerLanguagesOffered.isEmpty() || providerLanguagesOffered.contains("NONE")){
-                return results;
-            }
-
-            for (ProviderLanguagesOffered provider : ProviderLanguagesOffered.values()) {
-                if(providerLanguagesOffered.contains(provider.getValue())){
-                     if(!provider.getProviderLanguagePdfFieldValue().equalsIgnoreCase("Other")){
-                            String pdfMapInputName = String.format("providerLanguage%s", provider.getValue());
-                            results.put(pdfMapInputName, new SingleField(pdfMapInputName, "true", null));
-                     }
-                     if(provider.getProviderLanguagePdfFieldValue().equalsIgnoreCase("Other")){
-                         results.put("providerLanguageOther", new SingleField("providerLanguageOther", "true", null));
-                         otherLanguages.add(provider.getProviderLanguageOtherDetailPdfFieldValue());
-                     }
-                }
-            }
-
-            if(providerLanguagesOffered.contains("other")){
-                if (otherLanguages.isEmpty()) {
-                    results.put("providerLanguageOther", new SingleField("providerLanguageOther", "true", null));
-                }
-            }
-
-            if(!otherLanguages.isEmpty()){
-                String otherLanguagesList = String.join(", ", otherLanguages);
-                results.put("providerLanguageOtherDetail", new SingleField("providerLanguageOtherDetail", otherLanguagesList, null));
-            }
-            return results;
-        }
-        return results;
+  @Override
+  public Map<String, SubmissionField> prepareSubmissionFields(Submission familySubmission, PdfMap pdfMap) {
+    var results = new HashMap<String, SubmissionField>();
+    Optional<Submission> providerSubmission = getProviderSubmission(familySubmission);
+    if (providerSubmission.isEmpty()) {
+      return results;
     }
+
+    var providerInputData = providerSubmission.get().getInputData();
+    List<String> otherLanguages = new ArrayList<>();
+    if (providerInputData.containsKey("providerLanguagesOffered[]")) {
+      List<String> providerLanguagesOffered = (List<String>) providerInputData.get("providerLanguagesOffered[]");
+      if (providerLanguagesOffered.isEmpty() || providerLanguagesOffered.contains("NONE")) {
+        return results;
+      }
+
+      for (ProviderLanguagesOffered provider : ProviderLanguagesOffered.values()) {
+        if (providerLanguagesOffered.contains(provider.getValue())) {
+          if (!provider.getProviderLanguagePdfFieldValue().equalsIgnoreCase("Other")) {
+            String pdfMapInputName = String.format("providerLanguage%s", provider.getValue());
+            results.put(pdfMapInputName, new SingleField(pdfMapInputName, "true", null));
+          } else {
+            results.put("providerLanguageOther", new SingleField("providerLanguageOther", "true", null));
+            otherLanguages.add(provider.getProviderLanguageOtherDetailPdfFieldValue());
+          }
+        }
+      }
+
+      if (providerLanguagesOffered.contains("other")) {
+        if (otherLanguages.isEmpty()) {
+          results.put("providerLanguageOther", new SingleField("providerLanguageOther", "true", null));
+        }
+      }
+
+      if (!otherLanguages.isEmpty()) {
+        String otherLanguagesList = String.join(", ", otherLanguages);
+        results.put("providerLanguageOtherDetail", new SingleField("providerLanguageOtherDetail", otherLanguagesList, null));
+      }
+      return results;
+    }
+    return results;
+  }
 }
