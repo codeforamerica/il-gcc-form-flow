58210d7dbf994587d326780af86b9697203ae399
Ana Medrano <amedrano@codeforamerica.org>
2025-01-23 13:58:55 -0800

    Implement making ccrr name dynamic

--- src/main/java/org/ilgcc/app/submission/router/BasicApplicationRouterService.java
+++ src/main/java/org/ilgcc/app/submission/router/BasicApplicationRouterService.java
@@ -2,12 +2,11 @@ package org.ilgcc.app.submission.router;
 
 import java.util.Optional;
 import org.ilgcc.app.utils.ZipcodeOption;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BasicApplicationRouterService implements ApplicationRouterService {
-
-
     @Override
     public Optional<String> getOrganizationIdByZipCode(String zipCode) {
         final String truncatedZip = zipCode.substring(0, 5);
--- src/main/java/org/ilgcc/app/submission/router/CCRR.java
+++ src/main/java/org/ilgcc/app/submission/router/CCRR.java
@@ -17,21 +17,24 @@ public class CCRR {
     protected final String slug;
 
 
-    static public CCRR FOUR_C = new CCRR("4C: Community Coordinated Child Care", "56522729391679", "2", "BB", "4c-ccap-apps");
-    static public CCRR PROJECT_CHILD = new CCRR("Project CHILD", "59522729391675", "15", "QQ", "project-child-ccap-apps");
-    static public CCRR ILLINOIS_ACTION = new CCRR("Illinois Action for Children", "47522729391670", "6", "GG",
+    private static final CCRR FOUR_C = new CCRR("4C: Community Coordinated Child Care", "56522729391679", "2", "BB", "4c-ccap-apps");
+    private static final CCRR PROJECT_CHILD = new CCRR("Project CHILD", "59522729391675", "15", "QQ", "project-child-ccap-apps");
+    private static final CCRR ILLINOIS_ACTION = new CCRR("Illinois Action for Children", "47522729391670", "6", "GG",
             "illinois-action-for-children-ccap-apps");
 
-    static private final Map<String, CCRR> CCRR_MAP = new HashMap<>();
+    private static final Map<String, CCRR> CCRR_MAP_BY_CASELOAD = new HashMap<>();
+    private static final Map<String, CCRR> CCRR_MAP_BY_ORGANIZATIONAL_ID = new HashMap<>();
 
     static {
         for (CCRR ccrr : CCRR.values()) {
-            CCRR_MAP.put(ccrr.caseloadCode, ccrr);
+            CCRR_MAP_BY_CASELOAD.put(ccrr.caseloadCode, ccrr);
         }
     }
 
-    private static List<CCRR> values() {
-        return List.of(FOUR_C, PROJECT_CHILD, ILLINOIS_ACTION);
+    static {
+        for (CCRR ccrr : CCRR.values()) {
+            CCRR_MAP_BY_ORGANIZATIONAL_ID.put(ccrr.organizationId, ccrr);
+        }
     }
 
     public CCRR(String name, String organizationId, String SDA, String caseloadCode, String slug) {
@@ -42,13 +45,25 @@ public class CCRR {
         this.slug = slug;
     }
 
-    public static Optional<CCRR> findCCRRByCaseLoadCode(String caseloadCode) {
-        boolean caseloadPresent = CCRR_MAP.containsKey(caseloadCode);
-        return caseloadPresent ? Optional.of(CCRR_MAP.get(caseloadCode)) : Optional.empty();
+    private static List<CCRR> values() {
+        return List.of(FOUR_C, PROJECT_CHILD, ILLINOIS_ACTION);
     }
 
+    private static Optional<CCRR> findCCRRByCaseLoadCode(String caseloadCode) {
+        final boolean caseloadPresent = CCRR_MAP_BY_CASELOAD.containsKey(caseloadCode);
+        return caseloadPresent ? Optional.of(CCRR_MAP_BY_CASELOAD.get(caseloadCode)) : Optional.empty();
+    }
+
+    private static Optional<CCRR> findCCRRByOrganizationalId(String organizationId) {
+        final boolean orgIdPresent = CCRR_MAP_BY_ORGANIZATIONAL_ID.containsKey(organizationId);
+        return orgIdPresent ? Optional.of(CCRR_MAP_BY_ORGANIZATIONAL_ID.get(organizationId)) : Optional.empty();
+    }
     public static Optional<String> findOrgIdByCaseLoadCode(String caseloadCode) {
-        Optional<CCRR> ccrr = findCCRRByCaseLoadCode(caseloadCode);
+        final Optional<CCRR> ccrr = findCCRRByCaseLoadCode(caseloadCode);
         return ccrr.isPresent() ? Optional.of(ccrr.get().organizationId) : Optional.empty();
+    }
+    public static String findCCRRNameByOrganizationalId(String organizationId) {
+        final Optional<CCRR> ccrr = findCCRRByOrganizationalId(organizationId);
+        return ccrr.isPresent() ? ccrr.get().name : "";
     }
 }
