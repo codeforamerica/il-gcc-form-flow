90fe9a927001fa7276289e3b3693ea3f42e513e6
Abhinav Nahar <anahar@codeforamerica.org>
2025-02-06 12:09:45 -0800

    CCAP-636, removed unused imports

--- src/main/java/org/ilgcc/app/data/ProviderRepository.java
+++ src/main/java/org/ilgcc/app/data/ProviderRepository.java
@@ -3,7 +3,6 @@ package org.ilgcc.app.data;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
