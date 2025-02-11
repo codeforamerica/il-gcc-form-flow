28aa3dbf1ca6c55dd9f7ce9144620b1162e76014
Ana Medrano Fernandez <8609011+analoo@users.noreply.github.com>
2025-02-12 08:51:01 +1030

    [CCAP-78] - map applicant county (#1141)

--- src/main/java/org/ilgcc/app/data/CountyRepository.java
+++ src/main/java/org/ilgcc/app/data/CountyRepository.java
@@ -1,9 +1,12 @@
 package org.ilgcc.app.data;
 
+import java.math.BigInteger;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CountyRepository extends JpaRepository<County, String> {
 
+    Optional<County> findByZipCode(BigInteger truncatedZip);
 }
