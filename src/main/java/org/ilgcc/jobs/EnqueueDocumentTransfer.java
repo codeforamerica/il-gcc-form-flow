3db272c0a35cb53a7d9c7e20ef07775985191954
Marc Perlman <mperlman@codeforamerica.org>
2025-02-11 08:51:22 -0600

    [CCAP-659] Storing and transferring converted files

--- src/main/java/org/ilgcc/jobs/EnqueueDocumentTransfer.java
+++ src/main/java/org/ilgcc/jobs/EnqueueDocumentTransfer.java
@@ -14,12 +14,17 @@ import org.ilgcc.app.file_transfer.S3PresignService;
 import org.ilgcc.app.utils.ByteArrayMultipartFile;
 import org.ilgcc.app.utils.SubmissionUtilities;
 import org.ilgcc.app.utils.enums.FileNameUtility;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Component
 public class EnqueueDocumentTransfer {
+
+    @Value("${form-flow.uploads.file-conversion.convert-to-pdf:false}")
+    private boolean convertUploadToPDF;
+
     private final static String CONTENT_TYPE = "application/pdf";
 
     public void enqueuePDFDocumentBySubmission(PdfService pdfService, CloudFileRepository cloudFileRepository,
@@ -55,9 +60,17 @@ public class EnqueueDocumentTransfer {
     }
 
     public void enqueueUploadedDocumentBySubmission(UserFileRepositoryService userFileRepositoryService,
-            UploadedDocumentTransmissionJob uploadedDocumentTransmissionJob, S3PresignService s3PresignService, Submission submission) {
+            UploadedDocumentTransmissionJob uploadedDocumentTransmissionJob, S3PresignService s3PresignService,
+            Submission submission) {
         log.info("Sending uploaded files to document transfer service for submission with ID: {}", submission.getId());
-        List<UserFile> userFiles = userFileRepositoryService.findAllBySubmission(submission);
+        List<UserFile> userFiles;
+        if (convertUploadToPDF) {
+            log.info("Finding all uploaded and converted files of type {}", CONTENT_TYPE);
+            userFiles = userFileRepositoryService.findAllOrderByOriginalName(submission, CONTENT_TYPE);
+        } else {
+            userFiles = userFileRepositoryService.findAllOrderedByOriginalName(submission);
+        }
+
         if (!userFiles.isEmpty()) {
             for (int i = 0; i < userFiles.size(); i++) {
                 UserFile userFile = userFiles.get(i);
