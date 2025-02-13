4fb7d250aad4977aa6746566f6d67130c26d0da7
Ana Medrano <amedrano@codeforamerica.org>
2025-02-13 15:17:32 -0800

    Basic Mailer Logic

---
+++ src/main/java/org/ilgcc/app/submission/actions/Mailer.java
@@ -0,0 +1,52 @@
+package org.ilgcc.app.submission.actions;
+
+import formflow.library.config.submission.Action;
+import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepositoryService;
+import java.util.Locale;
+import org.ilgcc.app.email.ILGCCEmail;
+import org.ilgcc.jobs.SendEmailJob;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+@Component
+abstract class Mailer implements Action {
+
+    static MessageSource messageSource;
+
+    static SubmissionRepositoryService submissionRepositoryService;
+
+    static SendEmailJob sendEmailJob;
+
+    protected static String EMAIL_SENT_STATUS_INPUT_NAME;
+    protected static String RECIPIENT_EMAIL_INPUT_NAME;
+
+    public Mailer(SendEmailJob sendEmailJob, SubmissionRepositoryService submissionRepositoryService,
+            MessageSource messageSource) {
+        this.sendEmailJob = sendEmailJob;
+        this.submissionRepositoryService = submissionRepositoryService;
+        this.messageSource = messageSource;
+    }
+
+
+    protected static String getSenderName(Locale locale) {
+        return messageSource.getMessage("email.sender-name", null, locale);
+    }
+
+    protected static String getRecipientEmail(Submission submission) {
+        return submission.getInputData().getOrDefault(RECIPIENT_EMAIL_INPUT_NAME, "").toString();
+    }
+
+    protected void sendEmail(ILGCCEmail email) {
+        sendEmailJob.enqueueSendEmailJob(email);
+    }
+
+    protected Boolean skipEmailSend(Submission submission) {
+        return submission.getInputData().getOrDefault(EMAIL_SENT_STATUS_INPUT_NAME, "false").equals("true");
+    }
+
+    private void updateEmailStatus(Submission submission) {
+        submission.getInputData().putIfAbsent(EMAIL_SENT_STATUS_INPUT_NAME, "true");
+        submissionRepositoryService.save(submission);
+    }
+}
