70cffb1245cc1dcb9a932277b8c90232e3e979a1
Marc Perlman <mperlman@codeforamerica.org>
2025-02-18 10:15:32 -0600

    [CCAP-626] Disable jobrunr server on web container and dashboard

--- scripts/jobrunr_dashboard_launcher.sh
+++ scripts/jobrunr_dashboard_launcher.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # script for starting jobrunr dashboard via Aptible, used in Procfile
 export JOBRUNR_DASHBOARD_ENABLED=true
+export JOBRUNR_SERVER_ENABLED=false
 exec  java -jar /opt/il-gcc/app.jar
--- scripts/webapp_launcher.sh
+++ scripts/webapp_launcher.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # script for starting the webapp, used in Procfile
+export JOBRUNR_SERVER_ENABLED=false
 exec java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/*.jar
