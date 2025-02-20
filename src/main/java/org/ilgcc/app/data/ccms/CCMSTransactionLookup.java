7304f7f17e949e074c0dd8205950af61671ce2a0
spokenbird <agonzalez@codeforamerica.org>
2025-02-20 11:09:52 -0800

    [CCAP-577] Update JSON properties for CCMSTransactionLookup

--- src/main/java/org/ilgcc/app/data/ccms/CCMSTransactionLookup.java
+++ src/main/java/org/ilgcc/app/data/ccms/CCMSTransactionLookup.java
@@ -1,14 +1,19 @@
 package org.ilgcc.app.data.ccms;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public class CCMSTransactionLookup {
-    
-    @JsonProperty("transactionId")
     private final UUID transactionId;
 
-    public CCMSTransactionLookup(UUID transactionId) {
+    @JsonCreator
+    public CCMSTransactionLookup(@JsonProperty("transactionId") UUID transactionId) {
         this.transactionId = transactionId;
+    }
+
+    @JsonProperty("transactionId")
+    public UUID getTransactionId() {
+        return transactionId;
     }
 }
