f881bf53b3f9d4949d76009439ba0f08d9697503
Marc Perlman <mperlman@codeforamerica.org>
2025-02-25 15:04:02 -0600

    [CCAP-679] In Spanish, confirm-deny-care screen shows English Text

--- src/main/java/org/ilgcc/app/submission/actions/FormatChildrenWhoNeedCareNames.java
+++ src/main/java/org/ilgcc/app/submission/actions/FormatChildrenWhoNeedCareNames.java
@@ -3,10 +3,13 @@ package org.ilgcc.app.submission.actions;
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
 import formflow.library.data.SubmissionRepositoryService;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.UUID;
 import org.ilgcc.app.utils.ProviderSubmissionUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,13 +17,19 @@ public class FormatChildrenWhoNeedCareNames implements Action {
     
     @Autowired
     SubmissionRepositoryService submissionRepositoryService;
-    
+
+    @Autowired
+    MessageSource messageSource;
+
     @Override
     public void run(Submission providerSubmission) {
         Optional<UUID> familySubmissionId = ProviderSubmissionUtilities.getFamilySubmissionId(providerSubmission);
         if (familySubmissionId.isPresent()) {
             Submission familySubmission = submissionRepositoryService.findById(familySubmissionId.get()).get();
-            String formattedChildrenNames = ProviderSubmissionUtilities.formatChildNamesAsCommaSeperatedList(familySubmission);
+            Locale locale = LocaleContextHolder.getLocale();
+            String joiner = messageSource.getMessage("general.and", null, locale);
+
+            String formattedChildrenNames = ProviderSubmissionUtilities.formatChildNamesAsCommaSeperatedList(familySubmission, joiner);
             providerSubmission.getInputData().put("childrenWhoNeedCareNames", formattedChildrenNames);
         }
     }
