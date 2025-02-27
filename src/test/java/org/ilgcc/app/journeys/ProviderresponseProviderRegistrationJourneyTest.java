a672364c21582f9b21a6d77a27cc52ce00129d78
Marc Perlman <mperlman@codeforamerica.org>
2025-02-27 13:44:39 -0600

    [CCAP-357] Make the confirmation code input readonly

--- src/test/java/org/ilgcc/app/journeys/ProviderresponseProviderRegistrationJourneyTest.java
+++ src/test/java/org/ilgcc/app/journeys/ProviderresponseProviderRegistrationJourneyTest.java
@@ -1,7 +1,6 @@
 package org.ilgcc.app.journeys;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
 import formflow.library.data.SubmissionRepository;
 import java.time.OffsetDateTime;
