18ac8d60f85b89d63a4813e62cb57bd937df5166
chibuisienyia <cenyia@codeforamerica.org>
2025-03-04 10:48:23 -0600

    [CCAP-674] Test New and Existing Provider Submit Confirmation Screens

--- src/test/java/org/ilgcc/app/journeys/ProviderresponseProviderRegistrationJourneyTest.java
+++ src/test/java/org/ilgcc/app/journeys/ProviderresponseProviderRegistrationJourneyTest.java
@@ -125,6 +125,94 @@ public class ProviderresponseProviderRegistrationJourneyTest extends AbstractBas
     }
 
     @Test
+    public void ExistingProviderBasicflow(){
+        testPage.navigateToFlowScreen("gcc/activities-parent-intro");
+
+        saveSubmission(getSessionSubmissionTestBuilder().withDayCareProvider().withParentDetails()
+            .with("parentPreferredName", "FirstName").withChild("First", "Child", "Yes").withChild("Second", "Child", "No")
+            .withChild("NoAssistance", "Child", "No").withConstantChildcareSchedule(0)
+            .with("earliestChildcareStartDate", "10/10/2011")
+            .withSubmittedAtDate(OffsetDateTime.now()).withShortCode(CONF_CODE).build());
+
+        testPage.clickContinue();
+
+        driver.navigate()
+            .to("http://localhost:%s/s/%s".formatted(localServerPort, CONF_CODE));
+
+        // submit-start
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-submit-start.title"));
+        testPage.clickButton(getEnMessage("provider-response-submit-start.active.button"));
+
+        // paid-by-ccap
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("paid-by-ccap.title"));
+        testPage.clickYes();
+
+        //provider-number
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-provider-number.title"));
+
+        testPage.navigateToFlowScreen("providerresponse/confirmation-code");
+
+        //confirmation-code
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-confirmation-code.title"));
+        testPage.enter("providerResponseFamilyShortCode", CONF_CODE);
+        testPage.clickContinue();
+
+        //response
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-response.title"));
+        testPage.clickElementById("providerResponseAgreeToCare-true");
+        testPage.clickContinue();
+        //provider-info
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-info.title"));
+        testPage.enter("providerResponseBusinessName", "Business Name");
+        testPage.enter("providerResponseFirstName", "First Name");
+        testPage.enter("providerResponseLastName", "Last Name");
+        testPage.clickButton("Continue");
+
+        // service-address
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-provider-service-address.title"));
+        testPage.enter("providerResponseServiceStreetAddress1", "123 Main St");
+        testPage.enter("providerResponseServiceCity", "City");
+        testPage.selectFromDropdown("providerResponseServiceState", "IL - Illinois");
+        testPage.enter("providerResponseServiceZipCode", "12345");
+        testPage.clickButton("Continue");
+
+        // confirm-address
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-confirm-address.title"));
+        testPage.clickButton("Use this address");
+
+        //mailing-address
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response.mailing-address.title"));
+        testPage.clickElementById("providerMailingAddressSameAsServiceAddress-yes");
+        testPage.clickContinue();
+
+        //confirm-mailing-address
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("confirm-mailing-address.title"));
+        testPage.clickButton("Use this address");
+
+        // contact-info
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-contact-info.title"));
+        testPage.enter("providerResponseContactPhoneNumber", "5555555555");
+        testPage.enter("providerResponseContactEmail", "foo@bar.com");
+        testPage.clickButton("Continue");
+
+        // info-review
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("provider-response-info-review.title"));
+        assertThat(testPage.findElementTextById("business-name")).isEqualTo("Business Name");
+        assertThat(testPage.findElementTextById("full-name")).isEqualTo("First Name Last Name");
+        assertThat(testPage.findElementTextById("provider-service-address-1")).isEqualTo("123 Main St");
+        assertThat(testPage.findElementTextById("provider-service-city-state")).isEqualTo("City, IL");
+        assertThat(testPage.findElementTextById("provider-service-zipcode")).isEqualTo("12345");
+        assertThat(testPage.findElementTextById("phone")).isEqualTo("(555) 555-5555");
+        assertThat(testPage.findElementTextById("email")).isEqualTo("foo@bar.com");
+        testPage.clickButton("Continue");
+
+        // registration-submit-confirmation
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("registration-submit-confirmation.title"));
+        assertThat(testPage.getHeader()).isEqualTo(getEnMessage("registration-submit-confirmation.existing-provider.header"));
+
+    }
+
+    @Test
     void LicenseExemptInProviderHomeFlow() {
         createAValidLink();
         submitFamilyConfirmationCode();
