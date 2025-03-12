848c1b4aa5dbf0d83406c664b8224fc71446c767
Ana Medrano <amedrano@codeforamerica.org>
2025-03-12 09:27:29 -0700

    [CCAP-700] - implement sda 6

--- src/main/java/org/ilgcc/app/utils/CountyUtils.java
+++ src/main/java/org/ilgcc/app/utils/CountyUtils.java
@@ -1,6 +1,7 @@
 package org.ilgcc.app.utils;
 
 import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import org.ilgcc.app.data.County;
@@ -11,14 +12,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class CountyUtils {
 
-  @Autowired
-  private ApplicationRoutingServiceImpl applicationRoutingService;
+    @Autowired
+    private ApplicationRoutingServiceImpl applicationRoutingService;
 
-  @Getter
-  private static List<County> activeCounties;
+    @Getter
+    private static List<County> activeCounties;
 
-  @PostConstruct
-  public void init() {
-    activeCounties = applicationRoutingService.getActiveCountiesByCaseLoadCodes();
-  }
+    public static List<String> activeCountiesProperCapitalized = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        activeCounties = applicationRoutingService.getActiveCountiesByCaseLoadCodes();
+        for (County countyName : activeCounties) {
+            activeCountiesProperCapitalized.add(capitalizeCounty(countyName.getCounty()));
+        }
+    }
+
+    private static String capitalizeCounty(String countyName) {
+        String lowercaseCounty = countyName.toLowerCase();
+        if (lowercaseCounty.startsWith("mch") || lowercaseCounty.startsWith("dek")) {
+            return lowercaseCounty.substring(0, 1).toUpperCase() + lowercaseCounty.substring(1, 2) + lowercaseCounty.substring(2,
+                    3).toUpperCase() + lowercaseCounty.substring(3);
+        }
+        return lowercaseCounty.substring(0, 1).toUpperCase() + lowercaseCounty.substring(1);
+    }
 }
