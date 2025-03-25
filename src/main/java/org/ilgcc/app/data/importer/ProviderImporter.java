a02abf7aa0841abec031d5aa29a53c3038e1b8cd
Marc Perlman <mperlman@codeforamerica.org>
2025-03-25 14:59:09 -0500

    [CCAP-746] Redact Data from non-business license-exempt providers table on import

--- src/main/java/org/ilgcc/app/data/importer/ProviderImporter.java
+++ src/main/java/org/ilgcc/app/data/importer/ProviderImporter.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.assertj.core.util.diff.Delta.TYPE;
 
 public class ProviderImporter {
 
