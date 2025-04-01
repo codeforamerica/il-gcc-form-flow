e2941acb18f901911dcc61586c9d9eb94ba99091
spokenbird <agonzalez@codeforamerica.org>
2025-04-01 13:54:10 -0700

    [CCAP-741] Add false condition test

--- src/test/java/org/ilgcc/app/submission/conditions/ContactProviderViaTextTest.java
+++ src/test/java/org/ilgcc/app/submission/conditions/ContactProviderViaTextTest.java
@@ -3,6 +3,7 @@ package org.ilgcc.app.submission.conditions;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import formflow.library.data.Submission;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,13 +17,21 @@ class ContactProviderViaTextTest {
 
     @BeforeEach
     void setUp() {
+        Map<String, Object> inputData = new HashMap<>();
+        inputData.put("contactProviderMethod[]", List.of("TEXT"));
         testSubmission = Submission.builder()
-                .inputData(Map.of("contactProviderMethod[]", List.of("TEXT")))
+                .inputData(inputData)
                 .build();
     }
     
     @Test
     void selectedTextAsProviderContactMethod_shouldReturnTrueWhenUserSelectedText() {
         assertThat(contactProviderViaText.run(testSubmission)).isTrue();
+    }
+
+    @Test
+    void selectedTextAsProviderContactMethod_shouldReturnFalseWhenUserDidNotSelectText() {
+        testSubmission.getInputData().put("contactProviderMethod[]", List.of("EMAIL", "OTHER"));
+        assertThat(contactProviderViaText.run(testSubmission)).isFalse();
     }
 }
