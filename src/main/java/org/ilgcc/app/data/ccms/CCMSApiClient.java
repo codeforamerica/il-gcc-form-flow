e7deda2d768a37e8c0e9c9f92056cf4e334c5ba3
Marc P <mperlman@codeforamerica.org>
2025-04-03 12:48:53 -0500

    [CCAP-776] adding some logging for CCMS (#1374)

--- src/main/java/org/ilgcc/app/data/ccms/CCMSApiClient.java
+++ src/main/java/org/ilgcc/app/data/ccms/CCMSApiClient.java
@@ -6,10 +6,12 @@
     import com.fasterxml.jackson.core.JsonProcessingException;
     import com.fasterxml.jackson.databind.JsonNode;
     import com.fasterxml.jackson.databind.ObjectMapper;
+    import java.util.Arrays;
     import java.util.UUID;
     import lombok.extern.slf4j.Slf4j;
     import org.ilgcc.app.config.CCMSApiConfiguration;
     import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.core.env.Environment;
     import org.springframework.http.HttpHeaders;
     import org.springframework.http.MediaType;
     import org.springframework.stereotype.Service;
@@ -22,13 +24,17 @@
         private final CCMSApiConfiguration configuration;
         private final WebClient client;
         private final ObjectMapper objectMapper = new ObjectMapper();
-    
+        private final boolean isProduction;
+        
         @Autowired
-        public CCMSApiClient(CCMSApiConfiguration configuration) {
+        public CCMSApiClient(CCMSApiConfiguration configuration, Environment env) {
             this.configuration = configuration;
             this.client = WebClient.builder()
                     .baseUrl(configuration.getBaseUrl())
                     .build();
+
+            String[] activeProfiles = env.getActiveProfiles();
+            isProduction = Arrays.asList(activeProfiles).contains("production");
         }
         
         public JsonNode sendRequest(String endpoint, CCMSTransaction requestBody) throws JsonProcessingException {
@@ -38,8 +44,8 @@
                     .bodyValue(requestBody)
                     .retrieve()
                     .onStatus(status -> !status.is2xxSuccessful(), apiResponse -> {
-                        log.error("Received an error response {} from CCMS when attempting to send transaction payload for submission with ID: {}",
-                                apiResponse.statusCode(), requestBody.getSubmissionId());
+                        log.error("Received an error response {} from CCMS when attempting to send transaction payload for submission with ID: {} {}",
+                                apiResponse.statusCode(), requestBody.getSubmissionId(), (isProduction ? "" : ("requestBody: " + requestBody)));
                         return apiResponse.createException();
                     })
                     .bodyToMono(String.class)
