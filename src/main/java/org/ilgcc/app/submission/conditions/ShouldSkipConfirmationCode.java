4b2ed7480d682bc4d39247ea899bf503b8aedf4b
Marc Perlman <mperlman@codeforamerica.org>
2025-04-10 15:51:57 -0500

    [CCAP-822] Save the confirmation code when skipping the confirmation code screen

--- src/main/java/org/ilgcc/app/submission/conditions/ShouldSkipConfirmationCode.java
+++ src/main/java/org/ilgcc/app/submission/conditions/ShouldSkipConfirmationCode.java
@@ -1,9 +1,11 @@
 package org.ilgcc.app.submission.conditions;
 
 import static org.ilgcc.app.utils.constants.SessionKeys.SESSION_KEY_CAME_FROM_HOME_PAGE;
+import static org.ilgcc.app.utils.constants.SessionKeys.SESSION_KEY_FAMILY_CONFIRMATION_CODE;
 
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepositoryService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +13,24 @@ import org.springframework.stereotype.Component;
 public class ShouldSkipConfirmationCode implements Condition {
 
     private final HttpSession httpSession;
+    private final SubmissionRepositoryService submissionRepositoryService;
 
-    public ShouldSkipConfirmationCode(HttpSession httpSession) {
+    public ShouldSkipConfirmationCode(HttpSession httpSession, SubmissionRepositoryService submissionRepositoryService) {
         this.httpSession = httpSession;
+        this.submissionRepositoryService = submissionRepositoryService;
     }
 
     @Override
     public Boolean run(Submission submission) {
         Boolean cameFromHomePage = (Boolean) httpSession.getAttribute(SESSION_KEY_CAME_FROM_HOME_PAGE);
+        boolean shouldSkip = cameFromHomePage != null && cameFromHomePage;
+
+        if (shouldSkip) {
+            String confirmationCode = (String) httpSession.getAttribute(SESSION_KEY_FAMILY_CONFIRMATION_CODE);
+            submission.getInputData().put("providerResponseFamilyShortCode", confirmationCode);
+            submissionRepositoryService.save(submission);
+        }
+
         return cameFromHomePage != null && cameFromHomePage;
     }
 }
