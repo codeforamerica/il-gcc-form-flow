8fdc19491a9bc3d91181660ebf821809a7b61e33
spokenbird <agonzalez@codeforamerica.org>
2025-04-10 14:39:00 -0700

    [CCAP-690] Log active profile

--- src/main/java/org/ilgcc/app/IlGCCApplication.java
+++ src/main/java/org/ilgcc/app/IlGCCApplication.java
@@ -3,6 +3,8 @@ package org.ilgcc.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,17 +14,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class IlGCCApplication {
 
-  public static void main(String[] args) {
-    System.out.println("SPRING_PROFILES_ACTIVE: " + System.getenv("SPRING_PROFILES_ACTIVE"));
-    System.out.println("DATABASE_HOST: " + System.getenv("DATABASE_HOST"));
-    System.out.println("DATABASE_USER: " + System.getenv("DATABASE_USER"));
-    System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
-//    try {
-      SpringApplication.run(IlGCCApplication.class, args);
-//    } catch (Throwable t) {
-//      t.printStackTrace();
-//      System.err.println("FATAL ERROR: " + t.getMessage());
-//      System.exit(1);
-//    }
-}
+    public static void main(String[] args) {
+      ConfigurableApplicationContext context = SpringApplication.run(IlGCCApplication.class, args);
+      System.out.println("SPRING_PROFILES_ACTIVE: " + System.getenv("SPRING_PROFILES_ACTIVE"));
+        Environment env = context.getEnvironment();
+        System.out.println("ACTIVE PROFILE IS: " + String.join(", ", env.getActiveProfiles()));
+    }
 }
