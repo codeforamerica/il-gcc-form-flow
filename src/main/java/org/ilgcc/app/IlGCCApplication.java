a8f103fe67db5f1ecc4e68941e7020851055c67b
spokenbird <agonzalez@codeforamerica.org>
2025-04-15 11:44:22 -0700

    [CCAP-690] Remove debugging logs from IlGCCApplication.java

--- src/main/java/org/ilgcc/app/IlGCCApplication.java
+++ src/main/java/org/ilgcc/app/IlGCCApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -16,8 +15,5 @@ public class IlGCCApplication {
 
     public static void main(String[] args) {
       ConfigurableApplicationContext context = SpringApplication.run(IlGCCApplication.class, args);
-      System.out.println("SPRING_PROFILES_ACTIVE: " + System.getenv("SPRING_PROFILES_ACTIVE"));
-        Environment env = context.getEnvironment();
-        System.out.println("ACTIVE PROFILE IS: " + String.join(", ", env.getActiveProfiles()));
     }
 }
