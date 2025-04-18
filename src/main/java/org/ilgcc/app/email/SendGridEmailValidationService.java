882997a2e8920a6ab6abf5396eafba6fc7d284e4
Chibuisi Enyia <cenyia@codeforamerica.org>
2025-04-18 13:49:32 -0500

    [CCAP-711] validate provider email address (#1435)

---
+++ src/main/java/org/ilgcc/app/email/SendGridEmailValidationService.java
@@ -0,0 +1,100 @@
+package org.ilgcc.app.email;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import formflow.library.utils.RegexUtils;
+import java.io.IOException;
+import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SendGridEmailValidationService {
+
+  private boolean ENABLE_EMAIL_VALIDATION;
+  private final SendGrid sendGrid;
+
+  @Autowired
+  public SendGridEmailValidationService(
+      @Value("${sendgrid.enable-sendgrid-email-validation:false}") boolean enableSendgridEmailValidation,
+      @Value("${sendgrid.email-validation-api-key}") String apiKey) {
+    ENABLE_EMAIL_VALIDATION = enableSendgridEmailValidation;
+    this.sendGrid = new SendGrid(apiKey);
+  }
+
+  //Constructor for mocking SendGrid service for tests
+  public SendGridEmailValidationService(boolean enableValidation, SendGrid sendGrid) {
+    this.ENABLE_EMAIL_VALIDATION = enableValidation;
+    this.sendGrid = sendGrid;
+  }
+
+  public HashMap<String, String> validateEmail(String emailAddress) throws IOException {
+    HashMap<String, String> emailValidationResult = new HashMap<>();
+    if (emailAddress == null || emailAddress.isBlank() || !emailAddress.matches(RegexUtils.EMAIL_REGEX)) {
+      log.error("Email address will not be validated because email address is null, empty, or fails our basic email validation");
+      return emailValidationResult;
+    }
+    if (ENABLE_EMAIL_VALIDATION) {
+      try {
+        Response response = getSendGridResponse(emailAddress);
+        if (sendGridFailedToProcessEmailValidationRequest(response)) {
+          emailValidationResult.put("endpointReached", "failed");
+          return emailValidationResult;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        SendGridValidationResponseBody responseBody = mapper.readValue(response.getBody(), SendGridValidationResponseBody.class);
+        emailValidationResult.put("endpointReached", "success");
+
+        Boolean emailIsValid = isValidEmail(responseBody);
+        emailValidationResult.put("emailIsValid", emailIsValid.toString());
+        if (!emailIsValid) {
+          Boolean hasSuggestedEmail = responseBody.getResult().hasSuggestedEmailAddress();
+          emailValidationResult.put("hasSuggestion", hasSuggestedEmail.toString());
+          if (hasSuggestedEmail) {
+            emailValidationResult.put("suggestedEmail", responseBody.getResult().getSuggestedEmailAddress());
+          }
+        }
+      } catch (Exception e) {
+        log.error("Request to Sendgrid Failed! Error: {}", e.getMessage());
+      }
+    }
+    return emailValidationResult;
+  }
+
+  protected Response getSendGridResponse(String emailAddress) throws IOException {
+    Request request = new Request();
+    request.setMethod(Method.POST);
+    request.setEndpoint("validations/email");
+    request.setBody(new JSONObject(new HashMap<String, Object>() {
+      {
+        put("email", emailAddress);
+      }
+    }).toString());
+    return sendGrid.api(request);
+  }
+
+  public Boolean sendGridFailedToProcessEmailValidationRequest(Response response) {
+    boolean sendGridRequestFailed = response.getStatusCode() != 200;
+    if (sendGridRequestFailed) {
+      log.error("Sendgrid request failed.  Error code: {}", response.getStatusCode());
+    }
+    return sendGridRequestFailed;
+  }
+
+  public Boolean isValidEmail(@NotNull SendGridValidationResponseBody responseBody) {
+    SendGridValidationResponseBody.Result result = responseBody.getResult();
+    return result.hasValidAddressSyntax() &&
+        result.hasMxOrARecord() &&
+        !result.isSuspectedDisposableAddress() &&
+        !result.hasKnownBounces() &&
+        !result.hasSuspectedBounces();
+  }
+}
---
+++ src/test/java/org/ilgcc/app/email/SendGridEmailValidationServiceTest.java
@@ -0,0 +1,199 @@
+package org.ilgcc.app.email;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import java.util.HashMap;
+import org.ilgcc.app.IlGCCApplication;
+import org.ilgcc.app.utils.SendGridValidationResponseBodyBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(classes = IlGCCApplication.class, properties = {
+    "sendgrid.email-validation-api-key=fake-api-key",
+    "sendgrid.enable-sendgrid-email-validation=true"
+})
+@ActiveProfiles("test")
+public class SendGridEmailValidationServiceTest {
+
+  private SendGrid mockSendGrid = mock(SendGrid.class);
+
+  private static final String MOCK_VALID_EMAIL = "test@email.com";
+  private static final String MOCK_INVALID_EMAIL = "invalid@email.com";
+  private static final String ERROR_RESPONSE_BODY = """
+          {
+            "errors": [
+              {
+                "field": "email",
+                "message": "Invalid email format provided."
+              }
+            ]
+          }
+      """;
+
+  @Test
+  public void shouldReturnTrueWhenSendGridReturnsValidResponse() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    SendGridValidationResponseBody validSendGridResponseBody = new SendGridValidationResponseBodyBuilder()
+        .withHasValidAddressSyntax(true)
+        .withHasMxOrARecord(true)
+        .withIsSuspectedDisposableAddress(false)
+        .withHasKnownBounces(false)
+        .withHasSuspectedBounces(false).withEmailAddress("test@email.com").build();
+
+    String responseBody = new ObjectMapper().writeValueAsString(validSendGridResponseBody);
+    Response fakeResponse = new Response(200, responseBody, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_VALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_VALID_EMAIL);
+    assertEquals("success", result.get("endpointReached"));
+    assertEquals("true", result.get("emailIsValid"));
+  }
+
+  @Test
+  public void shouldSetEmailIsValidToFalseWhenHasValidSyntaxIsFalse() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    SendGridValidationResponseBody validSendGridResponseBody = new SendGridValidationResponseBodyBuilder()
+        .withHasValidAddressSyntax(false)
+        .withHasMxOrARecord(true)
+        .withIsSuspectedDisposableAddress(false)
+        .withHasKnownBounces(false)
+        .withHasSuspectedBounces(false).withEmailAddress("test@email.com").build();
+
+    String responseBody = new ObjectMapper().writeValueAsString(validSendGridResponseBody);
+    Response fakeResponse = new Response(200, responseBody, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_INVALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_INVALID_EMAIL);
+    assertEquals("success", result.get("endpointReached"));
+    assertEquals("false", result.get("emailIsValid"));
+  }
+
+  @Test
+  public void shouldSetEmailIsValidToFalseWhenHasMXOrARecordIsFalse() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    SendGridValidationResponseBody validSendGridResponseBody = new SendGridValidationResponseBodyBuilder()
+        .withHasValidAddressSyntax(true)
+        .withHasMxOrARecord(false)
+        .withIsSuspectedDisposableAddress(false)
+        .withHasKnownBounces(false)
+        .withHasSuspectedBounces(false).withEmailAddress("test@email.com").build();
+
+    String responseBody = new ObjectMapper().writeValueAsString(validSendGridResponseBody);
+    Response fakeResponse = new Response(200, responseBody, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_INVALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_INVALID_EMAIL);
+    assertEquals("success", result.get("endpointReached"));
+    assertEquals("false", result.get("emailIsValid"));
+  }
+
+  @Test
+  public void shouldSetEmailIsValidToFalseWhenIsSuspectedDisposableAddressIsTrue() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    SendGridValidationResponseBody validSendGridResponseBody = new SendGridValidationResponseBodyBuilder()
+        .withHasValidAddressSyntax(true)
+        .withHasMxOrARecord(true)
+        .withIsSuspectedDisposableAddress(true)
+        .withHasKnownBounces(false)
+        .withHasSuspectedBounces(false).withEmailAddress("test@email.com").build();
+
+    String responseBody = new ObjectMapper().writeValueAsString(validSendGridResponseBody);
+    Response fakeResponse = new Response(200, responseBody, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_INVALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_INVALID_EMAIL);
+    assertEquals("success", result.get("endpointReached"));
+    assertEquals("false", result.get("emailIsValid"));
+  }
+
+  @Test
+  public void shouldSetEmailIsValidToFalseWhenHasKnownBouncesIsTrue() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    SendGridValidationResponseBody validSendGridResponseBody = new SendGridValidationResponseBodyBuilder()
+        .withHasValidAddressSyntax(true)
+        .withHasMxOrARecord(true)
+        .withIsSuspectedDisposableAddress(false)
+        .withHasKnownBounces(true)
+        .withHasSuspectedBounces(false).withEmailAddress("test@email.com").build();
+
+    String responseBody = new ObjectMapper().writeValueAsString(validSendGridResponseBody);
+    Response fakeResponse = new Response(200, responseBody, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_INVALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_INVALID_EMAIL);
+    assertEquals("success", result.get("endpointReached"));
+    assertEquals("false", result.get("emailIsValid"));
+  }
+
+  @Test
+  public void shouldSetEmailIsValidToFalseWhenHasSuspectedBouncesIsTrue() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    SendGridValidationResponseBody validSendGridResponseBody = new SendGridValidationResponseBodyBuilder()
+        .withHasValidAddressSyntax(true)
+        .withHasMxOrARecord(true)
+        .withIsSuspectedDisposableAddress(false)
+        .withHasKnownBounces(false)
+        .withHasSuspectedBounces(true).withEmailAddress("test@email.com").build();
+
+    String responseBody = new ObjectMapper().writeValueAsString(validSendGridResponseBody);
+    Response fakeResponse = new Response(200, responseBody, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_INVALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_INVALID_EMAIL);
+    assertEquals("success", result.get("endpointReached"));
+    assertEquals("false", result.get("emailIsValid"));
+  }
+
+  @Test
+  public void shouldReturnEmptyHashMapWhenSendGridFlagIsOff() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(false, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    HashMap<String, String> emailValidationResult = spysendGridEmailValidationService.validateEmail(MOCK_VALID_EMAIL);
+    assertTrue(emailValidationResult.isEmpty());
+    verify(spysendGridEmailValidationService, never()).getSendGridResponse(MOCK_VALID_EMAIL);
+  }
+
+  @Test
+  public void shouldSetEndPointReachedToFalseResponseA200ResponseIsNotReturned() throws Exception {
+    SendGridEmailValidationService sendGridEmailValidationService = new SendGridEmailValidationService(true, mockSendGrid);
+    SendGridEmailValidationService spysendGridEmailValidationService = spy(sendGridEmailValidationService);
+
+    Response fakeResponse = new Response(403, ERROR_RESPONSE_BODY, new HashMap<>());
+
+    doReturn(fakeResponse).when(spysendGridEmailValidationService).getSendGridResponse(MOCK_VALID_EMAIL);
+
+    HashMap<String, String> result = spysendGridEmailValidationService.validateEmail(MOCK_VALID_EMAIL);
+    assertEquals("failed", result.get("endpointReached"));
+    verify(spysendGridEmailValidationService, never()).isValidEmail(any());
+  }
+}
