d7cf493ff85f9e6a2807b9bd51da8fcaea8929dc
Marc Perlman <mperlman@codeforamerica.org>
2025-04-23 12:26:40 -0500

    No need to send basic auth

--- src/main/java/org/ilgcc/app/data/ccms/CCMSApiClient.java
+++ src/main/java/org/ilgcc/app/data/ccms/CCMSApiClient.java
@@ -15,7 +15,6 @@
     import org.springframework.http.HttpHeaders;
     import org.springframework.http.MediaType;
     import org.springframework.stereotype.Service;
-    import org.springframework.web.reactive.function.client.ClientResponse;
     import org.springframework.web.reactive.function.client.WebClient;
     
     @Service
