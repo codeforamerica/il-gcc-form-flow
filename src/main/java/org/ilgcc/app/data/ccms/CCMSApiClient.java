0f9e0de93e100a2ff50b692296e846f8925c64c7
Marc P <mperlman@codeforamerica.org>
2025-04-23 12:26:56 -0500

    No need to send basic auth (#1483)

--- src/main/java/org/ilgcc/app/data/ccms/CCMSApiClient.java
+++ src/main/java/org/ilgcc/app/data/ccms/CCMSApiClient.java
@@ -8,16 +8,13 @@
     import com.fasterxml.jackson.databind.ObjectMapper;
     import java.net.InetAddress;
     import java.net.UnknownHostException;
-    import java.util.Arrays;
     import java.util.UUID;
     import lombok.extern.slf4j.Slf4j;
     import org.ilgcc.app.config.CCMSApiConfiguration;
     import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.core.env.Environment;
     import org.springframework.http.HttpHeaders;
     import org.springframework.http.MediaType;
     import org.springframework.stereotype.Service;
-    import org.springframework.web.reactive.function.client.ClientResponse;
     import org.springframework.web.reactive.function.client.WebClient;
     
     @Service
@@ -29,13 +26,11 @@
         private final ObjectMapper objectMapper = new ObjectMapper();
 
         @Autowired
-        public CCMSApiClient(CCMSApiConfiguration configuration, Environment env) {
+        public CCMSApiClient(CCMSApiConfiguration configuration) {
             this.configuration = configuration;
             this.client = WebClient.builder()
                     .baseUrl(configuration.getBaseUrl())
                     .build();
-
-            String[] activeProfiles = env.getActiveProfiles();
         }
         
         public JsonNode sendRequest(String endpoint, CCMSTransaction requestBody) throws JsonProcessingException {
@@ -47,7 +42,7 @@
                     .retrieve()
                     .onStatus(status -> !status.is2xxSuccessful(), apiResponse -> {
                         log.error("Received an error response from CCMS when attempting to send transaction payload for submission with ID: {}. Error: {}",
-                                requestBody.getSubmissionId(), getResponseDetails(apiResponse));
+                                requestBody.getSubmissionId(), apiResponse);
                         return apiResponse.createException();
                     })
                     .bodyToMono(String.class)
@@ -64,7 +59,7 @@
                     .retrieve()
                     .onStatus(status -> !status.is2xxSuccessful(), apiResponse -> {
                         log.error("Received an error response from CCMS when attempting to fetch the work item ID for Transaction with ID: {}. Error: {}",
-                                requestBody.getTransactionId(), getResponseDetails(apiResponse));
+                                requestBody.getTransactionId(), apiResponse);
                         return apiResponse.createException();
                     })
                     .bodyToMono(String.class)
@@ -76,9 +71,6 @@
         private HttpHeaders createRequestHeaders() {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            if (configuration.getUserName() != null && !configuration.getUserName().isBlank() && configuration.getPassword() != null && !configuration.getPassword().isBlank()) {
-                headers.setBasicAuth(configuration.getUserName(), configuration.getPassword());
-            }
             headers.set(CORRELATION_ID.getValue(), String.valueOf(UUID.randomUUID()));
             headers.set(OCP_APIM_SUBSCRIPTION_KEY.getValue(), configuration.getApiSubscriptionKey());
             return headers;
@@ -91,15 +83,5 @@
             } catch (UnknownHostException e) {
                 log.warn("Could not determine container IP address", e);
             }
-        }
-
-        private String getResponseDetails(ClientResponse response) {
-            StringBuilder responseDetails = new StringBuilder();
-            response.bodyToMono(String.class).subscribe(body -> {
-                responseDetails.append(" Status: ").append(response.statusCode());
-                responseDetails.append(" Headers: ").append(response.headers().asHttpHeaders());
-                responseDetails.append(" Body: ").append(body);
-            });
-            return responseDetails.toString();
         }
     }
