6948aeb759d4fe9ae5e47041c5356e7aa32c3499
chibuisienyia <cenyia@codeforamerica.org>
2025-04-29 11:30:46 -0500

    [CCAP-815] Adding GetCCRRNameFromFamilySubmissionTest

---
+++ src/test/java/org/ilgcc/app/submission/actions/GetCCRRNameFromFamilySubmissionTest.java
@@ -0,0 +1,9 @@
+package org.ilgcc.app.submission.actions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetCCRRNameFromFamilySubmissionTest {
+   void shouldReturnCCRRNameFromFamilySubmissionIfPresent() {
+
+   }
+}
