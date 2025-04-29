af92b467615d0258fd5fff5e41478269ca37b7ed
chibuisienyia <cenyia@codeforamerica.org>
2025-04-29 13:29:16 -0500

    [CCAP-815] Added a Journey and a GetCCRRNameFromFamilySubmissionTest

--- src/test/java/org/ilgcc/app/submission/actions/GetCCRRNameFromFamilySubmissionTest.java
+++ src/test/java/org/ilgcc/app/submission/actions/GetCCRRNameFromFamilySubmissionTest.java
@@ -2,8 +2,74 @@ package org.ilgcc.app.submission.actions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GetCCRRNameFromFamilySubmissionTest {
-   void shouldReturnCCRRNameFromFamilySubmissionIfPresent() {
+import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepository;
+import formflow.library.data.SubmissionRepositoryService;
+import org.ilgcc.app.IlGCCApplication;
+import org.ilgcc.app.utils.SubmissionTestBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-   }
+@SpringBootTest(
+    classes = IlGCCApplication.class
+)
+@ActiveProfiles("test")
+class GetCCRRNameFromFamilySubmissionTest {
+
+  @Autowired
+  private SubmissionRepositoryService submissionRepositoryService;
+  @Autowired
+  private GetCCRRNameFromFamilySubmission getCCRRNameFromFamilySubmission;
+
+
+  @Autowired
+  SubmissionRepository submissionRepository;
+  private Submission familySubmission;
+  private Submission providerSubmission;
+  private final String CCRR_NAME_INPUT = "ccrrName";
+  private final String CCRR_NAME_VALUE = "Sample Test CCRR";
+
+  @AfterEach
+  void tearDown() {
+    submissionRepository.deleteAll();
+  }
+
+  @Test
+  void shouldReturnCCRRNameFromFamilySubmissionIfPresent() {
+    familySubmission = new SubmissionTestBuilder()
+        .withFlow("gcc")
+        .withCCRR()
+        .build();
+    submissionRepositoryService.save(familySubmission);
+
+    providerSubmission = submissionRepositoryService.save(new SubmissionTestBuilder()
+        .withFlow("providerresponse")
+        .with("familySubmissionId", familySubmission.getId().toString())
+        .withProviderSubmissionData()
+        .build());
+
+    getCCRRNameFromFamilySubmission.run(providerSubmission);
+    assertTrue(providerSubmission.getInputData().containsKey(CCRR_NAME_INPUT));
+    assertEquals(CCRR_NAME_VALUE, familySubmission.getInputData().get(CCRR_NAME_INPUT));
+  }
+
+  @Test
+  void shouldNotSetCCRRNameIfCCRRNameNotFoundInFamilySubmission() {
+    familySubmission = new SubmissionTestBuilder()
+        .withFlow("gcc")
+        .build();
+    submissionRepositoryService.save(familySubmission);
+
+    providerSubmission = submissionRepositoryService.save(new SubmissionTestBuilder()
+        .withFlow("providerresponse")
+        .with("familySubmissionId", familySubmission.getId().toString())
+        .withProviderSubmissionData()
+        .build());
+
+    getCCRRNameFromFamilySubmission.run(providerSubmission);
+    assertFalse(providerSubmission.getInputData().containsKey(CCRR_NAME_INPUT));
+  }
 }
