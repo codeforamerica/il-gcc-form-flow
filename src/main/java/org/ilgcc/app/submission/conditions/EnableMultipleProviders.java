7d27334bb5cf2c1dfee4726484ca255c3cf9514b
Alex Gonzalez <agonzalez@codeforamerica.org>
2025-05-06 12:37:06 -0700

    [CCAP-874] Add Multiple Provider flag and flows config logic (#1534)

---
+++ src/main/java/org/ilgcc/app/submission/conditions/EnableMultipleProviders.java
@@ -0,0 +1,23 @@
+package org.ilgcc.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnableMultipleProviders implements Condition {
+
+    @Value("${il-gcc.enable-multiple-providers}")
+    private boolean enableMultipleProviders;
+
+    @Override
+    public Boolean run(Submission submission) {
+        return enableMultipleProviders;
+    }
+    
+    @Override
+    public Boolean run(Submission submission, String uuid) {
+        return enableMultipleProviders;
+    }
+}
