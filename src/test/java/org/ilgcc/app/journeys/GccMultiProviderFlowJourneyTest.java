4169b934e66701e3b3fe085b382183dd256145ff
chibuisienyia <cenyia@codeforamerica.org>
2025-05-08 13:32:09 -0500

    [CCAP-893] Navigation change for acceptance testing

--- src/test/java/org/ilgcc/app/journeys/GccMultiProviderFlowJourneyTest.java
+++ src/test/java/org/ilgcc/app/journeys/GccMultiProviderFlowJourneyTest.java
@@ -6,7 +6,7 @@ import org.ilgcc.app.utils.AbstractBasePageTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = {"il-gcc.enable-multiple-providers=true"})
+@TestPropertySource(properties = {"il-gcc.enable-multiple-providers=true", "il-gcc.enable-provider-messaging=true"})
 public class GccMultiProviderFlowJourneyTest extends AbstractBasePageTest {
 
     @Test
@@ -16,12 +16,16 @@ public class GccMultiProviderFlowJourneyTest extends AbstractBasePageTest {
         saveSubmission(getSessionSubmissionTestBuilder()
                 .withParentBasicInfo()
                 .with("familyIntendedProviderName", "ACME Daycare")
+                .withChild("test", "tested", "true")
                 .with("applicationCounty", "LEE")
                 .with("hasChosenProvider", "true")
                 .withShortCode("familyShortCode")
                 .build());
 
-        testPage.navigateToFlowScreen("gcc/providers-intro");
+        testPage.navigateToFlowScreen("gcc/children-add");
+        testPage.clickButton(getEnMessage("children-add.thats-all"));
         assertThat(testPage.getTitle()).isEqualTo(getEnMessage("providers-intro.title-header"));
+        testPage.clickContinue();
+        assertThat(testPage.getTitle()).isEqualTo(getEnMessage("after-submit-contact-provider.title"));
     }
 }
