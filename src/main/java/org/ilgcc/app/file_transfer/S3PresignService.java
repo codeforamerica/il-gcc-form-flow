cf2ef7a8e38f77f6c1ac806bf99279241cfefc2a
Alex Gonzalez <agonzalez@codeforamerica.org>
2025-05-13 16:42:31 -0700

    [CCAP-690] Revert to old authentication method for S3PresignService (#1559)

--- src/main/java/org/ilgcc/app/file_transfer/S3PresignService.java
+++ src/main/java/org/ilgcc/app/file_transfer/S3PresignService.java
@@ -12,6 +12,8 @@ import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -24,25 +26,35 @@ import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignReques
 @Service
 @Slf4j
 public class S3PresignService {
-    
+
+    private final Region region;
+    private final String secretKey;
+    private final String accessKey;
     private final String bucketName;
     private final S3Presigner s3Presigner;
     private final S3Client s3Client;
     private final Long presignedUrlDuration;
-    
-    public S3PresignService(
+
+    public S3PresignService(@Value("${form-flow.aws.access_key}") String accessKey,
+            @Value("${form-flow.aws.secret_key}") String secretKey,
             @Value("${form-flow.aws.s3_bucket_name}") String bucketName,
             @Value("${form-flow.aws.region}") String region,
             DocumentTransferConfiguration documentTransferConfiguration) {
+        this.region = Region.of(region);
+        this.secretKey = secretKey;
+        this.accessKey = accessKey;
         this.bucketName = bucketName;
         this.presignedUrlDuration = documentTransferConfiguration.getPresignedUrlDuration();
 
-        this.s3Client = S3Client.builder()
-                .region(Region.of(region))
-                .build();
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(this.accessKey, this.secretKey);
 
+        this.s3Client = S3Client.builder()
+                .region(this.region)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
         this.s3Presigner = S3Presigner.builder()
-                .region(Region.of(region))
+                .region(this.region)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
     }
 
@@ -59,11 +71,11 @@ public class S3PresignService {
 
         return s3Presigner.presignGetObject(getObjectPresignRequest).url().toString();
     }
-    
+
     @Async
     @Retryable(
-            retryFor = {S3ObjectNotScannedException.class}, 
-            maxAttempts = 5, 
+            retryFor = {S3ObjectNotScannedException.class},
+            maxAttempts = 5,
             backoff = @Backoff(delay = 5000)
     )
     public CompletableFuture<Boolean> isObjectScannedAndClean(String objectPath) {
@@ -88,13 +100,13 @@ public class S3PresignService {
             throw new S3ObjectNotFoundException("No object was found at path: " + objectPath + " when checking file tags. It's possible the object was moved to quarantine, or deleted.");
         }
     }
-    
+
     @Recover
     public CompletableFuture<Boolean> recover(S3ObjectNotScannedException e, String objectPath) {
         log.error("The file at path: {} was not scanned after 5 attempts. It's likely the file was not sent to SharePoint.", objectPath);
         return CompletableFuture.failedFuture(e);
     }
-    
+
     @Recover CompletableFuture<Boolean> recover(S3ObjectNotFoundException e, String objectPath) {
         log.error("The file at path: {} was not found. It's likely the file had a virus and was quarantined.", objectPath);
         return CompletableFuture.failedFuture(e);
