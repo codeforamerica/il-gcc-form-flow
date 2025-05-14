a23372d7e64fd06fe469d71336cbc7e1a3b134b5
Marc Perlman <mperlman@codeforamerica.org>
2025-05-14 13:40:50 -0500

    [CCAP-927][CCAP-920][CCAP-826] Updating FEIN validation everywhere

--- src/main/java/org/ilgcc/app/inputs/InputsConstants.java
+++ src/main/java/org/ilgcc/app/inputs/InputsConstants.java
@@ -1,7 +1,6 @@
 package org.ilgcc.app.inputs;
 
 public class InputsConstants {
-
     public static final String FEIN_REGEX = "\\d{2}-\\d{7}";
     public static final String ZIPCODE_REGEX = "^$|^\\d{5}(?:-\\d{4})?$";
-    }
+}
