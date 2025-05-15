e8f73804ecad9f489ce41c2a71b7844192a31a2d
Marc Perlman <mperlman@codeforamerica.org>
2025-05-15 10:51:41 -0500

    How did this work? How does it work locally? I am confused

--- src/main/java/org/ilgcc/app/submission/conditions/ProviderIsLicenseExemptChildCareCenter.java
+++ src/main/java/org/ilgcc/app/submission/conditions/ProviderIsLicenseExemptChildCareCenter.java
@@ -4,7 +4,6 @@ import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import java.util.List;
 import org.ilgcc.app.utils.enums.ProviderType;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,12 +13,10 @@ public class ProviderIsLicenseExemptChildCareCenter implements Condition {
             ProviderType.LICENSE_EXEMPT_CHILD_CARE_CENTER.name()
     );
 
-    @Value("${il-gcc.allow-provider-registration-flow}")
-    private boolean enableProviderRegistration;
 
     @Override
     public Boolean run(Submission submission) {
-        return enableProviderRegistration && displayScreen(submission);
+        return displayScreen(submission);
     }
 
     private Boolean displayScreen(Submission submission) {
