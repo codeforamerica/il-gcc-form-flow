3d6b7fa7e153d221e4a2c89497b24befe3816d26
Marc Perlman <mperlman@codeforamerica.org>
2025-05-21 10:02:09 -0500

    [CCAP-952]

--- src/main/java/org/ilgcc/app/submission/actions/SendAutomatedProviderEmail.java
+++ src/main/java/org/ilgcc/app/submission/actions/SendAutomatedProviderEmail.java
@@ -1,7 +1,11 @@
 package org.ilgcc.app.submission.actions;
 
 import formflow.library.config.submission.Action;
+import formflow.library.data.FormSubmission;
 import formflow.library.data.Submission;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.ilgcc.app.email.SendAutomatedProviderOutreachEmail;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +20,15 @@ public class SendAutomatedProviderEmail implements Action {
 
     @Override
     public void run(Submission familySubmission) {
-        if (familySubmission.getInputData().get("hasConfirmedIntendedProviderEmail").equals("true")) {
+        if ("true".equals(familySubmission.getInputData().get("hasConfirmedIntendedProviderEmail"))) {
+            sendAutomatedProviderOutreachEmail.send(familySubmission);
+        }
+    }
+
+    @Override
+    public void run(Submission familySubmission, String id) {
+        Optional<Map<String, Object>> subflow = Optional.of(familySubmission.getSubflowEntryByUuid("contactProviders", id));
+        if ("true".equals(subflow.get().get("hasConfirmedIntendedProviderEmail"))) {
             sendAutomatedProviderOutreachEmail.send(familySubmission);
         }
     }
--- src/main/java/org/ilgcc/app/submission/conditions/HasConfirmedProviderEmail.java
+++ src/main/java/org/ilgcc/app/submission/conditions/HasConfirmedProviderEmail.java
@@ -1,6 +1,8 @@
 package org.ilgcc.app.submission.conditions;
 
 import formflow.library.data.Submission;
+import java.util.Map;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,5 +11,11 @@ public class HasConfirmedProviderEmail extends BasicCondition {
     @Override
     public Boolean run(Submission submission) {
         return run(submission, "hasConfirmedIntendedProviderEmail", "true");
+    }
+
+    @Override
+    public Boolean run(Submission submission, String subflowUuid) {
+        Optional<Map<String, Object>> subflow = Optional.of(submission.getSubflowEntryByUuid("contactProviders", subflowUuid));
+        return "true".equals(subflow.get().get("hasConfirmedIntendedProviderEmail"));
     }
 }
