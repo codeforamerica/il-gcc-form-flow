b8a1f001d1b0b8698e6ac1382c83043427d8f6b2
Ana Medrano Fernandez <8609011+analoo@users.noreply.github.com>
2025-05-21 16:59:40 -0700

    [CCAP-916] - productize enable provider messaging (#1573)

--- src/main/java/org/ilgcc/app/submission/conditions/ContactProviderViaText.java
+++ src/main/java/org/ilgcc/app/submission/conditions/ContactProviderViaText.java
@@ -4,21 +4,14 @@ import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import java.util.Map;
 import org.ilgcc.app.utils.SubmissionUtilities;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ContactProviderViaText implements Condition {
-    
-    private final boolean enableProviderMessaging;
-
-    public ContactProviderViaText(@Value("${il-gcc.enable-provider-messaging}") boolean enableProviderMessaging) {
-        this.enableProviderMessaging = enableProviderMessaging;
-    }
 
     @Override
     public Boolean run(Submission submission) {
         Map<String, Object> inputData = submission.getInputData();
-        return enableProviderMessaging && SubmissionUtilities.isSelectedAsProviderContactMethod(inputData,"TEXT");
+        return SubmissionUtilities.isSelectedAsProviderContactMethod(inputData,"TEXT");
     }
 }
--- src/main/java/org/ilgcc/app/submission/conditions/DisplaySubmitContactProviderEmailScreen.java
+++ src/main/java/org/ilgcc/app/submission/conditions/DisplaySubmitContactProviderEmailScreen.java
@@ -4,19 +4,14 @@ import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import java.util.List;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DisplaySubmitContactProviderEmailScreen implements Condition {
-
-    @Value("${il-gcc.enable-provider-messaging}")
-    private boolean enableProviderMessaging;
-
     @Override
     public Boolean run(Submission submission) {
         Map<String, Object> inputData = submission.getInputData();
-        return enableProviderMessaging && selectedEmailAsProviderContactMethod(inputData) && isMissingProviderEmail(inputData);
+        return selectedEmailAsProviderContactMethod(inputData) && isMissingProviderEmail(inputData);
     }
 
     private Boolean isMissingProviderEmail(Map<String, Object> inputData){
