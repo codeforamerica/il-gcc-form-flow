585656d86eba541d95b92f5dd0335eba186d3523
Marc Perlman <mperlman@codeforamerica.org>
2025-05-21 22:02:08 -0500

    Fixing merge conflicts

--- src/main/java/org/ilgcc/app/submission/conditions/ContactProviderViaEmail.java
+++ src/main/java/org/ilgcc/app/submission/conditions/ContactProviderViaEmail.java
@@ -4,6 +4,7 @@ import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import java.util.Map;
 import org.ilgcc.app.utils.SubmissionUtilities;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/org/ilgcc/app/submission/conditions/ContactProviderViaText.java
+++ src/main/java/org/ilgcc/app/submission/conditions/ContactProviderViaText.java
@@ -4,6 +4,7 @@ import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import java.util.Map;
 import org.ilgcc.app.utils.SubmissionUtilities;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +16,7 @@ public class ContactProviderViaText implements Condition {
     @Override
     public Boolean run(Submission submission) {
         Map<String, Object> inputData = submission.getInputData();
-        return SubmissionUtilities.isSelectedAsProviderContactMethod(inputData,"TEXT");
+        return SubmissionUtilities.isSelectedAsProviderContactMethod(inputData, "TEXT");
     }
 
     @Override
