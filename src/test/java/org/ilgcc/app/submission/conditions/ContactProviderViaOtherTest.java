44808c64383348a5dcb7c0ee53f7c6f01d26334f
Marc Perlman <mperlman@codeforamerica.org>
2025-05-22 13:14:23 -0500

    [CCAP-959]

--- src/test/java/org/ilgcc/app/submission/conditions/ContactProviderViaOtherTest.java
+++ src/test/java/org/ilgcc/app/submission/conditions/ContactProviderViaOtherTest.java
@@ -6,6 +6,7 @@ import formflow.library.data.Submission;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +15,31 @@ class ContactProviderViaOtherTest {
     Submission testSubmission;
     
     ContactProviderViaOther contactProviderViaOther;
+    UUID uuid1;
+    UUID uuid2;
+    UUID uuid3;
 
     @BeforeEach
     void setUp() {
         Map<String, Object> inputData = new HashMap<>();
         inputData.put("contactProviderMethod[]", List.of("OTHER"));
+
+        uuid1 = UUID.randomUUID();
+        Map<String, Object> subflowData1 = new HashMap<>();
+        subflowData1.put("uuid", uuid1.toString());
+        subflowData1.put("contactProviderMethod[]", List.of("OTHER"));
+
+        uuid2 = UUID.randomUUID();
+        Map<String, Object> subflowData2 = new HashMap<>();
+        subflowData2.put("uuid", uuid2.toString());
+        subflowData2.put("contactProviderMethod[]", List.of("EMAIL", "TEXT"));
+
+        uuid3 = UUID.randomUUID();
+        Map<String, Object> subflowData3 = new HashMap<>();
+        subflowData3.put("uuid", uuid3.toString());
+        subflowData3.put("contactProviderMethod[]", List.of("TEXT", "OTHER"));
+
+        inputData.put("contactProviders", List.of(subflowData1, subflowData2, subflowData3));
         testSubmission = Submission.builder()
                 .inputData(inputData)
                 .build();
@@ -42,5 +63,16 @@ class ContactProviderViaOtherTest {
         contactProviderViaOther = new ContactProviderViaOther(true);
         testSubmission.getInputData().put("contactProviderMethod[]", List.of("EMAIL", "TEXT"));
         assertThat(contactProviderViaOther.run(testSubmission)).isFalse();
+    }
+
+    @Test
+    void contactProviderViaOther_shouldReturnTrueWhenUserSelectedOther_MultipleProviders() {
+        contactProviderViaOther = new ContactProviderViaOther(false);
+        assertThat(contactProviderViaOther.run(testSubmission, uuid1.toString())).isFalse();
+
+        contactProviderViaOther = new ContactProviderViaOther(true);
+        assertThat(contactProviderViaOther.run(testSubmission, uuid1.toString())).isTrue();
+        assertThat(contactProviderViaOther.run(testSubmission, uuid2.toString())).isFalse();
+        assertThat(contactProviderViaOther.run(testSubmission, uuid3.toString())).isTrue();
     }
 }
--- src/test/java/org/ilgcc/app/submission/conditions/ContactProviderViaTextTest.java
+++ src/test/java/org/ilgcc/app/submission/conditions/ContactProviderViaTextTest.java
@@ -6,6 +6,7 @@ import formflow.library.data.Submission;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +15,31 @@ class ContactProviderViaTextTest {
     Submission testSubmission;
     
     ContactProviderViaText contactProviderViaText;
+    UUID uuid1;
+    UUID uuid2;
+    UUID uuid3;
 
     @BeforeEach
     void setUp() {
         Map<String, Object> inputData = new HashMap<>();
         inputData.put("contactProviderMethod[]", List.of("TEXT"));
+
+        uuid1 = UUID.randomUUID();
+        Map<String, Object> subflowData1 = new HashMap<>();
+        subflowData1.put("uuid", uuid1.toString());
+        subflowData1.put("contactProviderMethod[]", List.of("TEXT"));
+
+        uuid2 = UUID.randomUUID();
+        Map<String, Object> subflowData2 = new HashMap<>();
+        subflowData2.put("uuid", uuid2.toString());
+        subflowData2.put("contactProviderMethod[]", List.of("EMAIL", "OTHER"));
+
+        uuid3 = UUID.randomUUID();
+        Map<String, Object> subflowData3 = new HashMap<>();
+        subflowData3.put("uuid", uuid3.toString());
+        subflowData3.put("contactProviderMethod[]", List.of("TEXT", "OTHER"));
+
+        inputData.put("contactProviders", List.of(subflowData1, subflowData2, subflowData3));
         testSubmission = Submission.builder()
                 .inputData(inputData)
                 .build();
@@ -42,5 +63,16 @@ class ContactProviderViaTextTest {
         contactProviderViaText = new ContactProviderViaText(true);
         testSubmission.getInputData().put("contactProviderMethod[]", List.of("EMAIL", "OTHER"));
         assertThat(contactProviderViaText.run(testSubmission)).isFalse();
+    }
+
+    @Test
+    void contactProviderViaText_shouldReturnTrueWhenUserSelectedText_MultipleProviders() {
+        contactProviderViaText = new ContactProviderViaText(false);
+        assertThat(contactProviderViaText.run(testSubmission, uuid1.toString())).isFalse();
+
+        contactProviderViaText = new ContactProviderViaText(true);
+        assertThat(contactProviderViaText.run(testSubmission, uuid1.toString())).isTrue();
+        assertThat(contactProviderViaText.run(testSubmission, uuid2.toString())).isFalse();
+        assertThat(contactProviderViaText.run(testSubmission, uuid3.toString())).isTrue();
     }
 }
