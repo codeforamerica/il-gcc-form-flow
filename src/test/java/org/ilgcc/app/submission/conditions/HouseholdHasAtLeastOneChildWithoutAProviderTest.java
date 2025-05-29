4fb4df55799a211663155bab2549e8a2f7f3c92d
chibuisienyia <cenyia@codeforamerica.org>
2025-05-29 14:52:08 -0500

    [CCAP-961] Update HouseholdHasAtLeastOneChildWithoutAProviderTest.java.

--- src/test/java/org/ilgcc/app/submission/conditions/HouseholdHasAtLeastOneChildWithoutAProviderTest.java
+++ src/test/java/org/ilgcc/app/submission/conditions/HouseholdHasAtLeastOneChildWithoutAProviderTest.java
@@ -11,30 +11,27 @@ class HouseholdHasAtLeastOneChildWithoutAProviderTest {
   @Test
   void shouldReturnFalseWhenEnableMultipleProvidersIsFalse() {
     boolean mockEnableMultipleProviders = false;
-    String mockContactProviderUUID = "mockContactProviderUUID";
     Submission submission = new SubmissionTestBuilder().with("choseProviderForEveryChildInNeedOfCare", "false").build();
     HouseholdHasAtLeastOneChildWithoutAProvider condition = new HouseholdHasAtLeastOneChildWithoutAProvider(mockEnableMultipleProviders);
 
-    assertFalse(condition.run(submission, mockContactProviderUUID));
+    assertFalse(condition.run(submission));
   }
   //If all the children have a chosen provider this should return false
   @Test
   void shouldReturnFalseWhenEnableMultipleProvidersIsTrueAndChoseProviderForEveryChildInNeedOfCareIsTrue() {
     boolean mockEnableMultipleProviders = true;
-    String mockContactProviderUUID = "mockContactProviderUUID";
     Submission submission = new SubmissionTestBuilder().with("choseProviderForEveryChildInNeedOfCare", "true").build();
     HouseholdHasAtLeastOneChildWithoutAProvider condition = new HouseholdHasAtLeastOneChildWithoutAProvider(mockEnableMultipleProviders);
 
-    assertFalse(condition.run(submission, mockContactProviderUUID));
+    assertFalse(condition.run(submission));
   }
 
   //If at least one child does not have a chose provider this should return true
   @Test
   void shouldReturnFalseWhenEnableMultipleProvidersIsTrueAndChoseProviderForEveryChildInNeedOfCareIsFalse() {
     boolean mockEnableMultipleProviders = true;
-    String mockContactProviderUUID = "mockContactProviderUUID";
     Submission submission = new SubmissionTestBuilder().with("choseProviderForEveryChildInNeedOfCare", "false").build();
     HouseholdHasAtLeastOneChildWithoutAProvider condition = new HouseholdHasAtLeastOneChildWithoutAProvider(mockEnableMultipleProviders);
-    assertTrue(condition.run(submission, mockContactProviderUUID));
+    assertTrue(condition.run(submission));
   }
 }
