946883cd719ceec3c612f3a0666046eec247c36c
Ana Medrano <amedrano@codeforamerica.org>
2025-06-13 12:02:17 -0700

    Connecting Chibuisi's screen work with repeatFor

--- src/main/java/org/ilgcc/app/submission/conditions/DisplaySchedulesStartCareScreen.java
+++ src/main/java/org/ilgcc/app/submission/conditions/DisplaySchedulesStartCareScreen.java
@@ -1,32 +1,24 @@
 package org.ilgcc.app.submission.conditions;
 
-import static java.util.Collections.emptyList;
+import static org.ilgcc.app.utils.SchedulePreparerUtility.relatedSubflowIterationData;
 
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DisplaySchedulesStartCareScreen extends EnableMultipleProviders implements Condition {
 
     @Override
-    public Boolean run(Submission submission, String subflowUuid) {
+    public Boolean run(Submission submission, String subflowUuid, String repeatForIterationUuid) {
         Map<String, Object> childcareSubflow = submission.getSubflowEntryByUuid("childcareSchedules", subflowUuid);
-        return super.run(submission) && (hasSelectedOneProvider(childcareSubflow) || hasOnlyOneProvider(submission));
+        Map<String, Object> repeatForSubflowIteration = relatedSubflowIterationData(childcareSubflow, "providerSchedules",
+                repeatForIterationUuid);
+        return super.run(submission) && (providerSelected(repeatForSubflowIteration));
     }
-    private boolean hasOnlyOneProvider(Submission submission) {
-      return ((List<Map<String, Object>>) submission.getInputData().getOrDefault("providers", emptyList())).size() <= 1;
-    }
-    private boolean hasSelectedOneProvider(Map<String, Object> childcareSubflow) {
-         return Optional.ofNullable(childcareSubflow.get("currentChildcareProvider[]"))
-             .filter(List.class::isInstance)
-             .map(list -> ((List<?>) list).stream()
-                .filter(String.class::isInstance)
-                .map(String.class::cast)
-                .anyMatch(item -> !item.equals("NO_PROVIDER")))
-             .orElse(false);
+
+    private boolean providerSelected(Map<String, Object> repeatForSubflowIteration){
+        return !repeatForSubflowIteration.get("repeatForValue").equals("NO_PROVIDER");
     }
 }
