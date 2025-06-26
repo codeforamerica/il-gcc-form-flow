ead00d95b17fa3c63ec25e6da313573fd02ecde8
Marc Perlman <mperlman@codeforamerica.org>
2025-06-26 17:17:27 -0500

    renaming class

--- src/main/java/org/ilgcc/app/submission/conditions/CurrentProviderHasResponded.java
+++ src/main/java/org/ilgcc/app/submission/conditions/CurrentProviderHasResponded.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HasProviderResponse implements Condition {
+public class CurrentProviderHasResponded implements Condition {
 
     @Value("${il-gcc.enable-multiple-providers}")
     private boolean enableMultipleProviders;
@@ -15,7 +15,7 @@ public class HasProviderResponse implements Condition {
     public Boolean run(Submission submission) {
         // TODO: Get the current provider that was selected on multiple-providers and confirmed on
         // confirm-provider, and if that provider has a recorded response, this value is true
-        boolean hasResponseForProvider = false;
-        return enableMultipleProviders && hasResponseForProvider;
+        boolean currentProviderHasResponded = false;
+        return enableMultipleProviders && currentProviderHasResponded;
     }
 }
