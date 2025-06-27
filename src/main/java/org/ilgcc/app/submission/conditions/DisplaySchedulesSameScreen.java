bbc0e8ce20e31931b3bc5380b33d2a6545c6527b
chibuisienyia <cenyia@codeforamerica.org>
2025-06-26 20:21:06 -0500

    [CCAP-966] Render schedules-same.html screen.  DisplaySchedulesSameScreen conditionally

--- src/main/java/org/ilgcc/app/submission/conditions/DisplaySchedulesSameScreen.java
+++ src/main/java/org/ilgcc/app/submission/conditions/DisplaySchedulesSameScreen.java
@@ -1,12 +1,14 @@
 package org.ilgcc.app.submission.conditions;
 
+import static java.util.Collections.emptyList;
 import static org.ilgcc.app.utils.SchedulePreparerUtility.relatedSubflowIterationData;
 
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,13 +16,8 @@ public class DisplaySchedulesSameScreen extends EnableMultipleProviders implemen
 
     @Override
     public Boolean run(Submission submission, String subflowUuid, String repeatForIterationUuid) {
-        /*
-          Steps:
-          2. Get the current provider from the provider schedules
-          3. Get all the children in need of chi
-         */
+
         List<Map<String, Object>> childcareSchedules = (List) submission.getInputData().get("childcareSchedules");
-        Boolean shouldNavigateToSameScreen = false;
 
         //should only run if 2 or more childcare schedules have been entered.
         if (childcareSchedules.size() >= 2) {
@@ -29,18 +26,20 @@ public class DisplaySchedulesSameScreen extends EnableMultipleProviders implemen
             Map<String, Object> currentProviderSchedule = relatedSubflowIterationData(currentChildcareSchedule, "providerSchedules",
         repeatForIterationUuid);
             String currentProviderUuidOrNoProvider = (String) currentProviderSchedule.get("repeatForValue");
-
+            return super.run(submission) && (getAnyChildcareSchedulesWithTheSameProvider(childcareSchedules, currentProviderUuidOrNoProvider, currentChildcareSchedule).size() == 1);
         }
-        return super.run(submission) && (shouldNavigateToSameScreen);
+        return false;
     }
 
-    private boolean hasOnlyOneChildWithTheSameProviderSelected (List<Map<String, Object>> childcareSchedules, String currentProviderUuidOrNoProvider, Map<String, Object> currentChildcareSchedule) {
+    private List<Map<String, Object>> getAnyChildcareSchedulesWithTheSameProvider(List<Map<String, Object>> childcareSchedules, String currentProviderUuidOrNoProvider, Map<String, Object> currentChildcareSchedule) {
         List<Map <String, Object>> remainingChildcareSchedules = childcareSchedules.stream()
             .filter(childcareSchedule -> !childcareSchedule.equals(currentChildcareSchedule))
             .toList();
-        return false;
+        return remainingChildcareSchedules.stream().filter(childcareSchedule -> childcareScheduleIncludesThisProvider(childcareSchedule, currentProviderUuidOrNoProvider)).toList();
     }
-    private boolean providerSelected(Map<String, Object> currentProviderSchedule){
-        return !currentProviderSchedule.get("repeatForValue").equals("NO_PROVIDER");
+
+    private boolean childcareScheduleIncludesThisProvider(Map<String, Object> childcareSchedule, String providerUuidOrNoProvider) {
+        List<Map<String, Object>> providerSchedules = (List<Map<String, Object>>) Optional.ofNullable(childcareSchedule.get("providerSchedules")).orElse(emptyList());
+        return providerSchedules.stream().anyMatch(providerSchedule -> providerUuidOrNoProvider.equals(providerSchedule.getOrDefault("repeatForValue", "")));
     }
 }
