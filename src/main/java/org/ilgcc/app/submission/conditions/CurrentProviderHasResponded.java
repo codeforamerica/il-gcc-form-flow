defe6264bca8819eedbe4108b1d195f0078f0b06
Marc Perlman <mperlman@codeforamerica.org>
2025-06-27 12:15:51 -0500

    fixing test

--- src/main/java/org/ilgcc/app/submission/conditions/CurrentProviderHasResponded.java
+++ src/main/java/org/ilgcc/app/submission/conditions/CurrentProviderHasResponded.java
@@ -5,20 +5,18 @@ import formflow.library.data.Submission;
 import java.util.Map;
 import org.ilgcc.app.utils.ProviderSubmissionUtilities;
 import org.ilgcc.app.utils.enums.SubmissionStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CurrentProviderHasResponded implements Condition {
-
-    @Value("${il-gcc.enable-multiple-providers}")
-    private boolean enableMultipleProviders;
+public class CurrentProviderHasResponded extends EnableMultipleProviders implements Condition {
 
     @Override
     public Boolean run(Submission submission) {
-        Map<String, Object> currentProvider = ProviderSubmissionUtilities.getCurrentProvider(submission);
-        String providerResponseStatus = currentProvider != null ? (String) currentProvider.get("providerResponseStatus") : null;
-        boolean currentProviderHasResponded = SubmissionStatus.RESPONDED.name().equals(providerResponseStatus);
-        return enableMultipleProviders && currentProviderHasResponded;
+        if (super.run(submission)) {
+            Map<String, Object> currentProvider = ProviderSubmissionUtilities.getCurrentProvider(submission);
+            String providerResponseStatus = currentProvider != null ? (String) currentProvider.get("providerResponseStatus") : null;
+            return SubmissionStatus.RESPONDED.name().equals(providerResponseStatus);
+        }
+        return false;
     }
 }
