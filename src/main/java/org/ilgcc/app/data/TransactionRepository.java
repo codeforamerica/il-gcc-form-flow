da47196dd9bbbd585e1ccba971bd5686e5b125e0
Marc P <mperlman@codeforamerica.org>
2025-07-16 10:16:07 -0500

    [CCAP-1098] Cleaning up queries (#1739)

--- src/main/java/org/ilgcc/app/data/TransactionRepository.java
+++ src/main/java/org/ilgcc/app/data/TransactionRepository.java
@@ -12,17 +12,21 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
     @Query(value =
-            "SELECT s.* FROM submissions s " +
+            "SELECT s.* " +
+                    "FROM submissions s " +
                     "LEFT JOIN transactions t ON t.submission_id = s.id " +
                     "WHERE s.submitted_at IS NOT NULL " +
-                    "AND s.input_data->>'providerResponseStatus' = 'ACTIVE' " +
+                    "AND ( " +
+                    "s.input_data->>'providerResponseStatus' = 'ACTIVE' " +
+                    "OR s.input_data->>'providerResponseStatus' IS NULL " +
+                    ") " +
                     "AND s.input_data->>'providerApplicationResponseExpirationDate' IS NOT NULL " +
-                    "AND CAST(s.input_data->>'providerApplicationResponseExpirationDate' AS TIMESTAMPTZ) <= (now() AT TIME ZONE"
-                    + " 'UTC' AT TIME ZONE 'America/Chicago')" +
+                    "AND TO_TIMESTAMP((s.input_data->>'providerApplicationResponseExpirationDate')::double precision)::timestamptz <= (now() AT TIME ZONE 'UTC' AT TIME ZONE 'America/Chicago') " +
                     "AND s.flow = 'gcc' " +
                     "AND t.transaction_id IS NULL " +
-                    "ORDER BY s.updated_at ASC",
+                    "ORDER BY s.created_at ASC",
             nativeQuery = true)
     Set<Submission> findExpiringSubmissionsWithoutTransactions();
 
