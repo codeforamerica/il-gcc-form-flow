b64e05a2665d21df0a21172c8995e32fa8638b78
chibuisienyia <cenyia@codeforamerica.org>
2025-07-16 12:47:59 -0500

    [CCAP-1147] Removed unnecessary code

--- src/main/java/org/ilgcc/app/submission/actions/UpdateCurrentChildcareProviderIfOneOrNoProviders.java
+++ src/main/java/org/ilgcc/app/submission/actions/UpdateCurrentChildcareProviderIfOneOrNoProviders.java
@@ -8,7 +8,7 @@ import formflow.library.data.Submission;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import org.ilgcc.app.utils.SubmissionUtilities;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,21 +23,13 @@ public class UpdateCurrentChildcareProviderIfOneOrNoProviders implements Action 
 
         if(childcareProvidersForCurrentChild.isEmpty()) {
             List<Map<String, Object>> providers = (List<Map<String, Object>>) inputData.getOrDefault("providers", emptyList());
-            if (hasNoProviders(inputData)) {
+            if (SubmissionUtilities.isNoProviderSubmission(inputData)) {
                 childcareProvidersForCurrentChild.add("NO_PROVIDER");
             }
-            if (providers.size() == 1 && allChildrenHaveProviders(inputData)) {
+            if (providers.size() == 1) {
                 childcareProvidersForCurrentChild.add(providers.getFirst().getOrDefault("uuid", "").toString());
             }
             formSubmission.getFormData().put("childcareProvidersForCurrentChild[]", childcareProvidersForCurrentChild);
         }
-    }
-
-    private boolean hasNoProviders(Map<String, Object> inputData)  {
-        return inputData.getOrDefault("hasChosenProvider", "false").equals("false");
-    }
-
-    private boolean allChildrenHaveProviders(Map<String, Object> inputData) {
-        return inputData.getOrDefault("choseProviderForEveryChildInNeedOfCare", "false").equals("true");
     }
 }
