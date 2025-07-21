39b9a5a1ca648db8a5ce1954a3faaad44f11a0c8
Marc P <mperlman@codeforamerica.org>
2025-07-21 15:27:24 -0500

    [CCAP-1180] Refactoring to remove duplicate email job code (#1768)

--- src/main/java/org/ilgcc/app/email/SendEmail.java
+++ src/main/java/org/ilgcc/app/email/SendEmail.java
@@ -106,7 +106,7 @@ public abstract class SendEmail {
     protected void sendEmail(ILGCCEmail email, Submission submission, String subflowName, Map<String, Object> subflowData, int offsetDelaySeconds) {
         log.info("{}: About to enqueue the Send Email Job for submissionId: {}",
                 email.getEmailType().getDescription(), submission.getId());
-        sendEmailJob.enqueueSendEmailJob(email, offsetDelaySeconds);
+        sendEmailJob.enqueueSendSubmissionEmailJob(email, offsetDelaySeconds);
         updateEmailStatus(submission, subflowName, subflowData);
     }
 
