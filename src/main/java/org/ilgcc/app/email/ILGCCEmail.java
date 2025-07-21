582f421952c2c70c848c348dcff5a4ac12e7706d
Marc P <mperlman@codeforamerica.org>
2025-07-21 12:08:06 -0500

    [CCAP-1180] Making other objects thread safe in the email chain (#1763)

--- src/main/java/org/ilgcc/app/email/ILGCCEmail.java
+++ src/main/java/org/ilgcc/app/email/ILGCCEmail.java
@@ -1,17 +1,13 @@
 package org.ilgcc.app.email;
 
-import jakarta.validation.constraints.NotNull;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.Getter;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
-@NoArgsConstructor
-@Getter
 public class ILGCCEmail implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -19,48 +15,76 @@ public class ILGCCEmail implements Serializable {
     public static final String FROM_ADDRESS = "noreply@getchildcareil.org";
     public static final String EMAIL_SENDER_KEY = "email.general.sender-name";
 
-    private Email senderEmail;
-    private String subject;
-    private Content body;
-    private EmailType emailType;
-    private UUID submissionId;
-    private List<Email> recipientEmails = new ArrayList<>();
-    private String orgId;
+    private final Email senderEmail;
+    private final String subject;
+    private final Content body;
+    private final EmailType emailType;
+    private final UUID submissionId;
+    private final List<Email> recipientEmails;
+    private final String orgId;
+
+    public ILGCCEmail(@NotNull List<String> recipientAddresses, ILGCCEmailTemplate emailTemplate, String orgId) {
+        this.senderEmail = new Email(emailTemplate.getSenderEmail().getEmail(), emailTemplate.getSenderEmail().getName());
+        this.subject = emailTemplate.getSubject();
+        this.body = new Content(emailTemplate.getBody().getType(), emailTemplate.getBody().getValue());
+        this.emailType = emailTemplate.getEmailType();
+        this.submissionId = null;
+        this.orgId = orgId;
+        this.recipientEmails = recipientAddresses.stream().map(Email::new).toList();
+    }
 
     public ILGCCEmail(@NotNull String recipientAddress, ILGCCEmailTemplate emailTemplate, UUID submissionId) {
         this.senderEmail = emailTemplate.getSenderEmail();
-        this.recipientEmails.add(new Email(recipientAddress));
         this.subject = emailTemplate.getSubject();
         this.body = emailTemplate.getBody();
         this.emailType = emailTemplate.getEmailType();
         this.submissionId = submissionId;
+        this.orgId = null;
+        this.recipientEmails = List.of(new Email(recipientAddress));
     }
 
-    public ILGCCEmail(@NotNull List<String> recipientAddresses, ILGCCEmailTemplate emailTemplate, String orgId) {
-        this.senderEmail = emailTemplate.getSenderEmail();
-        for (String recipientAddress : recipientAddresses) {
-            this.recipientEmails.add(new Email(recipientAddress));
-        }
-        this.subject = emailTemplate.getSubject();
-        this.body = emailTemplate.getBody();
-        this.emailType = emailTemplate.getEmailType();
-        this.orgId = orgId;
+    public Email getSenderEmail() {
+        return senderEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Content getBody() {
+        return body;
+    }
+
+    public EmailType getEmailType() {
+        return emailType;
+    }
+
+    public UUID getSubmissionId() {
+        return submissionId;
+    }
+
+    public List<Email> getRecipientEmails() {
+        return recipientEmails;
+    }
+
+    public String getOrgId() {
+        return orgId;
     }
 
     @Getter
     public enum EmailType {
-        FAMILY_CONFIRMATION_EMAIL("Family Confirmation Email"),
-        FAMILY_CONFIRMATION_EMAIL_NO_PROVIDER("No Provider Family Confirmation Email"),
-        PROVIDER_AGREES_TO_CARE_FAMILY_EMAIL("Provider Agrees to Care Family Email"),
-        PROVIDER_CONFIRMATION_EMAIL("Provider Confirmation Email"),
-        UNIDENTIFIED_PROVIDER_CONFIRMATION_EMAIL("Unidentified Provider Confirmation Email"),
-        PROVIDER_DECLINES_CARE_FAMILY_EMAIL("Provider Declines Care Family Email"),
-        PROVIDER_DID_NOT_RESPOND_FAMILY_EMAIL("Provider Did Not Respond Family Email"),
-        NEW_PROVIDER_AGREES_TO_CARE_FAMILY_EMAIL("New Provider Agrees to Care Family Email"),
-        NEW_PROVIDER_CONFIRMATION_EMAIL("New Provider Confirmation Email"),
-        AUTOMATED_PROVIDER_OUTREACH_EMAIL("Automated Provider Outreach Email"),
-        DAILY_NEW_APPLICATIONS_PROVIDER_EMAIL("Automated Email going to Processing Organizations Daily"),
-        FAMILY_APPLICATION_TRANSMITTED_CONFIRMATION_EMAIL("Family Application Transmitted Email");
+        FAMILY_CONFIRMATION_EMAIL("Family Confirmation Email"), FAMILY_CONFIRMATION_EMAIL_NO_PROVIDER(
+                "No Provider Family Confirmation Email"), PROVIDER_AGREES_TO_CARE_FAMILY_EMAIL(
+                "Provider Agrees to Care Family Email"), PROVIDER_CONFIRMATION_EMAIL(
+                "Provider Confirmation Email"), UNIDENTIFIED_PROVIDER_CONFIRMATION_EMAIL(
+                "Unidentified Provider Confirmation Email"), PROVIDER_DECLINES_CARE_FAMILY_EMAIL(
+                "Provider Declines Care Family Email"), PROVIDER_DID_NOT_RESPOND_FAMILY_EMAIL(
+                "Provider Did Not Respond Family Email"), NEW_PROVIDER_AGREES_TO_CARE_FAMILY_EMAIL(
+                "New Provider Agrees to Care Family Email"), NEW_PROVIDER_CONFIRMATION_EMAIL(
+                "New Provider Confirmation Email"), AUTOMATED_PROVIDER_OUTREACH_EMAIL(
+                "Automated Provider Outreach Email"), DAILY_NEW_APPLICATIONS_PROVIDER_EMAIL(
+                "Automated Email going to Processing Organizations Daily"), FAMILY_APPLICATION_TRANSMITTED_CONFIRMATION_EMAIL(
+                "Family Application Transmitted Email");
         private final String description;
 
         EmailType(String description) {
@@ -68,5 +92,4 @@ public class ILGCCEmail implements Serializable {
         }
 
     }
-
 }
