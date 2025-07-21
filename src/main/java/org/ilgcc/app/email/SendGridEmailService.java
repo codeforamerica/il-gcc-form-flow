4471d9f5bd5e7b9cee16eada02263a892e9abdc1
Marc P <mperlman@codeforamerica.org>
2025-07-21 10:59:48 -0500

    [CCAP-1180] Making SendGridEmailService thread safe (#1759)

--- src/main/java/org/ilgcc/app/email/SendGridEmailService.java
+++ src/main/java/org/ilgcc/app/email/SendGridEmailService.java
@@ -5,33 +5,42 @@ import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 import com.sendgrid.helpers.mail.objects.Personalization;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class SendGridEmailService {
 
-    @Autowired
-    MessageSource messageSource;
-
     private final SendGrid sendGrid = new SendGrid(System.getenv("SENDGRID_API_KEY"));
 
     public Response sendEmail(ILGCCEmail ilgccEmail) throws IOException {
-        Mail mail = new Mail();
-        mail.setFrom(ilgccEmail.getSenderEmail());
-        mail.setSubject(ilgccEmail.getSubject());
-        mail.addContent(ilgccEmail.getBody());
+        // Defensive copies
+        Email fromEmail = new Email(ilgccEmail.getSenderEmail().getEmail(), ilgccEmail.getSenderEmail().getName());
+        String subject = ilgccEmail.getSubject();
+        Content content = new Content("text/html", ilgccEmail.getBody().getValue());
 
-        for (Email email : ilgccEmail.getRecipientEmails()) {
+        // Mail
+        Mail mail = new Mail();
+        mail.setFrom(fromEmail);
+        mail.setSubject(subject);
+        mail.addContent(content);
+
+        // Defensive copy of recipient list
+        List<Email> recipients = new ArrayList<>(ilgccEmail.getRecipientEmails());
+        for (Email recipient : recipients) {
             Personalization personalization = new Personalization();
-            personalization.addTo(email);
+            personalization.addTo(new Email(recipient.getEmail(), recipient.getName()));
             mail.addPersonalization(personalization);
         }
 
+        // Send request
         Request request = new Request();
         request.setMethod(Method.POST);
         request.setEndpoint("mail/send");
