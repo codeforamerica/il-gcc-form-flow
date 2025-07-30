353c95dd1135fe833634aa22857f9c3e3ea567fc
chibuisienyia <cenyia@codeforamerica.org>
2025-07-30 17:43:06 -0500

    [CCAP-996] Generate SendProviderConfirmationAfterResponseEmailTemplate

---
+++ src/main/java/org/ilgcc/app/email/templates/SendProviderConfirmationAfterResponseEmailTemplate.java
@@ -0,0 +1,60 @@
+package org.ilgcc.app.email.templates;
+
+import static org.ilgcc.app.email.ILGCCEmail.FROM_ADDRESS;
+
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import java.util.Locale;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+import org.ilgcc.app.email.ILGCCEmail;
+import org.ilgcc.app.email.ILGCCEmail.EmailType;
+import org.ilgcc.app.email.ILGCCEmailTemplate;
+import org.springframework.context.MessageSource;
+
+@Getter
+@Setter
+public class SendProviderConfirmationAfterResponseEmailTemplate {
+
+    private Map<String, Object> emailData;
+    private MessageSource messageSource;
+    private Locale locale;
+
+   public SendProviderConfirmationAfterResponseEmailTemplate(Map<String, Object> emailData, MessageSource messageSource, Locale locale) {
+       this.emailData = emailData;
+       this.messageSource = messageSource;
+       this.locale = locale;
+
+   }
+   public ILGCCEmailTemplate createTemplate(){
+       return new ILGCCEmailTemplate(senderEmail(), setSubject(), new Content("text/html", setBodyCopy(emailData)), EmailType.PROVIDER_CONFIRMATION_AFTER_RESPONSE_EMAIL);
+   }
+
+    private Email senderEmail() {
+        return  new Email(FROM_ADDRESS, messageSource.getMessage(ILGCCEmail.EMAIL_SENDER_KEY, null, locale));
+    }
+
+    private String setSubject() {
+        return messageSource.getMessage("email.general.subject.confirmation-code", new Object[]{emailData.get("confirmationCode")}, locale);
+    }
+    //TODO: SET UP EMAIL DATA TO GET CHILDREN INITIALS, CCAP START DATE, CCRRNAME, CONFIRMATION_CODE
+    private String setBodyCopy(Map<String, Object> emailData) {
+        String p1 = messageSource.getMessage("email.provider-confirmation-after-response.p1", null, locale);
+        String p2 = messageSource.getMessage("email.provider-confirmation-after-response.p2",null, locale);
+        String p3 = messageSource.getMessage("email.provider-confirmation-after-response.p3",
+                new Object[]{emailData.get("childrenInitials"), emailData.get("ccapStartDate")}, locale);
+        String p4 = messageSource.getMessage("email.provider-confirmation-after-response.p4", new Object[]{emailData.get("confirmationCode")},
+                locale);
+        String p5 = messageSource.getMessage("email.provider-confirmation-after-response.p5", new Object[]{emailData.get("ccrrName")}, locale);
+        String familyHasMoreThanOneProviderParagraph = messageSource.getMessage("email.provider-confirmation-after-response.p6", new Object[]{emailData.get("hasMoreThanOneProvider")}, locale);
+        String familyIsNewProviderParagraph = messageSource.getMessage("email.provider-confirmation-after-response.p7", new Object[]{emailData.get("ccrrName")}, locale);
+        String familyIsNewProviderList = messageSource.getMessage("email.provider-confirmation-after-response.p8.ul", null, locale);
+
+        String p6 = messageSource.getMessage("email.general.footer.automated-response", null, locale);
+        String p7 = messageSource.getMessage("email.general.footer.cfa", null, locale);
+
+        return p1 + p2 + p3 + p4 + p5 + p6;
+    }
+
+}
