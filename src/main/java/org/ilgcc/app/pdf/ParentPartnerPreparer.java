9be4ba770caa53b808020945682cd54ef3ae7e39
Khushal.vora <kv4alll@gmail.com>
2025-08-04 13:43:45 -0500

    [CCAP-1185] : If partner is working or in school, indicate it on PDF yes/no questions on page 5

--- src/main/java/org/ilgcc/app/pdf/ParentPartnerPreparer.java
+++ src/main/java/org/ilgcc/app/pdf/ParentPartnerPreparer.java
@@ -44,17 +44,14 @@ public class ParentPartnerPreparer implements SubmissionFieldPreparer {
 
         //partner dob
         Optional<LocalDate> parentPartnerDateOfBirth = getDateInput(submission, "parentPartnerBirth");
-        if (parentPartnerDateOfBirth.isPresent()) {
-            LocalDate dob = parentPartnerDateOfBirth.get();
-            results.put(
-                    "parentPartnerDateOfBirth",
-                    new SingleField(
-                            "parentPartnerDateOfBirth",
-                            dob.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
-                            null
-                    )
-            );
-        }
+        parentPartnerDateOfBirth.ifPresent(dob -> results.put(
+                "parentPartnerDateOfBirth",
+                new SingleField(
+                        "parentPartnerDateOfBirth",
+                        dob.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+                        null
+                )
+        ));
         //Term Start Date
         String partnerProgramStart = getDateInputWithDayOptional(submission, "partnerProgramStart");
         if(!partnerProgramStart.isEmpty()){
