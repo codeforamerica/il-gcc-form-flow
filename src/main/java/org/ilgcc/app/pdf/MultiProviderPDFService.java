badf9ee2af4dddb5ee504f8aed106ea8e744d0b1
puneeth.bandi <bandipuneeth003@gmail.com>
2025-08-06 08:59:31 -0500

    [CCAP-1183] Refactored MultiProviderPDFService.java logic to ProviderSubmissionFieldPreparerService.java for proper data mapping in PDF output

--- src/main/java/org/ilgcc/app/pdf/MultiProviderPDFService.java
+++ src/main/java/org/ilgcc/app/pdf/MultiProviderPDFService.java
@@ -92,12 +92,6 @@ public class MultiProviderPDFService {
     }
 
     public Map<String, byte[]> generatePDFs(Submission submission) throws IOException {
-
-        List<Map<String, Object>> providers = SubmissionUtilities.getProviders(submission.getInputData());
-        boolean hasMultipleProviders = providers.size() > 1;
-
-        submission.getInputData().put("applicantMultipleproviders", hasMultipleProviders ? "true" : "false");
-
         Map<String, byte[]> allFiles = new HashMap<>();
         allFiles.put(getCCMSFileNameForApplicationPDF(submission), pdfService.getFilledOutPDF(submission));
 
