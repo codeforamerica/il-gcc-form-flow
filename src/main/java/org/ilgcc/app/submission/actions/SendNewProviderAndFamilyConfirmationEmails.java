088c09c18406247e9bc973980d8ea6cc70b82f0f
chibuisienyia <cenyia@codeforamerica.org>
2025-08-06 17:43:31 -0500

    [CCAP-996] Remove SendNewProviderAndFamilyConfirmationEmails

--- src/main/java/org/ilgcc/app/submission/actions/SendNewProviderAndFamilyConfirmationEmails.java
+++ src/main/java/org/ilgcc/app/submission/actions/SendNewProviderAndFamilyConfirmationEmails.java
@@ -3,8 +3,8 @@ package org.ilgcc.app.submission.actions;
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
 import lombok.extern.slf4j.Slf4j;
+import org.ilgcc.app.email.ProviderRespondedConfirmationEmail;
 import org.ilgcc.app.email.SendNewProviderAgreesToCareFamilyConfirmationEmail;
-import org.ilgcc.app.email.SendNewProviderConfirmationEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +16,11 @@ public class SendNewProviderAndFamilyConfirmationEmails implements Action {
     SendNewProviderAgreesToCareFamilyConfirmationEmail sendNewProviderAgreesToCareFamilyConfirmationEmail;
 
     @Autowired
-    SendNewProviderConfirmationEmail sendNewProviderConfirmationEmail;
+    ProviderRespondedConfirmationEmail providerRespondedConfirmationEmail;
 
     @Override
     public void run(Submission submission) {
         sendNewProviderAgreesToCareFamilyConfirmationEmail.send(submission);
-        sendNewProviderConfirmationEmail.send(submission);
+        providerRespondedConfirmationEmail.send(submission);
     }
 }
