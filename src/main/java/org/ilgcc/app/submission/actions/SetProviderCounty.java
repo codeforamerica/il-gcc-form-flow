4f40f2af2a80b3229bf3ac2aaf43550f8dfeaa4d
Mahbubullah Mohammed <Mahbubullah.mohammed@krasanconsulting.com>
2025-08-07 08:46:40 -0500

    [CCAP-1184] -Populate provider's county in PROVIDER_ADDRESS_COUNTY on PDF

--- src/main/java/org/ilgcc/app/submission/actions/SetProviderCounty.java
+++ src/main/java/org/ilgcc/app/submission/actions/SetProviderCounty.java
@@ -2,17 +2,17 @@ package org.ilgcc.app.submission.actions;
 
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
-import  java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.ilgcc.app.data.County;
-import org.ilgcc.app.data.ResourceOrganization;
-import org.ilgcc.app.data.CCMSDataServiceImpl;
-import org.ilgcc.app.submission.router.ApplicationRouterService;
 import formflow.library.data.SubmissionRepositoryService;
 import lombok.extern.slf4j.Slf4j;
+import org.ilgcc.app.data.CCMSDataServiceImpl;
+import org.ilgcc.app.data.County;
+import org.ilgcc.app.submission.router.ApplicationRouterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -23,25 +23,24 @@ public class SetProviderCounty implements Action {
 
     @Autowired
     ApplicationRouterService applicationRouterService;
-
     @Autowired
     CCMSDataServiceImpl ccmsDataServiceImpl;
-
     private static final String PROVIDER_ZIP_INPUT_NAME = "providerResponseServiceZipCode";
     private static final String PROVIDER_COUNTY_OUTPUT_NAME = "providerResponseAddressCounty";
+
     @Override
     public void run(Submission submission) {
         Map<String, Object> inputData = submission.getInputData();
-        if(inputData.containsKey(PROVIDER_ZIP_INPUT_NAME)) {
+        if (inputData.containsKey(PROVIDER_ZIP_INPUT_NAME)) {
             String providerZip = (String) inputData.get(PROVIDER_ZIP_INPUT_NAME);
             if (providerZip != null && providerZip.length() >= 5) {
                 Optional<County> providerCountyOpt = ccmsDataServiceImpl.getCountyByZipCode(providerZip);
-                   providerCountyOpt.ifPresentOrElse(county -> inputData.put(PROVIDER_COUNTY_OUTPUT_NAME, county.getCounty()), () -> inputData.put(PROVIDER_COUNTY_OUTPUT_NAME, null));
+                providerCountyOpt.ifPresentOrElse(county -> { inputData.put(PROVIDER_COUNTY_OUTPUT_NAME, county.getCounty());
+            },() -> inputData.put(PROVIDER_COUNTY_OUTPUT_NAME, null));
             }
         }
         boolean experiencingHomelessness = inputData.getOrDefault("parentHomeExperiencingHomelessness[]", "no").equals(
                 List.of("yes"));
         submissionRepositoryService.save(submission);
-
-}
+    }
 }
