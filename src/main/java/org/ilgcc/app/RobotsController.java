a652bf737b874b366005deb6eb8c5e5459803614
spokenbird <agonzalez@codeforamerica.org>
2025-08-11 13:25:54 -0700

    Make sitemap discoverable with full URL in prod

--- src/main/java/org/ilgcc/app/RobotsController.java
+++ src/main/java/org/ilgcc/app/RobotsController.java
@@ -10,18 +10,21 @@ public class RobotsController {
 
     @Value("${spring.profiles.active:default}")
     private String activeProfile;
+    
+    @Value("${il-gcc.base-url}")
+    private String baseUrl;
 
     @GetMapping(value = "/robots.txt", produces = MediaType.TEXT_PLAIN_VALUE)
     public String getRobotsTxt() {
         if ("production".equalsIgnoreCase(activeProfile)) {
-            return """
+            return String.format("""
                 User-agent: *
                 Disallow: /
                 Allow: /$
                 Allow: /faq$
                 Allow: /privacy$
-                Sitemap: /sitemap.xml
-                """;
+                Sitemap: %s/sitemap.xml
+                """, baseUrl);
         } else {
             return """
                 User-agent: *
