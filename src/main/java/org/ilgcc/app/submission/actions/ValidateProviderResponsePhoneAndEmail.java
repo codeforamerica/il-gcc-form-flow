cb94507533e6217ecf47d73a94e85936c9bf6d6d
Khushal.vora <kv4alll@gmail.com>
2025-08-15 11:35:14 -0500

    [CCAP-1100] : Require phone number or email address for providers

--- src/main/java/org/ilgcc/app/submission/actions/ValidateProviderResponsePhoneAndEmail.java
+++ src/main/java/org/ilgcc/app/submission/actions/ValidateProviderResponsePhoneAndEmail.java
@@ -4,6 +4,7 @@ package org.ilgcc.app.submission.actions;
 import formflow.library.config.submission.Action;
 import formflow.library.data.FormSubmission;
 import formflow.library.data.Submission;
+import formflow.library.utils.RegexUtils;
 import org.ilgcc.app.data.ProviderRepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -14,17 +15,13 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import static org.ilgcc.app.inputs.InputsConstants.PHONE_REGEX;
+
 @Component
 public class ValidateProviderResponsePhoneAndEmail implements Action {
 
     @Autowired
     MessageSource messageSource;
-
-    ProviderRepositoryService providerRepositoryService;
-
-    public ValidateProviderResponsePhoneAndEmail(ProviderRepositoryService providerRepositoryService) {
-        this.providerRepositoryService = providerRepositoryService;
-    }
 
     private final String EMAIL = "providerResponseContactEmail";
     private final String PHONE = "providerResponseContactPhoneNumber";
@@ -39,6 +36,12 @@ public class ValidateProviderResponsePhoneAndEmail implements Action {
         if (email.isBlank() && phone.isBlank()) {
             errorMessages.put(EMAIL, List.of(messageSource.getMessage("errors.require-email", null, locale)));
             errorMessages.put(PHONE, List.of(messageSource.getMessage("errors.invalid-phone-number", null, locale)));
+        }
+        if (!phone.isBlank() && !phone.matches(PHONE_REGEX)) {
+            errorMessages.put(PHONE, List.of(messageSource.getMessage("errors.invalid-phone-number", null, locale)));
+        }
+        if (!email.isBlank() && !email.matches(RegexUtils.EMAIL_REGEX)) {
+            errorMessages.put(EMAIL, List.of(messageSource.getMessage("errors.invalid-email.no-suggested-email-address", null, locale)));
         }
         return errorMessages;
     }
