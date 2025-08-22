5378b70071bff2691f1d13163a7b5718d078211c
chibuisienyia <cenyia@codeforamerica.org>
2025-08-22 14:22:19 -0500

    [CCAP-1263] Add ValidateBirthdateTest

---
+++ src/test/java/org/ilgcc/app/submission/actions/ValidateBirthdateTest.java
@@ -0,0 +1,10 @@
+package org.ilgcc.app.submission.actions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.context.MessageSource;
+
+class ValidateBirthdateTest {
+//  private final MessageSource messageSource;
+
+}
