7cdbe666c309a73fdb461517e2c63918c93ee454
chibuisienyia <cenyia@codeforamerica.org>
2025-09-10 19:13:58 -0500

    [CCAP-1342] Remove white spaces from email fields

---
+++ src/main/java/org/ilgcc/app/submission/actions/TrimEmailOnPost.java
@@ -0,0 +1,28 @@
+package org.ilgcc.app.submission.actions;
+
+import formflow.library.config.submission.Action;
+import formflow.library.data.FormSubmission;
+import formflow.library.data.Submission;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Pattern;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrimEmailOnPost implements Action {
+  @Override
+  public void run(FormSubmission formSubmission, Submission submission){
+    AtomicReference<String> emailKey = new AtomicReference<>("");
+    AtomicReference<String> trimmedEmail = new AtomicReference<>("");
+    Map<String, Object> formData = formSubmission.getFormData();
+    formData.forEach((key, value) -> {
+      if(Pattern.compile(Pattern.quote("email"), Pattern.CASE_INSENSITIVE).matcher(key).find()){
+        emailKey.set(key);
+        trimmedEmail.set(value.toString().trim());
+      }
+    });
+    if(!emailKey.get().isEmpty()){
+      formData.put(emailKey.get(), trimmedEmail.get());
+    }
+  }
+}
