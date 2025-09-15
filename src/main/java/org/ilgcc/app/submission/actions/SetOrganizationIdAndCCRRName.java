974740b7a6d53367f8de9b346ff59064499673be
Ana Medrano <amedrano@codeforamerica.org>
2025-09-15 13:41:38 -0700

    [CCAP-1337] - remove logic that pre checks the preferred comms when a user is unhoused

--- src/main/java/org/ilgcc/app/submission/actions/SetOrganizationIdAndCCRRName.java
+++ src/main/java/org/ilgcc/app/submission/actions/SetOrganizationIdAndCCRRName.java
@@ -31,6 +31,7 @@ public class SetOrganizationIdAndCCRRName implements Action {
     private static final String ZIP_CODE_INPUT_NAME = "parentHomeZipCode";
     private static final String APPLICATION_COUNTY_INPUT_NAME = "applicationCounty";
     private static final String APPLICATION_ZIPCODE_INPUT_NAME = "applicationZipCode";
+    private static final String APPLICANT_COUNTY_INPUT_NAME = "applicantAddressCounty";
 
     @Override
     public void run(Submission submission) {
