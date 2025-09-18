ee8cc97dd0782a58b6c57bc853840a0091267489
chibuisienyia <cenyia@codeforamerica.org>
2025-09-18 17:53:44 -0500

    [CCAP-868] Update emails to only send on active submissions

--- src/main/java/org/ilgcc/app/submission/actions/SendUnidentifiedProviderAndFamilyConfirmationEmailsAndUploadSubmission.java
+++ src/main/java/org/ilgcc/app/submission/actions/SendUnidentifiedProviderAndFamilyConfirmationEmailsAndUploadSubmission.java
@@ -2,11 +2,14 @@ package org.ilgcc.app.submission.actions;
 
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepositoryService;
 import java.util.Optional;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.ilgcc.app.data.SubmissionSenderService;
 import org.ilgcc.app.email.SendProviderDidNotRespondToFamilyEmail;
 import org.ilgcc.app.email.SendUnidentifiedProviderConfirmationEmail;
+import org.ilgcc.app.utils.ProviderSubmissionUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +23,9 @@ public class SendUnidentifiedProviderAndFamilyConfirmationEmailsAndUploadSubmiss
     @Autowired
     SendUnidentifiedProviderConfirmationEmail sendUnidentifiedProviderConfirmationEmail;
 
+    @Autowired
+    SubmissionRepositoryService submissionRepositoryService;
+
     private final SubmissionSenderService submissionSenderService;
 
     public SendUnidentifiedProviderAndFamilyConfirmationEmailsAndUploadSubmission(
@@ -32,9 +38,29 @@ public class SendUnidentifiedProviderAndFamilyConfirmationEmailsAndUploadSubmiss
     public void run(Submission providerSubmission) {
         // Because this is an unidentified provider, they don't get to upload documents and the job should be
         // enqueued instantly
-        submissionSenderService.sendProviderSubmissionInstantly(providerSubmission,
+        // If the providerSubmission has expired before this action has run, we should not send the emails linked to this action
+        if (providerSubmissionIsActive(providerSubmission)) {
+            submissionSenderService.sendProviderSubmissionInstantly(providerSubmission,
                 Optional.of(sendProviderDidNotRespondToFamilyEmail));
 
-        sendUnidentifiedProviderConfirmationEmail.send(providerSubmission);
+            sendUnidentifiedProviderConfirmationEmail.send(providerSubmission);
+        }
+    }
+
+    public boolean providerSubmissionIsActive(Submission providerSubmission) {
+        Optional<Submission> familySubmissionOptional = getFamilySubmission(providerSubmission);
+        String currentProviderUuid = (String) providerSubmission.getInputData().getOrDefault("currentProviderUuid", "");
+        if (familySubmissionOptional.isPresent() && !currentProviderUuid.isEmpty()) {
+            return !(ProviderSubmissionUtilities.hasProviderApplicationExpired(familySubmissionOptional.get(), providerSubmission));
+        }
+        return true;
+    }
+
+    public Optional<Submission> getFamilySubmission(Submission providerSubmission) {
+        Optional<UUID> familySubmissionId = ProviderSubmissionUtilities.getFamilySubmissionId(providerSubmission);
+        if (familySubmissionId.isPresent()) {
+            return submissionRepositoryService.findById(familySubmissionId.get());
+        }
+        return Optional.empty();
     }
 }
