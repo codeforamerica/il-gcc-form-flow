e3fb2235c5781fd9dcc8e576ae6908aa48a96f2c
chibuisienyia <cenyia@codeforamerica.org>
2025-09-18 17:53:44 -0500

    [CCAP-868] Update emails to only send on active submissions

--- src/main/java/org/ilgcc/app/submission/actions/SendNewProviderAndFamilyConfirmationEmails.java
+++ src/main/java/org/ilgcc/app/submission/actions/SendNewProviderAndFamilyConfirmationEmails.java
@@ -2,9 +2,13 @@ package org.ilgcc.app.submission.actions;
 
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepositoryService;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.ilgcc.app.email.SendProviderRespondedConfirmationEmail;
 import org.ilgcc.app.email.SendNewProviderAgreesToCareFamilyConfirmationEmail;
+import org.ilgcc.app.utils.ProviderSubmissionUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +22,31 @@ public class SendNewProviderAndFamilyConfirmationEmails implements Action {
     @Autowired
     SendProviderRespondedConfirmationEmail sendProviderRespondedConfirmationEmail;
 
+    @Autowired
+    SubmissionRepositoryService submissionRepositoryService;
+
     @Override
-    public void run(Submission submission) {
-        sendNewProviderAgreesToCareFamilyConfirmationEmail.send(submission);
-        sendProviderRespondedConfirmationEmail.send(submission);
+    public void run(Submission providerSubmission) {
+        if (providerSubmissionIsActive(providerSubmission)) {
+            sendNewProviderAgreesToCareFamilyConfirmationEmail.send(providerSubmission);
+            sendProviderRespondedConfirmationEmail.send(providerSubmission);
+        }
+    }
+
+    public boolean providerSubmissionIsActive(Submission providerSubmission) {
+        Optional<Submission> familySubmissionOptional = getFamilySubmission(providerSubmission);
+        String currentProviderUuid = (String) providerSubmission.getInputData().getOrDefault("currentProviderUuid", "");
+        if (familySubmissionOptional.isPresent() && !currentProviderUuid.isEmpty()) {
+            return !(ProviderSubmissionUtilities.hasProviderApplicationExpired(familySubmissionOptional.get(), providerSubmission));
+        }
+        return true;
+    }
+
+    public Optional<Submission> getFamilySubmission(Submission providerSubmission) {
+        Optional<UUID> familySubmissionId = ProviderSubmissionUtilities.getFamilySubmissionId(providerSubmission);
+        if (familySubmissionId.isPresent()) {
+            return submissionRepositoryService.findById(familySubmissionId.get());
+        }
+        return Optional.empty();
     }
 }
--- src/main/java/org/ilgcc/app/submission/actions/SendProviderAndFamilyEmails.java
+++ src/main/java/org/ilgcc/app/submission/actions/SendProviderAndFamilyEmails.java
@@ -1,6 +1,9 @@
 package org.ilgcc.app.submission.actions;
 
 import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepositoryService;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.ilgcc.app.email.SendProviderAgreesToCareFamilyConfirmationEmail;
 import org.ilgcc.app.email.SendProviderRespondedConfirmationEmail;
@@ -22,14 +25,36 @@ public class SendProviderAndFamilyEmails implements Action {
     @Autowired
     SendProviderDeclinesCareFamilyConfirmationEmail sendProviderDeclinesCareFamilyConfirmationEmail;
 
+    @Autowired
+    SubmissionRepositoryService submissionRepositoryService;
+
     @Override
-    public void run(Submission submission) {
+    public void run(Submission providerSubmission) {
         // If a provider is an existing provider that has done CCAP stuff before, send emails
         // New Provider Registration will send the emails later
-        if (!ProviderSubmissionUtilities.isProviderRegistering(submission)) {
-            sendProviderAgreesToCareFamilyConfirmationEmail.send(submission);
-            sendProviderDeclinesCareFamilyConfirmationEmail.send(submission);
-            sendProviderRespondedConfirmationEmail.send(submission);
+        // if the providerSubmission has expired then we should not send the emails linked to this action
+
+        if (!ProviderSubmissionUtilities.isProviderRegistering(providerSubmission) && providerSubmissionIsActive(providerSubmission)) {
+            sendProviderAgreesToCareFamilyConfirmationEmail.send(providerSubmission);
+            sendProviderDeclinesCareFamilyConfirmationEmail.send(providerSubmission);
+            sendProviderRespondedConfirmationEmail.send(providerSubmission);
         }
     }
+    public boolean providerSubmissionIsActive(Submission providerSubmission) {
+        Optional<Submission> familySubmissionOptional = getFamilySubmission(providerSubmission);
+        String currentProviderUuid = (String) providerSubmission.getInputData().getOrDefault("currentProviderUuid", "");
+        if (familySubmissionOptional.isPresent() && !currentProviderUuid.isEmpty()) {
+            return !(ProviderSubmissionUtilities.hasProviderApplicationExpired(familySubmissionOptional.get(), providerSubmission));
+        }
+        return true;
+    }
+
+    public Optional<Submission> getFamilySubmission(Submission providerSubmission) {
+        Optional<UUID> familySubmissionId = ProviderSubmissionUtilities.getFamilySubmissionId(providerSubmission);
+        if (familySubmissionId.isPresent()) {
+            return submissionRepositoryService.findById(familySubmissionId.get());
+        }
+        return Optional.empty();
+    }
+
 }
