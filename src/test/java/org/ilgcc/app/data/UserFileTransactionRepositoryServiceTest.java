52f292fc8ff2250225162ab782ee4b689ec0f790
spokenbird <agonzalez@codeforamerica.org>
2025-09-23 10:16:16 -0700

    Add test profile

--- src/test/java/org/ilgcc/app/data/UserFileTransactionRepositoryServiceTest.java
+++ src/test/java/org/ilgcc/app/data/UserFileTransactionRepositoryServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @EntityScan(basePackageClasses = {
@@ -24,6 +25,7 @@ import org.springframework.context.annotation.Import;
 })
 @Import(UserFileTransactionRepositoryService.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class UserFileTransactionRepositoryServiceTest {
 
     @Autowired TestEntityManager testEntityManager;
