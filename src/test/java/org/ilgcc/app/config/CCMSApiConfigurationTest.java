dbbc768a09af21863ded97c873e1dad2b3a258c4
Marc P <mperlman@codeforamerica.org>
2025-09-24 10:51:43 -0500

    [CCAP-1355] Cleaning up CCMS integration flag (#1977)

--- src/test/java/org/ilgcc/app/config/CCMSApiConfigurationTest.java
+++ src/test/java/org/ilgcc/app/config/CCMSApiConfigurationTest.java
@@ -22,6 +22,7 @@ public class CCMSApiConfigurationTest {
 
         ccmsApiConfiguration.setOfflineTransactionDelayOffset(15);
         ccmsApiConfiguration.setOfflineTimeRanges("[]");
+        ccmsApiConfiguration.setIntegrationEnabled(true);
 
         ccmsApiConfiguration.init();
     }
@@ -34,6 +35,7 @@ public class CCMSApiConfigurationTest {
         assertThat(ccmsApiConfiguration.getOfflineTransactionDelayOffset()).isEqualTo(15);
         assertThat(ccmsApiConfiguration.getOfflineTimeRanges()).isEqualTo("[]");
         assertThat(ccmsApiConfiguration.getCcmsOfflineTimeRanges().isEmpty()).isTrue();
+        assertThat(ccmsApiConfiguration.isCCMSIntegrationEnabled()).isTrue();
     }
 
     @Test
@@ -105,6 +107,83 @@ public class CCMSApiConfigurationTest {
         assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo(0);
 
         // Tomorrow 11:00:01pm - OFFLINE
+        tomorrow = tomorrow.withHour(23).withMinute(0).withSecond(1).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(tomorrow)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo((6 * 60 * 60) - 1);
+    }
+
+    @Test
+    public void defaultCCMSOfflineConfiguration_CCMS_disabled() {
+        // By default, we'll want to have CCMS off from 11pm - 5am.
+        ccmsApiConfiguration.setOfflineTimeRanges("[{\"start\": \"23:00\", \"end\": \"05:00\"}]");
+
+        // For this test, CCMS integration is overridden and disabled!
+        ccmsApiConfiguration.setIntegrationEnabled(false);
+        ccmsApiConfiguration.init();
+
+        assertThat(ccmsApiConfiguration.getApiSubscriptionKey()).isEqualTo("test");
+        assertThat(ccmsApiConfiguration.getBaseUrl()).isEqualTo("http://localhost:8080");
+        assertThat(ccmsApiConfiguration.getTransactionDelayMinutes()).isEqualTo(60);
+        assertThat(ccmsApiConfiguration.getOfflineTransactionDelayOffset()).isEqualTo(15);
+
+        assertThat(ccmsApiConfiguration.getOfflineTimeRanges()).isEqualTo("[{\"start\": \"23:00\", \"end\": \"05:00\"}]");
+        assertThat(ccmsApiConfiguration.getCcmsOfflineTimeRanges().size() == 1).isTrue();
+
+        ZoneId zoneId = ZoneId.of("America/Chicago");
+        ZonedDateTime today = ZonedDateTime.now(zoneId);
+
+        // 9:30am - CCMS should be ONLINE, but it's overridden to be completely disabled
+        today = today.withHour(9).withMinute(30).withSecond(0).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(today)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(today)).isEqualTo(3600);
+
+        // 9:30pm - CCMS should be ONLINE, but it's overridden to be completely disabled
+        today = today.withHour(21).withMinute(30).withSecond(0).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(today)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(today)).isEqualTo(3600);
+
+        // 10:59:59pm - CCMS should be ONLINE, but it's overridden to be completely disabled
+        today = today.withHour(22).withMinute(59).withSecond(59).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(today)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(today)).isEqualTo(3600);
+
+        // 11:00:00pm - OFFLINE due to the time range
+        today = today.withHour(23).withMinute(0).withSecond(0).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(today)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(today)).isEqualTo(6 * 60 * 60);
+
+        // 11:00:01pm - OFFLINE due to the time range
+        today = today.withHour(23).withMinute(0).withSecond(1).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(today)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(today)).isEqualTo((6 * 60 * 60) - 1);
+
+        // 11:59:59pm - OFFLINE due to the time range
+        today = today.withHour(23).withMinute(59).withSecond(59).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(today)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(today)).isEqualTo(1 + (5 * 60 * 60));
+
+        // Tomorrow 12:00:00am - OFFLINE due to the time range
+        ZonedDateTime tomorrow = today.plusDays(1);
+        tomorrow = tomorrow.withHour(0).withMinute(0).withSecond(0).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(tomorrow)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo((5 * 60 * 60));
+
+        // Tomorrow 4:59:59am - OFFLINE due to the time range
+        tomorrow = tomorrow.withHour(4).withMinute(59).withSecond(59).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(tomorrow)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo(1);
+
+        // Tomorrow 5:00:00am - OFFLINE due to the time range
+        tomorrow = tomorrow.withHour(5).withMinute(0).withSecond(0).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(tomorrow)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo(0);
+
+        // Tomorrow 5:00:01am - CCMS should be ONLINE, but it's overridden to be completely disabled
+        tomorrow = tomorrow.withHour(5).withMinute(0).withSecond(1).withNano(0);
+        assertThat(ccmsApiConfiguration.isOnlineAt(tomorrow)).isFalse();
+        assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo(3600);
+
+        // Tomorrow 11:00:01pm - OFFLINE due to the time range
         tomorrow = tomorrow.withHour(23).withMinute(0).withSecond(1).withNano(0);
         assertThat(ccmsApiConfiguration.isOnlineAt(tomorrow)).isFalse();
         assertThat(ccmsApiConfiguration.getSecondsUntilEndOfOfflineRangeStartingAt(tomorrow)).isEqualTo((6 * 60 * 60) - 1);
