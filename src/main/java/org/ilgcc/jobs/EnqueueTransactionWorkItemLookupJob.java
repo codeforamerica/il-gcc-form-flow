6e049c640d73b02e7bf92d0d2eec034bf8eab8f0
Mahbubullah Mohammed <Mahbubullah.mohammed@krasanconsulting.com>
2025-09-24 16:06:40 -0500

    [CCAP-1236]-

---
+++ src/main/java/org/ilgcc/jobs/EnqueueTransactionWorkItemLookupJob.java
@@ -0,0 +1,13 @@
+package org.ilgcc.jobs;
+
+import org.ilgcc.app.data.Transaction;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnqueueTransactionWorkItemLookupJob {
+
+    public void lookupWorkItemIDForTransaction(Transaction transaction) {
+        // TODO: Add implementation logic
+        System.out.println("Looking up Work Item ID for transaction: " + transaction.getTransactionId());
+    }
+}
