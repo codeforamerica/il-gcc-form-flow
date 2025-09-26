f7c9bfee4e03f6caef58bbd5daa80e6d2e258e1a
Ana Medrano <amedrano@codeforamerica.org>
2025-09-26 16:26:43 -0700

    [CCAP-1401] - updates flows config and conditions. Tested with e2e selenium test

--- src/main/java/org/ilgcc/app/submission/conditions/HasProviderForMoreThanOneChild.java
+++ src/main/java/org/ilgcc/app/submission/conditions/HasProviderForMoreThanOneChild.java
@@ -4,19 +4,13 @@ import static org.ilgcc.app.utils.SubmissionUtilities.getChildrenNeedingAssistan
 import static org.ilgcc.app.utils.SubmissionUtilities.hasChosenProvider;
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HasProviderForMoreThanOneChild implements Condition {
-    private final boolean enableMultipleProviders;
-
-    public HasProviderForMoreThanOneChild(@Value("${il-gcc.enable-multiple-providers}") boolean enableMultipleProviders) {
-        this.enableMultipleProviders = enableMultipleProviders;
-    }
     @Override
     public Boolean run(Submission submission) {
         boolean moreThanOneChildNeedsChildcareAssistance = getChildrenNeedingAssistance(submission.getInputData()).size() > 1;
-        return enableMultipleProviders && hasChosenProvider(submission) && moreThanOneChildNeedsChildcareAssistance;
+        return hasChosenProvider(submission) && moreThanOneChildNeedsChildcareAssistance;
     }
 }
