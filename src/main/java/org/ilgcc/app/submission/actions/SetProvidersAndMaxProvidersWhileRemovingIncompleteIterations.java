8f93f8d42db434cc671bdc61ad9f239b42c8f1bc
Marc Perlman <mperlman@codeforamerica.org>
2025-09-29 17:21:42 -0500

    [CCAP-1430] Remove childcare schedules if a provider within one is not a valid provider

--- src/main/java/org/ilgcc/app/submission/actions/SetProvidersAndMaxProvidersWhileRemovingIncompleteIterations.java
+++ src/main/java/org/ilgcc/app/submission/actions/SetProvidersAndMaxProvidersWhileRemovingIncompleteIterations.java
@@ -6,8 +6,11 @@ import static org.ilgcc.app.utils.SubmissionUtilities.getChildrenNeedingAssistan
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
 import formflow.library.data.SubmissionRepositoryService;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -15,35 +18,68 @@ import org.springframework.stereotype.Component;
 @Component
 public class SetProvidersAndMaxProvidersWhileRemovingIncompleteIterations implements Action {
 
-  private final SubmissionRepositoryService submissionRepositoryService;
+    private final SubmissionRepositoryService submissionRepositoryService;
 
-  public SetProvidersAndMaxProvidersWhileRemovingIncompleteIterations(
-      SubmissionRepositoryService submissionRepositoryService) {
-    this.submissionRepositoryService = submissionRepositoryService;
-  }
-
-  @Override
-  public void run(Submission submission) {
-    Map<String, Object> familyInputData = submission.getInputData();
-    var subflowData = (List<Map<String, Object>>) submission.getInputData().getOrDefault("providers", emptyList());
-    if (!subflowData.isEmpty()) {
-      subflowData.removeIf(providerIteration -> !(boolean) providerIteration.getOrDefault("iterationIsComplete", false));
-      submissionRepositoryService.save(submission);
+    public SetProvidersAndMaxProvidersWhileRemovingIncompleteIterations(
+            SubmissionRepositoryService submissionRepositoryService) {
+        this.submissionRepositoryService = submissionRepositoryService;
     }
 
-    List<Map<String, Object>> providers = (List<Map<String, Object>>) familyInputData.getOrDefault("providers",
-        emptyList());
-    submission.getInputData().put("maxProvidersAllowed", getMaxProvidersAllowed(submission));
-    submission.getInputData().put("providers", providers);
-  }
+    @Override
+    public void run(Submission submission) {
+        List<Map<String, Object>> providers = (List<Map<String, Object>>) submission.getInputData().getOrDefault(
+                "providers",
+                emptyList());
 
+        if (providers.isEmpty()) {
+            submission.getInputData().remove("childcareSchedules");
+        } else {
+            submission.getInputData().put("providers", providers.stream()
+                    .filter(providerIteration -> (boolean) providerIteration.getOrDefault("iterationIsComplete", false))
+                    .collect(Collectors.toList()));
 
-  private int getMaxProvidersAllowed(Submission submission) {
-    // Families with only 1 child never get to see the screen that would set choseProviderForEveryChildInNeedOfCare
-    if ("true".equals(submission.getInputData().get("choseProviderForEveryChildInNeedOfCare")) || getChildrenNeedingAssistance(submission.getInputData()).size() == 1) {
-      return 3;
-    } else {
-      return 2;
+            if (submission.getInputData().containsKey("childcareSchedules")) {
+                List<Map<String, Object>> childcareSchedules = (List<Map<String, Object>>) submission.getInputData()
+                        .get("childcareSchedules");
+
+                Set<String> validProviderUuids = providers.stream()
+                        .map(provider -> (String) provider.get("uuid"))
+                        .collect(Collectors.toSet());
+
+                validProviderUuids.add("NO_PROVIDER");
+
+                childcareSchedules.removeIf(schedule -> {
+                    List<String> childcareProvidersForCurrentChild = (List<String>) schedule.getOrDefault(
+                            "childcareProvidersForCurrentChild[]", new ArrayList<>());
+
+                    if (childcareProvidersForCurrentChild == null || childcareProvidersForCurrentChild.isEmpty()) {
+                        return true;
+                    } else {
+                        for (String childcareProvider : childcareProvidersForCurrentChild) {
+                            if (!validProviderUuids.contains(childcareProvider)) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                });
+            }
+        }
+
+        submission.getInputData().put("maxProvidersAllowed", getMaxProvidersAllowed(submission));
+        submission.getInputData().put("providers", providers);
+
+        submissionRepositoryService.save(submission);
     }
-  }
+
+
+    private int getMaxProvidersAllowed(Submission submission) {
+        // Families with only 1 child never get to see the screen that would set choseProviderForEveryChildInNeedOfCare
+        if ("true".equals(submission.getInputData().get("choseProviderForEveryChildInNeedOfCare"))
+                || getChildrenNeedingAssistance(submission.getInputData()).size() == 1) {
+            return 3;
+        } else {
+            return 2;
+        }
+    }
 }
