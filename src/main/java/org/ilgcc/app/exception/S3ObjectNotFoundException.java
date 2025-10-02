b5f61043449ce28ed9d0df961d89a4f2297f4fce
Marc P <mperlman@codeforamerica.org>
2025-10-02 12:46:23 -0500

    [CCAP-1437] Reduce noise and kill job when multiple retries will never ever matter (#2023)

--- src/main/java/org/ilgcc/app/exception/S3ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.ilgcc.app.exception;
-
-public class S3ObjectNotFoundException extends RuntimeException{
-    public S3ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
