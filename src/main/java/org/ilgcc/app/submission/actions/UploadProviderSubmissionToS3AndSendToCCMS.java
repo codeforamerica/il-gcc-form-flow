037059b9dc9bee6f058bab60f6252b1da808dc5d
Marc Perlman <mperlman@codeforamerica.org>
2025-10-02 09:44:37 -0500

    Cleaning up some logging and comments

--- src/main/java/org/ilgcc/app/submission/actions/UploadProviderSubmissionToS3AndSendToCCMS.java
+++ src/main/java/org/ilgcc/app/submission/actions/UploadProviderSubmissionToS3AndSendToCCMS.java
@@ -1,6 +1,5 @@
 package org.ilgcc.app.submission.actions;
 
-
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
 import formflow.library.data.SubmissionRepositoryService;
@@ -11,7 +10,6 @@ import org.ilgcc.app.data.SubmissionSenderService;
 import org.ilgcc.app.utils.ProviderSubmissionUtilities;
 import org.springframework.stereotype.Component;
 
-
 @Slf4j
 @Component
 public class UploadProviderSubmissionToS3AndSendToCCMS implements Action {
@@ -21,31 +19,32 @@ public class UploadProviderSubmissionToS3AndSendToCCMS implements Action {
     private final SubmissionSenderService submissionSenderService;
 
     public UploadProviderSubmissionToS3AndSendToCCMS(SubmissionSenderService submissionSenderService,
-        SubmissionRepositoryService submissionRepositoryService) {
+            SubmissionRepositoryService submissionRepositoryService) {
         this.submissionSenderService = submissionSenderService;
         this.submissionRepositoryService = submissionRepositoryService;
     }
 
     @Override
     public void run(Submission providerSubmission) {
-        // If a provider is an existing provider that has done CCAP stuff before, send their submission to CCMS
-        // New Provider Registration will send the application later
-        // Prevent sending provider submissions if a providers application has expired.
-
         Optional<Submission> familySubmissionOptional = getFamilySubmission(providerSubmission, submissionRepositoryService);
         if (familySubmissionOptional.isPresent()) {
-            if (!ProviderSubmissionUtilities.hasProviderApplicationExpired(familySubmissionOptional.get(), providerSubmission)){
+            // Prevent sending provider submissions if a providers application has expired.
+            if (!ProviderSubmissionUtilities.hasProviderApplicationExpired(familySubmissionOptional.get(), providerSubmission)) {
+                // If a provider is an existing provider that has done CCAP stuff before, send their submission to CCMS
+                // New Provider Registration will send the application later
                 if (!ProviderSubmissionUtilities.isProviderRegistering(providerSubmission)) {
                     submissionSenderService.sendProviderSubmission(providerSubmission);
                 }
-            }else {
-                log.error("Your provider submission {} expired.", providerSubmission.getId());
+            } else {
+                log.info("Provider submission {} expired for family submission {}.", providerSubmission.getId(), familySubmissionOptional.get().getId());
             }
-        }else {
-            log.error("Family Submission not found for provider: " + providerSubmission.getId());
+        } else {
+            log.error("Family submission not found for provider submission: " + providerSubmission.getId());
         }
     }
-    private static Optional<Submission> getFamilySubmission(Submission providerSubmission, SubmissionRepositoryService submissionRepositoryService) {
+
+    private static Optional<Submission> getFamilySubmission(Submission providerSubmission,
+            SubmissionRepositoryService submissionRepositoryService) {
         Optional<UUID> familySubmissionId = ProviderSubmissionUtilities.getFamilySubmissionId(providerSubmission);
         if (familySubmissionId.isPresent()) {
             return submissionRepositoryService.findById(familySubmissionId.get());
